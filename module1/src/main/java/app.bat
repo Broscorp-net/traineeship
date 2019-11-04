a34545e41b6782595f4619827194ddb238ca8e88
dmitry <dafuct@gmail.com>
2019-11-04 23:34:30 +0200

    write app.bat which create two file txt with instructions byte code

--- module1/src/main/java/app.bat
+++
@@ -1,6 +0,0 @@
-javac net\broscorp\b_bytecode\MainFirst.java
-javac net\broscorp\b_bytecode\MainSecond.java
-java net.broscorp.b_bytecode.MainFirst
-java net.broscorp.b_bytecode.MainSecond
-java -jar lib\jd-cli.jar -n -od net\broscorp\b_bytecode\decompiler net\broscorp\b_bytecode\MainFirst.class
-java -jar lib\jd-cli.jar -n -od net\broscorp\b_bytecode\decompiler net\broscorp\b_bytecode\MainSecond.class
---
+++ module1/src/main/java/net/broscorp/b_bytecode/app.bat
@@ -0,0 +1,4 @@
+javac MainFirst.java
+javac MainSecond.java
+javap -c MainFirst > first.txt
+javap -c MainSecond > second.txt
