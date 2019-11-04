a22e7b3233885c319e6344f4b5bd8e82ba40e18c
Adamenko Volodymyr <vovaadamenko01@gmail.com>
2019-11-04 14:54:16 +0200

    Второе задание/правка1

---
+++ module1/src/main/java/net/broscorp/b_bytecode/MyClass1.java
@@ -0,0 +1,19 @@
+public class MyClass1<Integer>{                     
+public static void main(String[] args){
+
+int i = 0;
+
+ 
+while(i<=12){
+  i++;
+}
+
+int number = getNumber();
+
+System.out.println("Byte code workflow");
+
+}
+public static int getNumber(){
+     return 5;
+}
+}
---
+++ module1/src/main/java/net/broscorp/b_bytecode/MyClass2.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class MyClass2{
+
+private String name;
+protected String password;
+public Float someFloat;
+
+public static void main(String[] args){
+
+int i = 0;
+
+ 
+ArrayList<String> arrayList = new ArrayList<String>();
+
+int number = getNumber();
+
+System.out.println("Byte code workflow");
+
+}
+public static int getNumber(){
+     return 5;
+}
+}
