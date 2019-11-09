27455e4cf7cf7d0bc6017d0abae45102cc785e3c
ponomaryova-yulia <57496830+ponomaryova-yulia@users.noreply.github.com>
2019-11-09 23:07:47 +0200

    b_bytecode

---
+++ module1/src/main/java/net/broscorp/b_bytecode/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    private static void method() { }
+
+    public static void main(String[] args) {
+        int index = 0;
+        for(int i = 0; i < 10; i++) {
+            index++;
+        }
+
+        method();
+        System.out.println("Hello, world!");
+    }
+}
+
+
+
---
+++ module1/src/main/java/net/broscorp/b_bytecode/script.cmd
@@ -0,0 +1,4 @@
+javac Main.java
+java Main
+javap -c Main
+cmd /k
