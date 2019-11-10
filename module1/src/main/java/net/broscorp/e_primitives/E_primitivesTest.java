4c6a33062176c90d6f4a2be1cd0efe112f8647d6
AdamenkoVlodymyr <vovaadanenko01@gmail.com>
2019-11-10 21:51:40 +0200

    e_primitive/correct1

--- module1/src/main/java/net/broscorp/e_primitives/E_primitivesTest.java
+++ module1/src/main/java/net/broscorp/e_primitives/E_primitivesTest.java
@@ -1,49 +1,55 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
 class E_primitivesTest {
 
-	
-	/**
-	 * In this taste case I try increment max Byte value, and failed on test execution.
-	 * Reason - byte can consist only 127; 
-	 */
-	@Test
-	void testOverflow() {
-		E_primitives e_prim = new E_primitives();
-		byte a = Byte.MAX_VALUE;   
-		byte result = e_prim.overflow(a);   
-		assertEquals(128,result); 
-	}
+  /**
+   * In this taste case I try increment max Byte value, and failed on test execution.
+   * Reason - byte can consist only 127; 
+   */
+  @Test
+  void testOverflow() {
+      
+      byte a = Byte.MAX_VALUE; 
+        a++;
+      byte result = a;   
+      assertEquals(128,result); 
+  }
 
-	
-	/**
-	 * In this test case I try to push double, into integer. Integer has now decimal part, that means
-	 * loosing of information. And as a result - test had failed.
-	 */
-	@Test
-	void testConvertation() {
-		E_primitives e_prim = new E_primitives();
-		double b = 12.9999;
-		 int result = e_prim.convertation(b);
-		 assertEquals(b,result);
-		 
-	}
+  
+  /**
+   * In this test case I try to push double, into integer. Integer has now decimal part, that means
+   * loosing of information. And as a result - test had failed.
+   */
+  @Test
+  void testConvertation() {
+     
+     double a = 12.9999;
+      int b = (int)a;
+      assertEquals(a,b);
+       
+  }
 
-	
-	/**
-	 * This test depictured mess conduction during Java simple  arithmetical function (multiplication) 
-	 * upon numbers with decimal point. We know for  sure that 2.11*5 = 10.55, but java thinking in other
-	 * way. 
-	 * Result - loosing of data, and fail on test. 
-	 */
-	@Test
-	void testDeviation() {
-		E_primitives e_prim = new E_primitives();
-		double a = 2.11;
-		double b = 5.0;
-		double result = e_prim.deviation(a, b);
-		assertEquals(10.55, result);
-	}
+  
+  /**
+   * This test depictured mess conduction during Java simple  arithmetical function (multiplication) 
+   * upon numbers with decimal point. We know for  sure that 2.11*5 = 10.55. This is happens because of storing data with decimal
+   * point will take a lot of memory, as we knows memory allocated for primitives is restricted. So the only way to store
+   * kinda data that makes rounding. But as well as all data finally stored in bite format, we have lose on precision.
+   * This is common compute problem, not just Java. 
+   * By the way Java has several solution for this problem:
+   * 1) key word "strictfp" - which is guaranty that all way long during calculation data will be store with restrict precision on rounding 
+   * 2) BigDecimal Class - which handles grate precision by providing very big and very small floating points numbers 
+   * 
+   * 
+   * Result - loosing of data, and fail on test. 
+   */
+  @Test
+  void testDeviation() {
+      
+      double a = 2.11;
+      double b = 5.0;
+      double result = a*b;
+      assertEquals(10.55, result);
+  }
 }
