1c071a532b7bebe1dce60c0a388a041d0ed0caa7
AdamenkoVlodymyr <vovaadanenko01@gmail.com>
2019-11-11 01:09:22 +0200

    g_value_ref

--- module1/src/main/java/net/broscorp/g_value_ref/Foo.java
+++ module1/src/main/java/net/broscorp/g_value_ref/Foo.java
@@ -1,11 +1,13 @@
-package net.broscorp.g_value_ref;
-
 import java.util.List;
 
-public class Foo {
 
-  private static void foo(int i, List<String> s) {
+public class Foo {
+  
+ @SuppressWarnings("unused")
+private  void foo (int i, List<String> s) {
     s.add("d");
-    i += 15;
+   i++;
+
   }
+ 
 }
