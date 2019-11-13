b9c8e280cf0f75988558800e84dc25101bcbb419
dmitry <dafuct@gmail.com>
2019-11-13 11:29:51 +0200

    code reformat in class Foo

--- module1/src/main/java/net/broscorp/g_value_ref/Foo.java
+++ module1/src/main/java/net/broscorp/g_value_ref/Foo.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class Foo {
 
-  public  void foo(int i, List<String> s) {
-    s.add("d");
-    i += 15;
-  }
+    public void foo(int i, List<String> s) {
+        s.add("d");
+        i += 15;
+    }
 }
