d86fc11f9a6c0a9d23c8615421ad0a8261107aa0
Dmitriy <dmitriy.belyi88@gmail.com>
2019-11-17 21:32:20 +0200

    e_primitives task fixed

--- module1/src/main/java/net/broscorp/e_primitives/NumbersOverflowTest.java
+++ module1/src/main/java/net/broscorp/e_primitives/NumbersOverflowTest.java
@@ -6,59 +6,59 @@ class NumbersOverflowTest {
 
   @Test
   public void shouldAnalyseByteOverflow() {
-    NumbersOverflow example = new NumbersOverflow();
-    byte result = example.ByteOverflow();
-    assertEquals(-128, result);
+    byte byteTest = Byte.MAX_VALUE;
+    byte result = ++byteTest;
+    assertEquals(Byte.MAX_VALUE + 1, result);
   }
 
   @Test
   public void shouldAnalyseShortOverflow() {
-    NumbersOverflow example = new NumbersOverflow();
-    short result = example.ShortOverflow();
-    assertEquals(-32768, result);
+    short shortTest = Short.MAX_VALUE;
+    short result = ++shortTest;
+    assertEquals(Short.MAX_VALUE + 1, result);
   }
 
 
   @Test
   public void shouldAnalyseIntegerOverflow() {
-    NumbersOverflow example = new NumbersOverflow();
-    int result = example.IntegerOverflow();
-    assertEquals(-2147483648, result);
+    int intTest = Integer.MAX_VALUE;
+    int result = ++intTest;
+    assertEquals((long) Integer.MAX_VALUE + 1, result);
   }
 
   @Test
   public void shouldAnalyseLongOverflow() {
-    NumbersOverflow example = new NumbersOverflow();
-    long result = example.LongOverflow();
+    long longTest = Long.MAX_VALUE;
+    long result = ++longTest;
     assertEquals(-9223372036854775808L, result);
   }
 
   @Test
   public void shouldAnalyseFloatOverflow() {
-    NumbersOverflow example = new NumbersOverflow();
-    float result = example.FloatOverflow();
-    assertEquals(3.4028235E38, result);
+    float floatTest = Float.MAX_VALUE;
+    float result = ++floatTest;
+    assertEquals(Float.MAX_VALUE, result);
 
   }
 
   @Test
   public void shouldAnalyseDoubleOverflow() {
-    NumbersOverflow example = new NumbersOverflow();
-    double result = example.DoubleOverflow();
-    assertEquals(1.7976931348623157E308, result);
+    double doubleTest = Double.MAX_VALUE;
+    double result = ++doubleTest;
+    assertEquals(Double.MAX_VALUE, result);
   }
 
   @Test
   public void shouldAnalysePrimitivesConversion() {
-    NumbersOverflow example = new NumbersOverflow();
-    int result = example.PrimitivesConversion();
-    assertEquals(15, result);
+    long b = (long) Integer.MAX_VALUE;
+    b++;
+    int result = (int) b;
+    assertEquals(b, result);
   }
 
   @Test
-  public void shouldAnalyseFloatNumbersConversion() {
-    NumbersOverflow example = new NumbersOverflow();
-    int result = example.FloatNumbersConversion();
-    assertEquals(10, result);
+  public void shouldAnalyseFloatNumbers() {
+    double result = 2.0 - 1.1;
+    assertEquals(0.9, result);
   }
 }
