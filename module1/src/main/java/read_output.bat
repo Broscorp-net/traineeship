a1cf0e84064e8c69a4a496fdfc257df4b2ef140e
dmitry <dafuct@gmail.com>
2019-11-20 22:59:59 +0200

    change bat file: enter read.jar

--- module1/src/main/java/read_output.bat
+++ module1/src/main/java/read_output.bat
@@ -1,4 +1,5 @@
 javac net\broscorp\k_classpath\*.java
 jar -cfe net\broscorp\k_classpath\read.jar net.broscorp.k_classpath.Read net\broscorp\k_classpath\*.class
+java -jar net\broscorp\k_classpath\read.jar
 
 
