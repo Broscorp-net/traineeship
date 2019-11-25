003a183e92c281e130a4e6588966e17b72e18b8b
AdamenkoVlodymyr <vovaadanenko01@gmail.com>
2019-11-25 22:59:20 +0200

    n_strings/understud

---
+++ module1/src/test/java/StringsTest.java
@@ -0,0 +1,47 @@
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import net.broscorp.n_strings.Strings;
+
+class StringsTest   {
+
+  private static Strings strings;
+
+  @BeforeAll
+  public static void initialization() {
+    strings = new Strings();
+  }
+  
+  @Test
+  void testIsPalindrom() {
+    strings.isPalindrom("mam");
+    assertTrue(strings.isPalindrom("mam"));
+  }
+
+  @Test
+  void testWordDeletion() {
+    assertEquals("Helloyuy", strings.wordDeletion("Hello world")     );
+  }
+
+  @Test
+  void testLetterDeletion() {
+    assertEquals("Heo word", strings.letterDeletion("Hello world"));
+  }
+
+  @Test
+  void testLettersChaneging() {
+    assertEquals("Hellq wqrld", strings.lettersChaneging("Hello world"));
+  }
+
+  @Test
+  void testWordsLetters() {
+    assertEquals(2, strings.wordsLetters("Hello world").size());
+  }
+
+  @Test
+  void testMakeSong() {
+    assertTrue(strings.makeSong(2, 3) instanceof String);
+  }
+
+}
