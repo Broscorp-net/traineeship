fcb6402daa168da032f0bd8ad852ad53a2b39d6e
AdamenkoVlodymyr <vovaadanenko01@gmail.com>
2019-11-25 23:23:43 +0200

    jlgk;kg;kggjk

--- module1/src/test/java/net/broscorp/n_strings/StringsTest.java
+++ module1/src/test/java/net/broscorp/n_strings/StringsTest.java
@@ -20,7 +20,7 @@ class StringsTest {
 
   @Test
   void testWordDeletion() {
-    assertEquals("Heghjgjhgllho", strings.wordDeletion("Hello world"));
+    assertEquals("jhgllho", strings.wordDeletion("Hello world"));
   }
 
   @Test
