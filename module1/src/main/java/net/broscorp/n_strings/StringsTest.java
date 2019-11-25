684d497a644fdc81a633eb8adab129d4f62e3be2
AdamenkoVlodymyr <vovaadanenko01@gmail.com>
2019-11-25 20:04:12 +0200

    n_strings

---
+++ module1/src/main/java/net/broscorp/n_strings/StringsTest.java
@@ -0,0 +1,47 @@
+package net.broscorp.n_strings;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class StringsTest {
+
+  private static Strings strings;
+
+  @BeforeAll
+  public static void initialization() {
+    strings = new Strings();
+  }
+
+  @Test
+  void testIsPalindrom() {
+    strings.isPalindrom("mam");
+    assertTrue(strings.isPalindrom("mam"));
+  }
+
+  @Test
+  void testWordDeletion() {
+    assertEquals("Hello", strings.wordDeletion("Hello world"));
+  }
+
+  @Test
+  void testLetterDeletion() {
+    assertEquals("Heo word", strings.letterDeletion("Hello world"));
+  }
+
+  @Test
+  void testLettersChaneging() {
+    assertEquals("Hellq wqrld", strings.lettersChaneging("Hello world"));
+  }
+
+  @Test
+  void testWordsLetters() {
+    assertEquals(2, strings.wordsLetters("Hello world").size());
+  }
+
+  @Test
+  void testMakeSong() {
+    assertTrue(strings.makeSong(2, 3) instanceof String);
+  }
+
+}
