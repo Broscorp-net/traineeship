09286d5ef0c2373f25f48608ac7a8762111555cd
AdamenkoVlodymyr <vovaadanenko01@gmail.com>
2019-11-25 21:12:20 +0200

    n_strings/changes check

--- module1/src/main/java/net/broscorp/n_strings/StringsTest.java
+++ module1/src/main/java/net/broscorp/n_strings/StringsTest.java
@@ -3,7 +3,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class StringsTest {
+class StringsTest   {
 
   private static Strings strings;
 
