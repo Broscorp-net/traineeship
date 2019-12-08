098c3ce5c297ff26c52ab12a85ee9266e8a0b93a
Roman Zagorulko <rr.zagorulko@gmail.com>
2019-12-08 15:04:12 +0200

    Create FooTest.java

---
+++ module1/src/main/java/net/broscorp/g_value_ref/FooTest.java
@@ -0,0 +1,36 @@
+package net.broscorp.g_value_ref;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import org.junit.jupiter.api.Test;
+
+class FooTest {
+
+  @Test
+  public void fooCheckList() {
+    Foo foo = new Foo();
+    int i = 10;
+    ArrayList s = new ArrayList<String>();
+    //передали в метод ArrayList ожидаем что метод добавит "d" в этот лист
+    foo.foo(i, s);
+    assertEquals("d", s.get(0));
+
+  }
+
+  @Test
+  public void fooCheckInt() {
+    Foo foo = new Foo();
+    int i = 10;
+    ArrayList s = new ArrayList<String>();
+    //передали в метод int ожидаем что int не измениться
+    //потому что примитивы передаются по значению
+    foo.foo(i, s);
+    assertEquals(10, i);
+
+  }
+}
