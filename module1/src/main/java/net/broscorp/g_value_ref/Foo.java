e4855cac9666cdc9ecdd05e4f0b73f950489a0a9
MaxD <max.moderation@gmail.com>
2019-12-08 13:34:03 +0200

    g_value_ref

--- module1/src/main/java/net/broscorp/g_value_ref/Foo.java
+++ module1/src/main/java/net/broscorp/g_value_ref/Foo.java
@@ -7,8 +7,5 @@ public class Foo {
   public void foo(int i, List<String> s) {
     s.add("d");
     i += 15;
-
-    System.out.println("Inside foo() -> i = " + i + ", s = " + s);
-
   }
 }
--- module1/src/test/net/broscorp/g_value_ref/FooTest.java
+++ module1/src/test/net/broscorp/g_value_ref/FooTest.java
@@ -12,27 +12,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class FooTest {
 
     Foo foo = new Foo();
+    int beforeNum = 100; // beforeNum, beforeStr - сохранить состояние до вызова метода
+    List<String> beforeStr = new ArrayList<>();
+
+    /**
+     * В метод передается копия значения переменной "int i" и копия ссылки "s"
+     * на массив списка строк "ArrayList". В методе "foo()" создается новая переменная,
+     * которой присваивается значение 115, но это значение не возвращается в переданную
+     * переменную, поэтому int i = 100 не поменялось после отработки метода.
+     * А значение объекта пеменялось на [d], так как обе ссылки получили доступ к
+     * одному объекту в памяти и могут влиять на его содержимое.
+     */
+    @Test
+    void fooRef() {
+        List<String> s = new ArrayList<>();
+        beforeStr = s;
+        foo.foo(beforeNum, s);
+        assertEquals(beforeStr, s);
+    }
 
     @Test
-    void foo() {
+    void fooValue() {
         int i = 100;
-        List<String> s = new ArrayList<>();
-        s.add("c");
-        System.out.print("Before method income ");
-        System.out.print("i = " + i);
-        System.out.println(", s = " + s);
-        /**
-         * В метод передается копия значения переменной "int i" и копия ссылки "s"
-         * на массив списка строк "ArrayList". В методе "foo()" создается новая переменная
-         * которой присваивается значение 115, но это значение не возвращается в переданную
-         * переменную, поэтому int i = 100 не поменялось после отработки метода.
-         * А значение объекта пеменялось на [c, d], так как обе ссылки получили доступ к
-         * одному объекту в памяти и могут влиять на его содержимое.
-         */
-        foo.foo(i, s);
-        System.out.print("After method income ");
-        System.out.print("i = " + i);
-        System.out.println(", s = " + s);
-
+        beforeNum = i;
+        foo.foo(beforeNum, beforeStr);
+        assertEquals(beforeNum, i);
     }
 }
