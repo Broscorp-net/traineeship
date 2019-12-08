834f58ec14f226228029a45b08e93ca96f6fec6d
Roman Zagorulko <rr.zagorulko@gmail.com>
2019-12-08 15:12:31 +0200

    h_generics

---
+++ module1/src/main/java/net/broscorp/h_generics/MyCoolListTest.java
@@ -0,0 +1,34 @@
+package net.broscorp.h_generics;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class MyCoolListTest {
+
+  @Test
+  void getAdd() {
+    MyCoolList<Integer> mcl = new MyCoolList();
+    mcl.add(15);
+   assertEquals(15,mcl.get(0));
+  }
+  @Test
+  void removeAdd() {
+    MyCoolList<Integer> mcl = new MyCoolList();
+    mcl.add(15);
+    mcl.add(10);
+    mcl.remove(0);
+    assertEquals(10,mcl.get(0));
+  }
+
+  @Test
+  void map() {
+    MyCoolList<Integer> mcl = new MyCoolList();
+    mcl.add(15);
+    mcl.add(10);
+    MyCoolList<Integer> mcl1 = mcl.map(x-> (Integer)x * 2);
+    assertEquals(30 , mcl1.get(0));
+    assertEquals(20,mcl1.get(1));
+  }
+
+
+}
