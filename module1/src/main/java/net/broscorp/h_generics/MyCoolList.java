33adb3a10659533926947abdd10b9bbab562ac4f
Roman Zagorulko <rr.zagorulko@gmail.com>
2019-12-15 12:03:47 +0200

    Update MyCoolList.java

--- module1/src/main/java/net/broscorp/h_generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/h_generics/MyCoolList.java
@@ -3,37 +3,37 @@ package net.broscorp.h_generics;
 import java.util.function.Function;
 
 
-public class MyCoolList<N extends Number>  {
+public class MyCoolList<N extends Number> {
 
   private MyCoolList next;
-  private int index =0;
+  private int index = 0;
   private Object[] arr = new Object[32];
 
-
-  public void add(N o){
-    if (index == arr.length-1)
-    {
-      Object[] newArray = new Object[arr.length*2];
+  
+  public void add(N o) {
+    if (index == arr.length - 1) {
+      Object[] newArray = new Object[arr.length * 2];
       System.arraycopy(arr, 0, newArray, 0, index);
       arr = newArray;
     }
     arr[index++] = o;
   }
 
-  public N get(int index){
-    return (N)arr[index];
+  public N get(int index) {
+    return (N) arr[index];
   }
 
-  public void remove(int index){
-    for (int i = index; i<this.index; i++)
-      arr[i] = arr[i+1];
+  public void remove(int index) {
+    for (int i = index; i < this.index; i++) {
+      arr[i] = arr[i + 1];
+    }
     arr[this.index] = null;
     this.index--;
   }
 
-  public MyCoolList map(Function f) {
+  public MyCoolList<N> map(Function f) {
     MyCoolList<N> mcl = new MyCoolList<>();
-    for (int i = 0; i < index; i++){
+    for (int i = 0; i < index; i++) {
       mcl.add((N) f.apply(arr[i]));
     }
     return mcl;
