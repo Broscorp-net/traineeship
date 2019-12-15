cb7690d0df5daf8bee6a9f33f324e3397779c25c
Roman Zagorulko <rr.zagorulko@gmail.com>
2019-12-15 20:36:19 +0200

    Create MyCoolList.java

---
+++ module1/src/main/java/net/broscorp/h_generics/MyCoolList.java
@@ -0,0 +1,45 @@
+package net.broscorp.h_generics;
+
+import java.util.function.Function;
+
+
+public class MyCoolList<N extends Number> {
+
+  private MyCoolList next;
+  private int index = 0;
+  private Object[] arr = new Object[32];
+
+
+  public void add(N o) {
+    if (index == arr.length - 1) {
+      Object[] newArray = new Object[arr.length * 2];
+      System.arraycopy(arr, 0, newArray, 0, index);
+      arr = newArray;
+    }
+    arr[index++] = o;
+  }
+
+  public N get(int index) {
+    return (N) arr[index];
+  }
+
+  public void remove(int index) {
+    for (int i = index; i < this.index; i++) {
+      arr[i] = arr[i + 1];
+    }
+    arr[this.index] = null;
+    this.index--;
+  }
+
+  public MyCoolList<N> map(Function<Object,Object> f) {
+    MyCoolList<N> mcl = new MyCoolList<>();
+    for (int i = 0; i < index; i++) {
+      mcl.add((N) f.apply( arr[i]));
+    }
+    return mcl;
+  }
+
+  public int size() {
+    return index;
+  }
+}
