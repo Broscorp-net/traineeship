a9d074c1862ed850f38e3dad333fd61c891b3dd3
Bada1208 <sysoievbogdan@gmail.com>
2019-12-17 20:50:09 +0100

    m_override_overload commit message

--- module1/src/main/java/net/broscorp/m_override_overload/CalculatorTestClass.java
+++ module1/src/main/java/net/broscorp/m_override_overload/CalculatorTestClass.java
@@ -22,72 +22,63 @@ public class CalculatorTestClass {
     void sumDouble() {
         double a = 2.0;
         double b = 3.0;
-        double actual = calculatorAdd.operation(a, b);
-        assertEquals(5.0, actual);
+        assertEquals(5.0, calculatorAdd.operation(a, b));
     }
 
     @Test
     void sumDOUBLE() {
         Double a = 2.0;
         Double b = 2.0;
-        double actual = calculatorAdd.operation(a, b);
-        assertEquals(4.0, actual);
+        assertEquals(4.0, calculatorAdd.operation(a, b));
     }
 
     @Test
     void sumInt() {
         int a = 2;
         int b = 3;
-        double actual = calculatorAdd.operation(a, b);
-        assertEquals(5, actual);
+        assertEquals(5, calculatorAdd.operation(a, b));
     }
 
     @Test
     void sumINTEGER() {
         Integer a = 2;
         Integer b = 22;
-        double actual = calculatorAdd.operation(a, b);
-        assertEquals(24, actual);
+        assertEquals(24, calculatorAdd.operation(a, b));
     }
 
     @Test
     void multiplyDouble() {
         double a = 2.0;
         double b = 3.0;
-        double actual = calculatorMultiply.operation(a, b);
-        assertEquals(6.0, actual);
+        assertEquals(6.0, calculatorMultiply.operation(a, b));
     }
 
     @Test
     void multiplyDOUBLE() {
         Double a = 2.0;
         Double b = 5.0;
-        double actual = calculatorMultiply.operation(a, b);
-        assertEquals(10.0, actual);
+        assertEquals(10.0, calculatorMultiply.operation(a, b));
     }
 
     @Test
     void multiplyInt() {
         int a = 2;
         int b = 3;
-        double actual = calculatorMultiply.operation(a, b);
-        assertEquals(6, actual);
+        assertEquals(6, calculatorMultiply.operation(a, b));
     }
 
     @Test
     void multiplyINTEGER() {
         Integer a = 2;
         Integer b = 22;
-        double actual = calculatorMultiply.operation(a, b);
-        assertEquals(44, actual);
+        assertEquals(44, calculatorMultiply.operation(a, b));
     }
 
     @Test
     void mixedTypes() {
         Integer a = 5;
         int b = 3;
-        int actual = a * b;
-        assertEquals(15, actual);
+        assertEquals(15, a * b);
     }
 
 }
