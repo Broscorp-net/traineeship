512ce571a221c99cdff64b8941898fc0eb248209
Bada1208 <sysoievbogdan@gmail.com>
2019-12-25 18:20:30 +0100

    j_exceptions commit

---
+++ module1/src/main/java/net/broscorp/j_exceptions/ExceptionClass.java
@@ -0,0 +1,25 @@
+package net.broscorp.j_exceptions;
+
+import java.io.*;
+
+public class ExceptionClass {
+    public static void openFile(String fileName) {
+        try (Reader reader = new FileReader(fileName)) {
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            System.out.println("Finally block!!!");
+        }
+    }
+
+    public static void methodThrowable() {
+        int a = 10;
+        try {
+            int c = a / 0;
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+    }
+
+}
---
+++ module1/src/test/net/broscorp/j_exceptions/ExceptionsTestClass.java
@@ -0,0 +1,19 @@
+package net.broscorp.j_exceptions;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ExceptionsTestClass {
+
+    ExceptionClass exceptionClass=new ExceptionClass();
+
+    @Test
+    void ioexceptionTest(){
+        String fileName="name";
+        assertDoesNotThrow(()->exceptionClass.openFile(fileName));
+    }
+    @Test
+    void ancestorThrowableTest(){
+        assertDoesNotThrow(()->exceptionClass.methodThrowable());
+    }
+}
