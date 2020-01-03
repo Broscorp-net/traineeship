43af54578e99e1ec9a451f7a08eadb8b2c04a2cc
Bada1208 <sysoievbogdan@gmail.com>
2020-01-03 04:56:52 +0100

    h_generics try to fix build were added some changes2

--- module1/src/test/net/broscorp/h_generics/MyCoolListTestClass.java
+++ module1/src/test/net/broscorp/h_generics/MyCoolListTestClass.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class MyCoolListTestClass {
+
     MyOwnCoolList<Integer> myOwnCoolList;
 
     @BeforeEach
