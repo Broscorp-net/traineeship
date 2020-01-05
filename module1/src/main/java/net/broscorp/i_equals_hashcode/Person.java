ec6f1224304fa5ffe14e2ffdca6b0dbc968aa252
Bada1208 <sysoievbogdan@gmail.com>
2020-01-05 08:02:25 +0100

    i_equals_hashcode commit

---
+++ module1/src/main/java/net/broscorp/i_equals_hashcode/Person.java
@@ -0,0 +1,42 @@
+package net.broscorp.i_equals_hashcode;
+
+
+public class Person {
+    private String name;
+    private int age;
+    private int height;
+
+    public Person(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || this.getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && height == person.height && (name == person.name || name != null && name.equals(person.getName()));
+    }
+
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + age;
+        result = prime * result + height;
+        return result;
+    }
+}
---
+++ module1/src/test/net/broscorp/i_equals_hashcode/PersonTestClass.java
@@ -0,0 +1,84 @@
+package net.broscorp.i_equals_hashcode;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonTestClass {
+    @Test
+    void equalsReflexivenessTest() {
+        Person alex = new Person("Alex", 22, 180);
+        assertTrue(alex.equals(alex));
+    }
+
+    @Test
+    void equalsSymmetricTest() {
+        Person alex1 = new Person("Alex", 22, 180);
+        Person alex2 = new Person("Alex", 22, 180);
+
+        assertEquals(alex1.equals(alex2), alex2.equals(alex1));
+    }
+
+    @Test
+    void equalsTransitivenessTest() {
+        Person alex1 = new Person("Alex", 22, 180);
+        Person alex2 = new Person("Alex", 22, 180);
+        Person alex3 = new Person("Alex", 22, 180);
+        assertTrue(alex1.equals(alex2));
+        assertTrue(alex2.equals(alex3));
+        assertTrue(alex1.equals(alex3));
+
+    }
+
+    @Test
+    void equalsConsistencyTest() {
+        Person alex1 = new Person("Alex", 22, 180);
+        Person alex2 = new Person("Alex", 22, 180);
+        for (int i = 0; i < 5; i++) {
+            assertEquals(alex1.equals(alex2), alex2.equals(alex1));
+        }
+
+    }
+
+    @Test
+    void equalsNullTest() {
+        Person alex1 = new Person("Alex", 22, 180);
+        assertFalse(alex1.equals(null));
+    }
+
+    @Test
+    void hashCodeConsistencyTest() {
+        Person alex1 = new Person("Alex", 22, 180);
+        Person alex2 = new Person("Alex", 22, 180);
+        assertTrue(alex1.equals(alex2));
+        assertTrue(alex1.hashCode() == alex2.hashCode());
+    }
+
+    @Test
+    void hashCodeInternalConsistencyTest() {
+        Person alex1 = new Person("Alex", 22, 180);
+        Person alex2 = new Person("Alex2", 32, 170);
+        assertFalse(alex1.equals(alex2));
+        assertFalse(alex1.hashCode() == alex2.hashCode());
+    }
+
+    @Test
+    void hashCodeCollisionsTest() {
+        Map<Integer, Person> personMapCollision = new HashMap<>();
+        int duplicatedHashCodes = 0;
+        Random random = new Random(1_00_000);
+        for (int i = 0; i < 1_00_000; i++) {
+            Person person = new Person("Vasia", 22, random.nextInt(1_000_000));
+            if (!personMapCollision.containsKey(person.hashCode())) {
+                personMapCollision.put(person.hashCode(), person);
+            } else {
+                System.out.println(person.hashCode() + " this hashCode already exist");
+                duplicatedHashCodes++;
+            }
+
+        }
+        assertTrue(duplicatedHashCodes > 0);
+    }
+}
