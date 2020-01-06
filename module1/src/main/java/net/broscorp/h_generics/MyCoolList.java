09e6fdaec6961a8cd67d21d1dd413784d735018d
Bada1208 <sysoievbogdan@gmail.com>
2020-01-06 12:18:51 +0100

    h_generics fixed commit from new project

--- module1/src/main/java/net/broscorp/h_generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/h_generics/MyCoolList.java
@@ -1,28 +1,57 @@
 package net.broscorp.h_generics;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.function.Function;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class MyCoolList {
+public class MyCoolList<N extends Number> {
+    private int size = 0;
+    private Number[] array;
 
-  public void add(Object o){
-    throw new NotImplementedException();
-  }
+    public MyCoolList() {
+        this.array = new Number[10];
+        this.size = 0;
+    }
 
-  public Object get(int index){
-    throw new NotImplementedException();
-  }
+    public void add(N item) {
+        if (array.length - size <= 5) {
+            array = Arrays.copyOf(array, array.length * 2);
+        }
+        array[size++] = item;
+    }
 
-  public Object remove(int index){
-    throw new NotImplementedException();
-  }
+    public N get(int index) {
+        return (N) Array.get(array, index);
+    }
 
-  public MyCoolList map(Function f){
-    throw new NotImplementedException();
-  }
+    public N remove(int index) {
+        if (index < size) {
+            Number obj = array[index];
+            array[index] = null;
+            int tmp = index;
+            while (tmp < size) {
+                array[tmp] = array[tmp + 1];
+                array[tmp + 1] = null;
+                tmp++;
+            }
+            size--;
+            return (N) obj;
+        } else {
+            throw new ArrayIndexOutOfBoundsException();
+        }
 
-  public int size(){
-    throw new NotImplementedException();
-  }
+    }
+
+    public MyCoolList<? extends Number> map(Function<N, ? extends Number> f) {
+        MyCoolList myCoolList = new MyCoolList();
+        for (int i = 0; i < this.size; i++) {
+            myCoolList.add(f.apply((N) array[i]));
+        }
+        return myCoolList;
+    }
+
+    public int size() {
+        return size;
+    }
 
 }
