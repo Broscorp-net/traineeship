868fea2cb19fbb68e214117626fcd110f3f94691
Yura Horb <g.y.andreevich@gmail.com>
2020-06-21 13:32:25 +0300

    1st commit for e_primitives

---
+++ module1/src/main/java/net/broscorp/e_primitives/TestPrimitives.java
@@ -0,0 +1,41 @@
+package net.broscorp.e_primitives;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestPrimitives {
+
+    @Test
+    public void numberTypeOverflow(){
+        byte number = 127;
+        byte expectedResponse = -128;
+        assertEquals(expectedResponse, ++number);
+    }
+
+    @Test
+    public void conversionBetweenPrimitiveTypeLongToIntWithoutProblem(){
+        long number = 1147483647L;
+        int expectedResponse = 1147483647;
+        assertEquals(expectedResponse, (int) number);
+    }
+
+    @Test
+    public void narrowingDownPrimitiveTypesBetweenLongToIntWithOverflow(){
+        long number = 3147483647L;
+        int expectedResponse = -1147483649;
+        assertEquals(expectedResponse, (int) number);
+    }
+
+    @Test
+    public void conversionBetweenPrimitiveTypeFloatToDouble(){
+        float number = 0.1f;
+        double expectedResponse = 0.1;
+        assertTrue((Math.abs((double) number) - expectedResponse) < 0.0001);
+    }
+
+    @Test
+    public void uncertaintyWhenWorkingWithFloatingPointNumbers (){
+        float expected = 0.2f;
+        assertTrue(Math.abs(expected - 0.2) < 0.0001);
+    }
+}
