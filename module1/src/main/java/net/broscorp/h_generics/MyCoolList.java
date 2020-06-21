391f2e760bc62426a33f156ac4f9a3995bed800e
Yura Horb <g.y.andreevich@gmail.com>
2020-06-21 13:14:28 +0300

    1st commit

--- module1/src/main/java/net/broscorp/h_generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/h_generics/MyCoolList.java
@@ -1,28 +1,60 @@
 package net.broscorp.h_generics;
 
 import java.util.function.Function;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class MyCoolList {
+public class MyCoolList<T extends Number> {
+    private final int INIT_SIZE = 10;
+    private Object[] array = new Object[INIT_SIZE];
+    private int size = 0;
 
-  public void add(Object o){
-    throw new NotImplementedException();
-  }
+    public void add(T item) {
+        if (size == array.length - 1)
+            resize(array.length * 2);
+        array[size++] = item;
+    }
 
-  public Object get(int index){
-    throw new NotImplementedException();
-  }
+    public T get(int index) {
+        return (T) array[index];
+    }
 
-  public Object remove(int index){
-    throw new NotImplementedException();
-  }
+    public void remove(int index) {
+        for (int i = index; i < size; i++)
+            array[i] = array[i + 1];
+        array[size] = null;
+        size--;
+    }
 
-  public MyCoolList map(Function f){
-    throw new NotImplementedException();
-  }
+    public <R extends Number> MyCoolList<R> map(Function f) {
+        MyCoolList<R> result = new MyCoolList<>();
+        for (Object ls : array) {
+            if (ls == null)
+                break;
+            result.add((R) f.apply(ls));
+        }
+        return result;
+    }
 
-  public int size(){
-    throw new NotImplementedException();
-  }
+    public int size() {
+        return size;
+    }
+
+    private void resize(int newLength) {
+        Object[] newArray = new Object[newLength];
+        System.arraycopy(array, 0, newArray, 0, size);
+        array = newArray;
+    }
+
+    public String showList() {
+        StringBuilder str = new StringBuilder();
+        str.append("[");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i + 1] == null) {
+                str.append(array[i]);
+                break;
+            }
+            str.append(array[i]).append(", ");
+        }
+        return str.append("]").toString();
+    }
 
 }
