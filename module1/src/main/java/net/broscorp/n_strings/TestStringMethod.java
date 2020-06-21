4939c5508e09061a5be351c63b46a301e443178a
Yura Horb <g.y.andreevich@gmail.com>
2020-06-21 16:31:54 +0300

    1st commit for n_strings

---
+++ module1/src/main/java/net/broscorp/n_strings/TestStringMethod.java
@@ -0,0 +1,99 @@
+package net.broscorp.n_strings;
+
+import org.junit.jupiter.api.Test;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestStringMethod {
+
+    private String mainWords = "Hello World";
+
+    @Test
+    public void palindromeTest(){
+        assertTrue(isPalindrome("косок"));
+    }
+
+    @Test
+    public void testForRemovingTheWordWorld(){
+        assertEquals("Hello", leftOnlyHello(mainWords));
+    }
+
+    @Test
+    public void testForRemovingTheLetterL(){
+        assertEquals("Heo Word", deleteAllLettersLInString(mainWords));
+    }
+
+    @Test
+    public void testToReplaceTheLetterOToQ(){
+        assertEquals("Hellq Wqrld", replaceAllLettersOToQ(mainWords));
+    }
+
+    @Test
+    public void testsplitWordsAndLetters(){
+        String expectedString = "Слово Hello имеет - 5 букв. Слово World имеет - 5 букв. Количество слов - 2.";
+        assertEquals(expectedString, numberOfWordsAndNumberOfLettersInEachWord(mainWords));
+    }
+
+    @Test
+    public void testingSong(){
+        int numberOfBags = 2;
+        int numberOfCouplet = 1;
+        assertEquals(numberOfCouplet * 24, splitWords(songAboutBugs(numberOfBags, numberOfCouplet)).length);
+    }
+
+    private boolean isPalindrome(String s){
+        int leftChar = 0;
+        int rightChar = s.length() - 1;
+        while(leftChar < rightChar){
+            if(s.charAt(leftChar) != s.charAt(rightChar))
+                return false;
+            leftChar++;
+            rightChar--;
+        }
+        return true;
+    }
+
+    private String leftOnlyHello(String string){
+        return string.replace("World", "").trim();
+    }
+
+    private String deleteAllLettersLInString(String string){
+        return string.replace("l", "");
+    }
+
+    private String replaceAllLettersOToQ(String string){
+        return string.replace("o", "q");
+    }
+
+    private String numberOfWordsAndNumberOfLettersInEachWord(String string){
+        StringBuilder result = new StringBuilder();
+        for (String str : splitWords(string)){
+            result.append("Слово ").append(str).append(" имеет - ").append(str.length()).append(" букв. ");
+        }
+        return result.append("Количество слов - ").append(splitWords(string).length).append(".").toString();
+    }
+
+    private String songAboutBugs(int numberOfBags, int numberOfCouplet){
+        StringBuilder song = new StringBuilder();
+        Random random = new Random();
+        for(int i = 0; i < numberOfCouplet; i++){
+            int m = 0;
+            if(numberOfBags == 0 || numberOfBags < 0)
+                m =0;
+            m = numberOfBags - 10 + random.nextInt(20);
+            song.append(numberOfBags).append(" little bugs in the code,\n")
+                    .append(numberOfBags).append(" little bugs in the code.\n")
+                    .append("Take one down, patch it around ").append(m).append(" little bugs in the code.\n");
+            numberOfBags = m;
+        }
+        return song.toString();
+    }
+
+    private String [] splitWords(String string){
+        return string.replace(", ", " ")
+                .replace(",", " ")
+                .replace(".\n", " ")
+                .split(" ");
+    }
+}
