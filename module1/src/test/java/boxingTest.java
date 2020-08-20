791910304ff8388f4bc287a932df5159962c408b
Yulian Belov <belovyulian@gmail.com>
2020-08-20 12:22:57 +0300

    commit

---
+++ module1/src/test/java/boxingTest.java
@@ -0,0 +1,21 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class boxingTest {
+
+  Integer a = new Integer(4);
+  Integer b = new Integer(4);
+
+
+  @Test
+  public void falseTest() {
+    assertFalse(a == b);
+  }
+
+  @Test
+  public void trueTest() {
+    assertTrue(a.equals(b));
+  }
+}
