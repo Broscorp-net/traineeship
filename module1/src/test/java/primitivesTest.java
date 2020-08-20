c5e3a253e297de33d09d602d0092e59680a47dc1
Yulian Belov <belovyulian@gmail.com>
2020-08-20 17:33:03 +0300

    primitives

---
+++ module1/src/test/java/primitivesTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class primitivesTest {
+
+
+  @Test
+  public void intMaxValueSUm() {
+    int a = Integer.MAX_VALUE;
+    int b = Integer.MAX_VALUE;
+
+    assertTrue(a + b < 0);
+  }
+
+  @Test
+  public void longToInt() {
+    long a = Long.MAX_VALUE;
+    int b = (int) a;
+
+    assertNotEquals(a, b);
+  }
+
+  @Test
+  public void floatToDouble() {
+    float a = 10.60f;
+    double b = a;
+
+    assertNotEquals(10.60, b);
+  }
+
+  @Test
+  public void operationWithFloatingPoint() {
+    double a = 2.0 - 1.1;
+
+    assertNotEquals(0.9, a);
+  }
+
+}
