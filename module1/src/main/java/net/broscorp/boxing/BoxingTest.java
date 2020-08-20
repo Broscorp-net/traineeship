6b726d56443ea3a11c13a8443bf1ddd7d05b025c
Olena Haladzhii <marvintikln@gmail.com>
2020-08-21 00:27:37 +0300

    boxing

---
+++ module1/src/main/java/net/broscorp/boxing/BoxingTest.java
@@ -0,0 +1,30 @@
+package net.broscorp.boxing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class BoxingTest {
+  Integer first = 1234;
+  Integer second = 1234;
+
+  @Test
+  public void compareIntegerWithEqualsSign() {
+    assertFalse(first == second);
+  }
+
+  @Test
+  public void compareIntegerWithEqual() {
+    assertTrue(first.equals(second));
+  }
+
+  @Test
+  public void integerBoxing() {
+    Integer fromInt = Integer.valueOf(1234);
+    int fromInteger = fromInt;
+    int intValue = fromInt.intValue();
+    assertEquals(fromInteger, intValue);
+  }
+}
