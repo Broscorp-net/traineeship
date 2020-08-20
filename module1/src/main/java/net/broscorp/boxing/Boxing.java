647dbd7435222ccda93f7dde83a2f67d4bec6ff7
Yulian Belov <belovyulian@gmail.com>
2020-08-20 11:43:04 +0300

    commit

---
+++ module1/src/main/java/net/broscorp/boxing/Boxing.java
@@ -0,0 +1,12 @@
+package net.broscorp.boxing;
+
+public class Boxing {
+
+  public static void main(String[] args) {
+    Integer a = 4;
+    Integer b = new Integer(4);
+
+    System.out.println(a == b);
+    System.out.println(a.equals(b));
+  }
+}
