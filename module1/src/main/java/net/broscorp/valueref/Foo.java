a3dea7773edfe98aa4659c4458a6cb5e9c69f7fc
Adik2344 <adik2344@gmail.com>
2020-08-21 16:41:34 +0300

    checkstyle end

--- module1/src/main/java/net/broscorp/valueref/Foo.java
+++ module1/src/main/java/net/broscorp/valueref/Foo.java
@@ -2,9 +2,9 @@ package net.broscorp.valueref;
 
 import java.util.List;
 
-public class Foo {
+public class Foo{
 
-  public void foo(int i, List<String> s) {
+  public void foo(int i, List<String> s){
     s.add("d");
     i += 15;
   }
