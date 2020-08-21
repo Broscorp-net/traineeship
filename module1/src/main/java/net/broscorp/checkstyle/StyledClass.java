75bb1f17d56ab76626af680d3b77bf66038636fd
Aleksandr <iilliinnpro@gmail.com>
2020-08-22 00:37:48 +0300

    boxing task v5

--- module1/src/main/java/net/broscorp/checkstyle/StyledClass.java
+++ module1/src/main/java/net/broscorp/checkstyle/StyledClass.java
@@ -1,15 +1,8 @@
 package net.broscorp.checkstyle;
 
 public class StyledClass {
-<<<<<<< HEAD
-    public static void MAIN(String[] args) {
-        System.out.println ("Hello checkstyle");
-    }
-}
-=======
 
   public static void main(String[] args) {
     System.out.println("Hello checkstyle");
   }
 }
->>>>>>> c653f5427fcca52a3cfece414ecf7fa07c4b8fbe
