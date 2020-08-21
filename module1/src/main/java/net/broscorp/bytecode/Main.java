254051aedfb9882de340e42ced24049b612081c9
Nikita <32339836+nacenik@users.noreply.github.com>
2020-08-21 18:20:13 +0300

    Bytecode

--- module1/src/main/java/net/broscorp/bytecode/Main.java
+++ module1/src/main/java/net/broscorp/bytecode/Main.java
@@ -1,23 +1,34 @@
 package net.broscorp.bytecode;
 /**
+ * bytecode task.
  * @author Nikita Oleksin
+ * @version 1.0
  */
+
 public class Main {
-
-    public static void main(String[] args) {
-        int i = 2;
-        while(i++<8){
-            System.out.println("i = " + i);
-        }
-
-        i = print(5545);
-
-        System.out.println("new i = " + i);
-        System.out.println("Hello World! by Nikita");
+  /**
+   * main method.
+   * @param args - params
+   */
+  public static void main(String[] args) {
+    int i = 2;
+    while (i++ < 8) {
+      System.out.println("i = " + i);
     }
 
-    public static int print(int tmp){
-        tmp += 10;
-        return tmp;
-    }
+    i = print(5545);
+
+    System.out.println("new i = " + i);
+    System.out.println("Hello World! by Nikita");
+  }
+
+  /**
+   * print method.
+   * @param tmp - user value
+   * @return int
+   */
+  public static int print(int tmp) {
+    tmp += 10;
+    return tmp;
+  }
 }
