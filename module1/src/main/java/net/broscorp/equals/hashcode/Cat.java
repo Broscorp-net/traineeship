73c65921a81c64c06a4a16d84695ffc730d61666
MentTea <mentteabags@gmail.com>
2020-08-21 19:08:34 +0300

    completed the fifth task "Hashcode"

---
+++ module1/src/main/java/net/broscorp/equals/hashcode/Cat.java
@@ -0,0 +1,50 @@
+package net.broscorp.equals.hashcode;
+
+import java.util.Objects;
+
+public class Cat {
+
+  String name;
+  String breed;
+  int age;
+
+  /** Constructor of the Cat class.
+   *
+   * @param name  of the Cat (not NULL)
+   * @param breed of the Cat (not NULL)
+   * @param age   How many years does he live
+   */
+  public Cat(String name, String breed, int age) {
+    this.name = name;
+    this.breed = breed;
+    this.age = age;
+  }
+
+  @Override
+  public String toString() {
+    return "Animal{"
+        + "name='" + name + '\''
+        + ", breed='" + breed + '\''
+        + ", age=" + age
+        + '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Cat cat = (Cat) o;
+    return age == cat.age
+        && name.equals(cat.name)
+        && breed.equals(cat.breed);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(name, breed, age);
+  }
+}
---
+++ module1/src/main/java/net/broscorp/equals/hashcode/CatTest.java
@@ -0,0 +1,66 @@
+package net.broscorp.equals.hashcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class CatTest {
+
+  static List<Cat> cats = new ArrayList<>();
+
+  /**
+   * Methods with this annotation are run before each test.
+   * Let's add some objects to our list
+   */
+  @BeforeAll
+  public static void fillingList() {
+    cats = Stream.of(
+        new Cat("Tom", "British", 4),
+        new Cat("Tom", "British", 4),
+        new Cat("Luci", "American", 5),
+        new Cat("Angel", "Russian", 2))
+        .collect(Collectors.toList());
+  }
+
+  @Test
+  public void equalsHashTest() {
+    assertEquals(cats.get(0).hashCode(), cats.get(1).hashCode());
+  }
+
+  @Test
+  public void notEqualsHashTest() {
+    assertNotEquals(cats.get(0).hashCode(), cats.get(2).hashCode());
+  }
+
+  @Test
+  public void equalsTest() {
+    assertTrue(cats.get(0).equals(cats.get(1)));
+  }
+
+  @Test
+  public void notEqualsTest() {
+    assertFalse(cats.get(0).equals(cats.get(2)));
+  }
+
+  @Test
+  public void findTheSameObjectsForEqualsHashCodes() {
+
+    for (int i = 0; i < cats.size() - 1; i++) {
+      Cat first = cats.get(i);
+      Cat second = cats.get(i + 1);
+      if (first.hashCode() == second.hashCode() && !first.equals(second)) {
+        System.out.println("Вот объект " + first + " , вот объект " + second
+            + " , они разные, но их хеш сопадает");
+      }
+    }
+  }
+
+}
