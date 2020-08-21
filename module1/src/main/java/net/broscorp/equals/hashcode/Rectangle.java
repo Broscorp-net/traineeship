5a764097a96e6d4cd9464a9a67ca85829677c9fa
Yulian Belov <belovyulian@gmail.com>
2020-08-21 14:57:38 +0300

    valueref

---
+++ module1/src/main/java/net/broscorp/equals/hashcode/Rectangle.java
@@ -0,0 +1,38 @@
+package net.broscorp.equals.hashcode;
+
+public class Rectangle {
+
+  int height;
+  int width;
+
+  public Rectangle(int height, int width) {
+    this.height = height;
+    this.width = width;
+  }
+
+  @Override
+  public String toString() {
+    return "Rectangle{" +
+        "height=" + height +
+        ", width=" + width +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Rectangle rectangle = (Rectangle) o;
+    return (height == rectangle.height && width == rectangle.width) ||
+        (height == rectangle.width && width == rectangle.height);
+  }
+
+  @Override
+  public int hashCode() {
+    return height * width;
+  }
+}
