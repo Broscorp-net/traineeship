4783b3a4c9a8830d8a5fb44f45e90ef7390397e0
dertosp <dertosp@gmail.com>
2020-08-22 19:17:40 +0300

    add overflow/precision error tests for floating-point types

--- module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
+++ module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
@@ -2,6 +2,8 @@ package net.broscorp.primitives;
 
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PrimitivesTest {
@@ -48,6 +50,81 @@ public class PrimitivesTest {
 
     assertEquals(Byte.MIN_VALUE, maxByteOverflowResult);
     assertEquals(Byte.MAX_VALUE, minByteOverflowResult);
+  }
+
+  @Test
+  public void longToIntConversion() {
+    long numLong = 100;
+    int numInt;
+
+    numInt = (int)numLong;
+
+    assertEquals(100, numLong);
+    assertEquals(100, numInt);
+    assertEquals(numLong, numInt);
+  }
+
+  @Test
+  public void longToIntConversionWithOverflow() {
+    int numInt;
+    long numLong = (long)Integer.MAX_VALUE + 1;
+
+    numInt = (int)numLong;
+
+    assertNotEquals(numInt, numLong);
+    assertNotEquals(Integer.MIN_VALUE, numLong);
+    assertEquals(Integer.MIN_VALUE, numInt);
+  }
+
+  @Test
+  public void floatToDoubleConversion() {
+    double numD;
+    float numF = 0.1234567f;
+
+    numD = numF;
+
+    assertEquals(numF, numD);
+  }
+
+  @Test
+  public void floatToDoubleConversionPrecisionError() {
+    double numD;
+    float numF = 3.2f;
+
+    numD = numF;
+
+    assertEquals(3.2f, numD);
+    assertNotEquals(3.2d, numD);
+  }
+
+  @Test
+  public void precisionErrorsDuringOperationsOverFloats() {
+    float addResult, subResult, divResult, mulResult;
+
+    addResult = 0.1f + 3.53f;
+    subResult = 3.84f - 1.2f;
+    divResult = 3.6f / 1.2f;
+    mulResult = 5.6f * 2.1f;
+
+    assertNotEquals(3.63f, addResult);
+    assertNotEquals(2.64f, subResult);
+    assertNotEquals(3f, divResult);
+    assertNotEquals(11.76f, mulResult);
+  }
+
+  @Test
+  public void precisionErrorsDuringOperationsOverDoubles() {
+    double addResult, subResult, divResult, mulResult;
+
+    addResult = 0.11d + 3.53d;
+    subResult = 3.84d - 1.2d;
+    divResult = 3.55d / 0.05d;
+    mulResult = 5.65d * 2.1d;
+
+    assertNotEquals(3.64d, addResult);
+    assertNotEquals(2.64d, subResult);
+    assertNotEquals(71d, divResult);
+    assertNotEquals(11.865d, mulResult);
   }
 
 }
