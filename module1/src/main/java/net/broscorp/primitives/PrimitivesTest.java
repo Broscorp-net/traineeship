07b7b2b2c49082d82e2bfbf24016df8bc855e79a
Nikita <32339836+nacenik@users.noreply.github.com>
2020-08-23 13:40:49 +0300

    primitives. fix checkstyle

--- module1/src/main/java/net/broscorp/primitives/PrimitivesTest.java
+++ module1/src/main/java/net/broscorp/primitives/PrimitivesTest.java
@@ -1,6 +1,7 @@
 package net.broscorp.primitives;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
