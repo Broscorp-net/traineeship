c6a54c8803e92c22db2c18266ed650e77620a3bd
Adik2344 <adik2344@gmail.com>
2020-08-24 13:13:54 +0300

    Primitives

--- module1/src/main/java/net/broscorp/generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/generics/MyCoolList.java
@@ -3,25 +3,25 @@ package net.broscorp.generics;
 import java.util.function.Function;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class MyCoolList{
+public class MyCoolList {
 
-  public void add(Object o){
+  public void add(Object o) {
     throw new NotImplementedException();
   }
 
-  public Object get(int index){
+  public Object get(int index) {
     throw new NotImplementedException();
   }
 
-  public Object remove(int index){
+  public Object remove(int index) {
     throw new NotImplementedException();
   }
 
-  public MyCoolList map(Function f){
+  public MyCoolList map(Function f) {
     throw new NotImplementedException();
   }
 
-  public int size(){
+  public int size() {
     throw new NotImplementedException();
   }
 
