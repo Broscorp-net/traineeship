dab48c1dd45146a65daed2e9d57aa725cd79c398
pesua <pesua.mail@gmail.com>
2020-08-24 14:45:17 +0300

    Test example

---
+++ module1/src/test/java/net/broscorp/MyTest.java
@@ -0,0 +1,14 @@
+package net.broscorp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyTest {
+
+    @Test
+    void test() {
+        Assertions.assertTrue(true);
+    }
+}
