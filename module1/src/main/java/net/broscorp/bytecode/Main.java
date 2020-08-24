03d971874eebd802c0dce944260d5721842c0447
Yulian Belov <belovyulian@gmail.com>
2020-08-24 15:48:25 +0300

    bytecode

---
+++ module1/src/main/java/net/broscorp/bytecode/Main.java
@@ -0,0 +1,23 @@
+package net.broscorp.bytecode;
+
+public class Main {
+
+
+  public static void main(String[] args) {
+
+    int a = 0;
+
+    while (a < 100) {
+      a++;
+    }
+
+    sayHello();
+
+  }
+
+  public static void sayHello() {
+
+    System.out.println("Hello!");
+  }
+
+}
