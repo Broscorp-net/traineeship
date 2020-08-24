f24802c9973be5c16cb7aab3da1a5065470ec1b4
QM <zdorenkos@gmail.com>
2020-08-24 21:56:45 +0300

    primitives

--- module1/src/test/java/broscorp/primitives/PrimitivesTest.java
+++ module1/src/test/java/broscorp/primitives/PrimitivesTest.java
@@ -1,6 +1,6 @@
-package net.broscorp.primitives;
+package broscorp.primitives;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,21 +11,21 @@ public class PrimitivesTest {
   public void overflowExample() {
     short number1 = (short) (Short.MAX_VALUE + 1);
     int number2 = Short.MAX_VALUE + 1;
-    assertEquals(number1, number2);
+    assertNotEquals(number1, number2);
   }
 
   @Test
   public void convertLongToInt() {
     long number1 = 123456789101112L;
     int number2 = (int) number1;
-    assertEquals(0, number2 - number1);
+    assertNotEquals(0, number2 - number1);
   }
 
   @Test
   public void convertFloatToDouble() {
-    float number1 = 123.123456789f;
-    double number2 = number1;
-    assertEquals(String.valueOf(number1), String.valueOf(number2));
+    float  number1 = 1.00000009999999999f;
+    double number2 = 1.00000009999999999;
+    assertNotEquals((double) number1, number2) ;
   }
 
   @Test
@@ -34,6 +34,6 @@ public class PrimitivesTest {
     for (int i = 1; i <= 1_000; i++) {
       number += 0.1111111111111111;
     }
-    assertEquals(111.11111111111111, number, 0);
+    assertNotEquals(111.11111111111111, number, 0);
   }
 }
