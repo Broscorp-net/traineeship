831dc27483093d8cd311885e7639e94e3c90120c
Gerzonianzes <ol.ccherednik@gmail.com>
2020-08-25 23:16:49 +0300

    Boxing redone

--- module1/src/main/java/net/broscorp/boxing/BoxingTest.java
+++ module1/src/main/java/net/broscorp/boxing/BoxingTest.java
@@ -11,12 +11,14 @@ public class BoxingTest {
     Assert.assertTrue(int1 == int2);
     //Минус сравнивания по "==" он сравнивает ссылку на обьекты а не их значение
   }
+
   @Test
   public void boxing() {
     int boxInt = 98;
     Integer integer = boxInt;
     Assert.assertSame(boxInt, integer);
   }
+
   @Test
   public void unBoxing() {
     Long lung = 88L;
