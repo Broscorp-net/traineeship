4a96c55df18869c52b8d73b24fe11b78dd675b46
Gerzonianzes <ol.ccherednik@gmail.com>
2020-08-25 23:05:22 +0300

    Boxing done

---
+++ module1/src/main/java/net/broscorp/boxing/BoxingTest.java
@@ -0,0 +1,26 @@
+package net.broscorp.boxing;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BoxingTest {
+  @Test
+  public void compareWrappersInt() {
+    Integer int1 =  new Integer(11);
+    Integer int2 = 11;
+    Assert.assertTrue(int1 == int2);
+    //Минус сравнивания по "==" он сравнивает ссылку на обьекты а не их значение
+  }
+  @Test
+  public void boxing() {
+    int boxInt = 98;
+    Integer integer = boxInt;
+    Assert.assertSame(boxInt, integer);
+  }
+  @Test
+  public void unBoxing() {
+    Long lung = 88L;
+    long unLong = lung;
+    Assert.assertSame(lung, unLong);
+  }
+}
