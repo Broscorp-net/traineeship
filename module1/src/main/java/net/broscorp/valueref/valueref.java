ac7b2699bb9c6b051373625680736e98f1b7ec4e
Adik2344 <adik2344@gmail.com>
2020-08-25 23:44:13 +0300

    add valueref.java

---
+++ module1/src/main/java/net/broscorp/valueref/valueref.java
@@ -0,0 +1,24 @@
+package net.broscorp.valueref;
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class TestValueRef {
+
+  private Foo someFoo = new Foo();
+  private int someValue = 10;
+  private List<String> list = new LinkedList<>();
+
+  @Test
+  public void testOfValueReflationUsingFooMethod() {
+    int expectedValue = someValue;
+    someFoo.foo(someValue, list);
+    assertEquals(expectedValue, someValue);
+    assertEquals("d", list.get(0));
+  }
+}
