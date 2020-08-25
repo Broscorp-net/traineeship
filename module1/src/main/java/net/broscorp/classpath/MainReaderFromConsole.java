91fadb3b7a469ce1d54668a3afde1c0d2e079576
MentTea <mentteabags@gmail.com>
2020-08-25 21:26:33 +0300

    completed the tenth task "classpath" fix 1.2

--- module1/src/main/java/net/broscorp/classpath/MainReaderFromConsole.java
+++ module1/src/main/java/net/broscorp/classpath/MainReaderFromConsole.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class MainReaderFromConsole {
 
+  /**
+   * Main method: Read from And write To.
+   */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     WriterToConsole writerToConsole = new WriterToConsole();
