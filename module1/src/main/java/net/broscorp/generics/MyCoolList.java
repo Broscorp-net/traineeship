58a04ddf93fd00ca954d6905f72e79248efd1677
Nikita <32339836+nacenik@users.noreply.github.com>
2020-08-26 18:08:58 +0300

    generics. Using simple array

--- module1/src/main/java/net/broscorp/generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/generics/MyCoolList.java
@@ -1,28 +1,61 @@
 package net.broscorp.generics;
 
+import java.util.Arrays;
 import java.util.function.Function;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class MyCoolList {
+public class MyCoolList<T extends Number> {
 
-  public void add(Object o) {
-    throw new NotImplementedException();
+  private T[] objectArray;
+  private int size;
+
+  public MyCoolList() {
+    size = 0;
+    objectArray = (T[]) new Number[size];
   }
 
-  public Object get(int index) {
-    throw new NotImplementedException();
+  public void add(T object) {
+    resize(++size);
+    objectArray[size - 1] = object;
   }
 
-  public Object remove(int index) {
-    throw new NotImplementedException();
+  public T get(int index) {
+    return objectArray[index];
   }
 
-  public MyCoolList map(Function f) {
-    throw new NotImplementedException();
+  /**
+   * remove method.
+   * @param index - index
+   */
+  public void remove(int index) {
+    if (index >= 0 && index < size) {
+      for (int i = index; i < size - 1; i++) {
+        objectArray[i] = objectArray[i + 1];
+      }
+      resize(--size);
+    } else {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+  }
+
+  /**
+   * map method.
+   * @param f - function
+   * @param <R> - type
+   * @return new CoolList
+   */
+  public <R extends Number> MyCoolList<R> map(Function f) {
+    MyCoolList<R> myCoolList = new MyCoolList<>();
+    for (T t: objectArray) {
+      myCoolList.add((R) f.apply(t));
+    }
+    return myCoolList;
   }
 
   public int size() {
-    throw new NotImplementedException();
+    return size;
   }
 
+  private void resize(int newSize) {
+    objectArray = Arrays.copyOf(objectArray, newSize);
+  }
 }
---
+++ module1/src/main/test/net/broscorp/generics/MyCoolListTest.java
@@ -0,0 +1,78 @@
+package net.broscorp.generics;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Function;
+
+class MyCoolListTest {
+
+  private Function<Integer, Long> function;
+  private MyCoolList<Integer> myCoolList;
+  private int size;
+
+  @BeforeEach
+  void setUp() {
+    function = integer -> integer.longValue() * 2;
+    myCoolList = new MyCoolList<>();
+    size = 20;
+
+    for (int i = 0; i < size; i++) {
+      myCoolList.add(i);
+    }
+  }
+
+  @Test
+  void addTest() {
+    Integer integer = 10000;
+    myCoolList.add(integer);
+
+    assertEquals(integer, myCoolList.get(myCoolList.size()-1));
+    assertEquals(size + 1, myCoolList.size());
+  }
+
+  @Test
+  void getTest() {
+    for (int i = size - 1; i >= 0; i--) {
+      assertEquals(i, myCoolList.get(i));
+    }
+  }
+
+  @Test
+  void getExceptionTest() {
+    assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+      myCoolList.get(100);
+    });
+  }
+
+  @Test
+  void removeTest() {
+    myCoolList.remove(10);
+    assertEquals(size - 1, myCoolList.size());
+  }
+
+  @Test
+  void badRemoveTest() {
+    assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
+      myCoolList.remove(200);
+    });
+  }
+
+  @Test
+  void mapTest() {
+    MyCoolList <Number> longMyCoolList = myCoolList.map(function);
+
+    for (int i = 0; i < longMyCoolList.size(); i++) {
+      assertTrue(longMyCoolList.get(i) instanceof Long);
+    }
+  }
+
+  @Test
+  void sizeTest() {
+    assertEquals(size, myCoolList.size());
+  }
+}
