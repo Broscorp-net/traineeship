539babca288efae8fcdd60efb95c3d77d00b3764
QM <zdorenkos@gmail.com>
2020-08-26 21:13:42 +0300

    exceptions

---
+++ module1/src/test/java/broscorp/exceptions/ExceptionsTest.java
@@ -0,0 +1,30 @@
+package broscorp.exceptions;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+public class ExceptionsTest {
+
+  @Test
+  void readFile() {
+    File file = new File("D:/test.txt");
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        System.out.println(line);
+        throw new IOException();
+      }
+    } catch (FileNotFoundException ex) {
+      System.out.println("Catch FileNotFoundException");
+    } catch (IOException e) {
+      System.out.println("Catch IOException");
+    } finally {
+      System.out.println("Finally here");
+    }
+  }
+}
+
