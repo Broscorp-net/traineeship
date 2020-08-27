e810a6aabb875ae5bd820d7b172d8a296ae6b340
NikitaBatalskiy <45694965+NikitaBatalskiy@users.noreply.github.com>
2020-08-27 20:58:25 +0200

    primitives

--- module1/src/test/java/net/broscorp/primitivesTest.java
+++ module1/src/test/java/net/broscorp/primitivesTest.java
@@ -2,9 +2,8 @@ package net.broscorp;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class primitivesTest {
 
