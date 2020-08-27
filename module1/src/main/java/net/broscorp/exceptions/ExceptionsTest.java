05618f9af5f86eb3aa9573c598c5f7632adad521
Olena Haladzhii <marvintikln@gmail.com>
2020-08-28 00:34:52 +0300

    exceptions

---
+++ module1/src/main/java/net/broscorp/exceptions/ExceptionsTest.java
@@ -0,0 +1,34 @@
+package net.broscorp.exceptions;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Test;
+
+public class ExceptionsTest {
+
+  @Test
+  public void readFileWithException() throws IOException {
+    try (Scanner scanner = new Scanner(new File("test.txt"))) {
+      while (scanner.hasNext()) {
+        System.out.println(scanner.nextLine());
+      }
+    } catch (IOException e) {
+      e.getMessage();
+    } finally {
+      System.out.println("Bye-Bye!");
+    }
+  }
+
+  @Test
+  public void catchParent() {
+    try {
+      throw new RuntimeException();
+    } catch (Exception e) {
+      assertTrue(e instanceof RuntimeException);
+    }
+  }
+}
