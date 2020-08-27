8a1cbe5e6e37e2ceee4ace91805f5c732c421b4e
Gerzonianzes <ol.ccherednik@gmail.com>
2020-08-27 22:44:52 +0300

    TestFoo done

---
+++ module1/src/main/java/net/broscorp/valueref/TestFoo.java
@@ -0,0 +1,24 @@
+package net.broscorp.valueref;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestFoo {
+
+  Foo clazz = new Foo();
+  int anInt = 20;
+  List<String> list = new ArrayList<>();
+
+  @Test
+  public void fooTest() {
+    int value = anInt;
+    clazz.foo(value, list);
+    Assert.assertSame(anInt, value);
+    //Примитивный тип не будет подвержен изменению так как в метод поступает его копия
+    Assert.assertTrue(list.contains("d"));
+    //А вот лист будет изменёт потому что пердаёт свою ссылку в метод
+  }
+}
