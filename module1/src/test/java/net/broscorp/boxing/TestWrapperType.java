cb706b4da990a67445244d86fc5737f07393829f
Yura Horb <g.y.andreevich@gmail.com>
2020-08-28 16:20:19 +0300

    Boxing - 1st commit.

---
+++ module1/src/test/java/net/broscorp/boxing/TestWrapperType.java
@@ -0,0 +1,33 @@
+package net.broscorp.boxing;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class TestWrapperType {
+
+  @Test
+  public void equalsBetweenWrapperType() {
+    Integer firstValue = new Integer(8);
+    Integer secondValue = new Integer(8);
+    assertFalse(firstValue == secondValue);
+  }
+
+  @Test
+  public void equalsBetweenWrapperTypeBoxing() {
+    Integer firstValue = 8;
+    Integer secondValue = 8;
+    assertTrue(firstValue == secondValue);
+  }
+
+  @Test
+  public void equalsBetweenWrapperTypeUnboxing() {
+    Integer firstValue = new Integer(8);
+    Integer secondValue = new Integer(8);
+    int firstUnboxingValue = firstValue;
+    int secondUnboxingValue = secondValue;
+    assertTrue(firstUnboxingValue == secondUnboxingValue);
+  }
+
+}
