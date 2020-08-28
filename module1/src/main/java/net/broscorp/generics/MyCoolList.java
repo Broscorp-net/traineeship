ff191d80da87c98bf32bea5d2f9146f6b818b40c
QM <zdorenkos@gmail.com>
2020-08-28 16:11:48 +0300

    generics

--- module1/src/main/java/net/broscorp/generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/generics/MyCoolList.java
@@ -1,30 +1,65 @@
-/*
 package net.broscorp.generics;
 
+import java.util.ArrayList;
+import java.util.concurrent.atomic.DoubleAccumulator;
+import java.util.concurrent.atomic.DoubleAdder;
 import java.util.function.Function;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class MyCoolList {
+public class MyCoolList<T extends Number> extends ArrayList<T> {
 
-  public void add(Object o) {
-    throw new NotImplementedException();
+  /**I don't understand, why checkstile demands this.
+   * @param t only Number supported.
+   */
+  @Override
+  public boolean add(T t) {
+    if (isWholeNumber(t)) {
+      super.add(t);
+      return true;
+    }
+    return false;
   }
 
-  public Object get(int index) {
-    throw new NotImplementedException();
+  @Override
+  public T get(int index) {
+    return super.get(index);
   }
 
-  public Object remove(int index) {
-    throw new NotImplementedException();
+  @Override
+  public T remove(int index) {
+    return super.remove(index);
   }
 
-  public MyCoolList map(Function f) {
-    throw new NotImplementedException();
+  /**
+   * I don't understand, why checkstile demands this.
+   */
+  public <C extends Number> MyCoolList<C> map(Function f) {
+    MyCoolList<C> myCoolList = new MyCoolList<>();
+    for (T t: this) {
+      myCoolList.add((C) f.apply(t));
+    }
+    return myCoolList;
   }
 
+  @Override
   public int size() {
-    throw new NotImplementedException();
+    return super.size();
   }
 
+  private boolean isWholeNumber(Object o) {
+    Number number;
+    try {
+      number = (Number) o;
+    } catch (ClassCastException ex) {
+      return false;
+    }
+
+    // отсеиваем всех наследников Number, которые не являются целыми числами
+    if (number instanceof Float
+        || number instanceof Double
+        || number instanceof DoubleAccumulator
+        || number instanceof DoubleAdder) {
+      return false;
+    }
+    return true;
+  }
 }
-*/
