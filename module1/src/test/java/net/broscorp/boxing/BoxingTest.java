7c95e9f281f05bd3f8b0c72898d8faf94e547d5d
NikitaBatalskiy <45694965+NikitaBatalskiy@users.noreply.github.com>
2020-08-28 19:11:32 +0200

    boxing

---
+++ module1/src/test/java/net/broscorp/boxing/BoxingTest.java
@@ -0,0 +1,33 @@
+package net.broscorp.boxing;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.Test;
+
+public class BoxingTest {
+  Integer integer1 = new Integer(5);
+  Integer integer2 = new Integer(5);
+
+  @Test
+  public void shouldBeFalseWhenCompareWrappersLikeThis() {
+    assertFalse(integer1 == integer2);
+  }
+
+  @Test
+  public void shouldBeTrueWhenCompareWrappersWithEquals() {
+    assertEquals(integer1, integer2);
+  }
+
+  @Test
+  public void boxing() {
+    int someInt = 5;
+    assertEquals(integer1, someInt);
+  }
+
+  @Test
+  public void unboxing() {
+    int someInt = 5;
+    assertEquals(someInt, integer1);
+  }
+}
