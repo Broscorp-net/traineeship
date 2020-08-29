b0d209573ab9bd53fd4fe43e28d5fea2c5a21d53
Yura Horb <g.y.andreevich@gmail.com>
2020-08-29 03:02:23 +0300

    Generics - 1st commit.

--- module1/src/main/java/net/broscorp/generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/generics/MyCoolList.java
@@ -1,28 +1,83 @@
 package net.broscorp.generics;
 
 import java.util.function.Function;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-public class MyCoolList {
+public class MyCoolList<T extends Number> {
+  //я знаю, что есть негласное правило на счет констант, но я не смог обойти CheckStyle
+  private final int defSize = 10;
+  private Object[] array = new Object[defSize];
+  private int size = 0;
 
-  public void add(Object o) {
-    throw new NotImplementedException();
+  /**
+   * Метод добавляющий T элемент.
+   */
+  public void add(T element) {
+    if (size == array.length) {
+      resize(array.length * 2);
+    }
+    array[size++] = element;
   }
 
-  public Object get(int index) {
-    throw new NotImplementedException();
+  /**
+   * Метод добавляющий T элемент.
+   */
+
+  public T get(int index) {
+    return (T) array[index];
   }
 
-  public Object remove(int index) {
-    throw new NotImplementedException();
+  /**
+   * Метод удаляющий элемент по индексу.
+   */
+
+  public void remove(int index) {
+    for (int i = index; i < size; i++) {
+      array[i] = array[i + 1];
+    }
+    array[size - 1] = null;
+    size--;
   }
 
-  public MyCoolList map(Function f) {
-    throw new NotImplementedException();
+  /**
+   * Метод конвертирующий с T в R объект (числового типа).
+   */
+
+  public <R extends Number> MyCoolList<R> map(Function f) {
+    MyCoolList<R> result = new MyCoolList<>();
+    for (Object ls : array) {
+      if (ls == null) {
+        break;
+      }
+      result.add((R) f.apply(ls));
+    }
+    return result;
   }
 
   public int size() {
-    throw new NotImplementedException();
+    return size;
+  }
+
+  private void resize(int newLength) {
+    Object[] newArray = new Object[newLength];
+    System.arraycopy(array, 0, newArray, 0, size);
+    array = newArray;
+  }
+
+  /**
+   * Выводит лист на экран.
+   */
+
+  public String showList() {
+    StringBuilder str = new StringBuilder();
+    str.append("[");
+    for (int i = 0; i < array.length; i++) {
+      if (array[i + 1] == null) {
+        str.append(array[i]);
+        break;
+      }
+      str.append(array[i]).append(", ");
+    }
+    return str.append("]").toString();
   }
 
 }
---
+++ module1/src/test/java/net/broscorp/generics/TestMyCoolList.java
@@ -0,0 +1,58 @@
+package net.broscorp.generics;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.function.Function;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestMyCoolList {
+
+  private Function<Integer, Double> intToDouble = integer -> integer.doubleValue();
+  private MyCoolList<Integer> integerList = new MyCoolList<>();
+
+  @BeforeEach
+  public void init() {
+    integerList.add(12);
+  }
+
+  @Test
+  public void addToList() {
+    integerList.add(47);
+    int expectedSize = 2;
+    assertEquals(expectedSize, integerList.size());
+  }
+
+  @Test
+  public void removeFromList() {
+    integerList.add(41);
+    integerList.remove(0);
+    int expectedSize = 1;
+    assertEquals(expectedSize, integerList.size());
+  }
+
+  @Test
+  public void getElementByIndexFromList() {
+    assertEquals(12, integerList.get(0));
+  }
+
+  @Test
+  public void getSizeFromList() {
+    integerList.add(18);
+    integerList.add(116);
+    integerList.add(1144);
+    assertEquals(4, integerList.size());
+  }
+
+  @Test
+  public void mapFromIntegerToDoubleList() {
+    integerList.add(78);
+    integerList.add(8);
+    integerList.add(38);
+    MyCoolList<Double> doubleList = integerList.map(intToDouble);
+    String expectedArray = "[12.0, 78.0, 8.0, 38.0]";
+    assertEquals(expectedArray, doubleList.showList());
+  }
+
+}
