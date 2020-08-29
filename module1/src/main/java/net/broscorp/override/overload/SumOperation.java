d7a8475cdfe49a3ac9aefdcb55b393449337160f
Yura Horb <g.y.andreevich@gmail.com>
2020-08-29 03:12:34 +0300

    Override and overload - 1st commit.

---
+++ module1/src/main/java/net/broscorp/override/overload/SumOperation.java
@@ -0,0 +1,19 @@
+package net.broscorp.override.overload;
+
+public class SumOperation implements MathOperation {
+
+  @Override
+  public double operation(double firstValue, double secondValue) {
+    return firstValue + secondValue;
+  }
+
+  @Override
+  public double operation(int firstValue, int secondValue) {
+    return (double) firstValue + secondValue;
+  }
+
+  @Override
+  public double operation(Integer firstValue, Integer secondValue) {
+    return (double) firstValue + secondValue;
+  }
+}
