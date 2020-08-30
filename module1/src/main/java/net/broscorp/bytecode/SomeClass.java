3fbd0a645958bd4e854d2c460876927f25807d26
NikitaBatalskiy <45694965+NikitaBatalskiy@users.noreply.github.com>
2020-08-30 21:18:46 +0200

    bytecode

---
+++ module1/src/main/java/net/broscorp/bytecode/SomeClass.java
@@ -0,0 +1,25 @@
+package net.broscorp.bytecode;
+
+public class SomeClass {
+  /**
+   * Constructor parameters.
+   *
+   * @param args args
+   */
+  public static void main(String[] args) {
+    int number = 0;
+    for (int i = 0; i < 100; i++) {
+      number++;
+    }
+    printSomeString();
+    System.out.println("Hello, new app!");
+  }
+
+  static void printSomeString() {
+    System.out.println("Some String!");
+  }
+
+  static String newMethod() {
+    return "newMethod";
+  }
+}
---
+++ module1/src/main/java/net/broscorp/bytecode/script.cmd
@@ -0,0 +1,4 @@
+javac -d . SomeClass.java
+java net.broscorp.bytecode.SomeClass
+javap -c net.broscorp.bytecode.SomeClass
+pause
