458b3809276caf8317125acdd5b18182e62747da
dertosp <dertosp@gmail.com>
2020-08-30 13:33:45 +0300

    use wrappers' valueOf method instead of constructor

--- module1/src/test/java/net/broscorp/boxing/WrappersTest.java
+++ module1/src/test/java/net/broscorp/boxing/WrappersTest.java
@@ -9,9 +9,9 @@ class WrappersTest {
 
   @Test
   void compareEqualValueSameReferenceWrappers() {
-    Integer i1 = new Integer(5);
+    Integer i1 = Integer.valueOf(5);
     Integer i2 = i1;
-    Boolean b1 = new Boolean(false);
+    Boolean b1 = Boolean.valueOf(false);
     Boolean b2 = b1;
     // https://docs.oracle.com/javase/specs/jls/se8/html/jls-5.html#jls-5.1.7
     Integer i3 = 127;
@@ -27,17 +27,14 @@ class WrappersTest {
 
   @Test
   void compareEqualValueDifferentReferenceWrappers() {
-    Integer i1 = new Integer(5);
-    Integer i2 = new Integer(5);
-    Boolean b1 = new Boolean(false);
-    Boolean b2 = new Boolean(false);
+    Integer i1 = Integer.valueOf(200);
+    Integer i2 = Integer.valueOf(200);
     // https://docs.oracle.com/javase/specs/jls/se8/html/jls-5.html#jls-5.1.7
     Integer i3 = 128;
     Integer i4 = 128;
 
     assertFalse(i1 == i2);
     assertFalse(i3 == i4);
-    assertFalse(b1 == b2);
   }
 
   @Test
@@ -50,7 +47,7 @@ class WrappersTest {
 
   @Test
   void explicitlyBoxUnbox() {
-    Integer intWrapper = new Integer(1000);
+    Integer intWrapper = Integer.valueOf(1000);
     int i = 1000;
 
     assertTrue(intWrapper.intValue() == i);
