760a70e04e9c484bd123869cd7d71bea877db3ab
Olena Haladzhii <marvintikln@gmail.com>
2020-08-30 15:16:05 +0300

    boxing

--- module1/src/test/java/BoxingTest.java
+++ module1/src/test/java/BoxingTest.java
@@ -1,5 +1,3 @@
-package net.broscorp.boxing;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
