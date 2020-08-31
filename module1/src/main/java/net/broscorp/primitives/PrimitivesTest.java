6d0235e8dfb56a63ccd81f40e5af6f6ea1bbb1e3
Gerzonianzes <ol.ccherednik@gmail.com>
2020-08-31 21:35:08 +0300

    Primitives done

---
+++ module1/src/main/java/net/broscorp/primitives/PrimitivesTest.java
@@ -0,0 +1,35 @@
+package net.broscorp.primitives;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PrimitivesTest {
+
+  @Test
+  public void overflowInt() {
+    int maxValue = Integer.MAX_VALUE;
+    int minValue = Integer.MIN_VALUE;
+    Assert.assertEquals(++maxValue, minValue);
+  }
+
+  @Test
+  public void longToInteger() {
+    long maxValueLong = Long.MAX_VALUE;
+    int expected = -1;
+    Assert.assertEquals(expected,(int)maxValueLong);
+  }
+
+  @Test
+  public void floatToDouble() {
+    float number = 3.143267581489987665f;
+    double expectedDoubleNumber = 3.143267581489987665;
+    Assert.assertSame(expectedDoubleNumber,number);
+  }
+
+  @Test
+  public void errorWithFloatingNumber() {
+    double number = 0.2 + 0.4;
+    double expected = 0.6;
+    Assert.assertSame(expected,number);
+  }
+}
