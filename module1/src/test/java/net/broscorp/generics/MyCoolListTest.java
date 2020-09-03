17137f5d2288554bb90b147651b03f4772700465
QM <zdorenkos@gmail.com>
2020-09-04 01:38:04 +0300

    checkstyle fix

--- module1/src/test/java/net/broscorp/generics/MyCoolListTest.java
+++ module1/src/test/java/net/broscorp/generics/MyCoolListTest.java
@@ -1,6 +1,8 @@
 package net.broscorp.generics;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.ArrayList;
 import java.util.function.Function;
