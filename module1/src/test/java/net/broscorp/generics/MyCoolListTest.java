3231ce9a346c06bf73d008c9e196d72c4c4ed380
QM <zdorenkos@gmail.com>
2020-09-04 01:35:09 +0300

    add MyCoolList in branch generics

---
+++ module1/src/test/java/net/broscorp/generics/MyCoolListTest.java
@@ -0,0 +1,58 @@
+package net.broscorp.generics;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.function.Function;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MyCoolListTest {
+
+  MyCoolList<Number> myCoolList;
+
+  @BeforeEach
+  void init() {
+    myCoolList = new MyCoolList();
+    myCoolList.add(1);
+    myCoolList.add(2);
+    myCoolList.add(3);
+  }
+
+  @Test
+  void add() {
+    assertTrue(myCoolList.add(1000));
+    assertTrue(myCoolList.add((short) 1000));
+    assertTrue(myCoolList.add(1213484687L));
+    assertTrue(myCoolList.add(100.00));
+    assertTrue(myCoolList.add(100.00f));
+  }
+
+  @Test
+  void get() {
+    Number expect = 2;
+    assertEquals(expect, myCoolList.get(1));
+  }
+
+  @Test
+  void remove() {
+    myCoolList.remove(0);
+    assertFalse(myCoolList.contains(1));
+  }
+
+  @Test
+  void map() {
+    Function<Number, Long> convert = x -> x.longValue();
+    ArrayList<Long> newList = myCoolList.map(convert);
+    for (Long l : newList) {
+      assertTrue(l instanceof Long);
+    }
+  }
+
+  @Test
+  void size() {
+    int expect = 3;
+    assertTrue(myCoolList.size() == expect);
+  }
+}
