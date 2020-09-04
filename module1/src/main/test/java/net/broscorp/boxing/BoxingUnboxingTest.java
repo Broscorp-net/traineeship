f129f17bc01567cdcc3abaf4eea82d70aa2ed276
MentTea <mentteabags@gmail.com>
2020-09-04 16:44:09 +0300

    fix 1.4 "boxing"

--- module1/src/main/test/java/net/broscorp/boxing/BoxingUnboxingTest.java
+++ module1/src/main/test/java/net/broscorp/boxing/BoxingUnboxingTest.java
@@ -1,21 +1,32 @@
 package net.broscorp.boxing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class BoxingUnboxingTest {
 
+  /**
+   * Идея теста в том, чтобы создать две примитивных переменных,
+   * после чего оборачиваем их в Integer. И сравниваем уже непосредственно
+   * два объекта. Уже зная, что мы не выделяем для них разные ссылки, мы
+   * просто сравним два одинаковых объекта, в которых хранятся одинаковые данные.
+   * Ожидаем True.
+   */
   @Test
   public void boxingTest() {
-    int number = 1;
-    List<Integer> list = new ArrayList<>();
-    list.add(number);
-    assertEquals(1, list.get(number));
+    int firstNumber = 1;
+    int secondNumber = 1;
+    Integer firstInteger = firstNumber;
+    Integer secondInteger = secondNumber;
+    assertTrue(firstInteger == secondInteger);
   }
 
+  /**
+   * В этом тесте, вы записываем два объекта Integer, используя downcast.
+   * Таким образом, мы получаем примитивны тип данных int. Ожидаем True.
+   */
   @Test
   public void unboxingTest() {
     Integer firstNumber = 5;
--- module1/src/main/test/java/net/broscorp/boxing/ComparisonWrappersTest.java
+++ module1/src/main/test/java/net/broscorp/boxing/ComparisonWrappersTest.java
@@ -7,6 +7,11 @@ import org.junit.jupiter.api.Test;
 
 public class ComparisonWrappersTest {
 
+  /**
+   * В этом тесте мы создаём два новых объекта, отдельно для каждого выделяем память
+   * и создавая новую ссылку. При сравнивании через ==, мы сравниваем не содержание объекта,
+   * а его ссылки, так как мы создаём две новых ссылки, то и возвращать должно False.
+   */
   @Test
   public void comparisonWithNewObjectsTest() {
     Integer firstNumber = new Integer(7);
@@ -14,6 +19,11 @@ public class ComparisonWrappersTest {
     assertFalse(firstNumber == secondNumber);
   }
 
+  /**
+   * Как и в случае с прошлым тестом мы сравниваем два объекта, только в этот раз
+   * мы не выделяем память отдельно, и не создаём новых ссылок, в этом случае мы сравниваем
+   * два одинаковых объекта с одинаковым наполнением, ожидаем True
+   */
   @Test
   public void comparisonObjectsTest() {
     Integer firstNumber = 7;
