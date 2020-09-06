ce69da15d65141af7361ea7730b7f498f71de988
dertosp <dertosp@gmail.com>
2020-09-06 17:35:21 +0300

    add a Javadoc comment

--- module1/src/main/java/net/broscorp/classpath/Reader.java
+++ module1/src/main/java/net/broscorp/classpath/Reader.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class Reader {
 
+  /**
+   * Entry point.
+   * @param args Command-line arguments.
+   */
   public static void main(String[] args) {
     try (Scanner scanner = new Scanner(System.in)) {
       System.out.print("Waiting for input: ");
