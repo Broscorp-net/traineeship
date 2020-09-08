ea1d537b339169cf2b600155fa80232562f87c87
dertosp <dertosp@gmail.com>
2020-09-08 22:54:33 +0300

    add tests for Iterable implementation of MyCoolList

--- module1/src/test/java/net/broscorp/generics/MyCoolListTest.java
+++ module1/src/test/java/net/broscorp/generics/MyCoolListTest.java
@@ -6,6 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
 import org.junit.jupiter.api.Test;
 
 public class MyCoolListTest {
@@ -167,6 +171,161 @@ public class MyCoolListTest {
 
     assertFalse(newList == list);
     assertEquals(0, newList.size());
+  }
+
+  @Test
+  void iterateOverList() {
+    final MyCoolList<Integer> list = new MyCoolList<>();
+    final List<Integer> arrList = new ArrayList<>();
+
+    for (int i = 0; i < 10; i++) {
+      list.add(i * 3);
+    }
+
+    for (Integer elem : list) {
+      arrList.add(elem);
+    }
+
+    assertEquals(10, list.size());
+    assertEquals(arrList.size(), list.size());
+    assertEquals(arrList.get(9), list.get(9));
+    assertEquals(arrList.get(5), list.get(5));
+  }
+
+  @Test
+  void iterateOverEmptyList() {
+    MyCoolList<Integer> list = new MyCoolList<>();
+    int acc = 0;
+
+    for (Integer elem : list) {
+      acc += elem;
+    }
+
+    assertEquals(0, acc);
+  }
+
+  @Test
+  void forEachWithNullFunc() {
+    MyCoolList<Integer> list = new MyCoolList<>();
+
+    for (int i = 0; i < 10; i++) {
+      list.add(i * 3);
+    }
+
+    assertThrows(NullPointerException.class, () -> list.forEach(null));
+  }
+
+  @Test
+  void forEachWithCorrectFunc() {
+    MyCoolList<Integer> list = new MyCoolList<>();
+    List<Integer> arrList = new ArrayList<>();
+
+    for (int i = 0; i < 10; i++) {
+      list.add(i * 3);
+    }
+
+    list.forEach(arrList::add);
+
+    assertEquals(arrList.size(), list.size());
+    assertEquals(arrList.get(3), list.get(3));
+  }
+
+  @Test
+  void compareMyListWithArrListThroughIterator() {
+    MyCoolList<Integer> myList = new MyCoolList<>();
+    List<Integer> arrList = new ArrayList<>();
+
+    for (int i = 0; i < 10; i++) {
+      myList.add(i * 3);
+      arrList.add(i * 3);
+    }
+
+    Iterator<Integer> it1 = myList.iterator();
+    Iterator<Integer> it2 = arrList.iterator();
+
+    while (it1.hasNext() && it2.hasNext()) {
+      assertEquals(it1.next(), it2.next());
+    }
+
+    assertEquals(myList.size(), arrList.size());
+  }
+
+  @Test
+  void iteratorRemoveWithoutCallingNext() {
+    MyCoolList<Integer> myList = new MyCoolList<>();
+    myList.add(3);
+
+    Iterator<Integer> it = myList.iterator();
+
+    assertThrows(IllegalStateException.class, it::remove);
+  }
+
+  @Test
+  void iteratorRemoveTwoTimesInARow() {
+    MyCoolList<Integer> myList = new MyCoolList<>();
+    myList.add(3);
+    myList.add(3);
+    myList.add(3);
+
+    Iterator<Integer> it = myList.iterator();
+    it.next();
+    it.remove();
+
+    assertThrows(IllegalStateException.class, it::remove);
+  }
+
+  @Test
+  void iteratorRemoveCorrectly() {
+    MyCoolList<Integer> myList = new MyCoolList<>();
+    myList.add(3);
+    myList.add(-1);
+    myList.add(0);
+    Iterator<Integer> it = myList.iterator();
+
+    it.next();
+    it.remove();
+    it.next();
+    it.remove();
+
+    assertEquals(1, myList.size());
+    assertEquals(0, myList.get(0));
+    assertThrows(IndexOutOfBoundsException.class, () -> myList.get(1));
+  }
+
+  @Test
+  void iteratorRemoveOnEmptyList() {
+    MyCoolList<Integer> myList = new MyCoolList<>();
+    Iterator<Integer> it = myList.iterator();
+
+    assertThrows(IllegalStateException.class, it::remove);
+  }
+
+  @Test
+  void iteratorNextOnEmptyList() {
+    MyCoolList<Integer> myList = new MyCoolList<>();
+    Iterator<Integer> it = myList.iterator();
+
+    assertThrows(NoSuchElementException.class, it::next);
+  }
+
+  @Test
+  void iteratorNextOnLastElement() {
+    MyCoolList<Integer> myList = new MyCoolList<>();
+    myList.add(3);
+
+    Iterator<Integer> it = myList.iterator();
+    it.next();
+
+    assertThrows(NoSuchElementException.class, it::next);
+  }
+
+  @Test
+  void iteratorHasNextOnEmptyList() {
+    MyCoolList<Integer> myList = new MyCoolList<>();
+
+    Iterator<Integer> it = myList.iterator();
+
+    assertFalse(it.hasNext());
   }
 
 }
