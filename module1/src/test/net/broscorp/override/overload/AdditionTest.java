474ab7bb37f4bb178a25aa79981eabe19c4b176f
QM <zdorenkos@gmail.com>
2020-09-09 00:14:08 +0300

    fix test methods naming

--- module1/src/test/net/broscorp/override/overload/AdditionTest.java
+++ module1/src/test/net/broscorp/override/overload/AdditionTest.java
@@ -7,33 +7,39 @@ import org.junit.jupiter.api.Test;
 class AdditionTest {
 
   Addition addition = new Addition();
-  double d1 = 10.0;
-  double d2 = 100.0;
-  int i1 = 10;
-  int i2 = 100;
-  double expect = 110.0;
-
 
   @Test
-  void calculate() {
+  void doublePrimitivesAdditionTest() {
+    double d1 = 10.0;
+    double d2 = 100.0;
+    double expect = 110.0;
     double result = addition.calculate(d1, d2);
     assertEquals(expect, result);
   }
 
   @Test
-  void testCalculate() {
+  void doublePrimitivesAdditionWithCastingTest() {
+    double d1 = 10.0;
+    double d2 = 100.0;
+    double expect = 110.0;
     double result = addition.calculate((Double) d1, (Double) d2);
     assertEquals(expect, result);
   }
 
   @Test
-  void testCalculate1() {
+  void intPrimitivesAdditionTest() {
+    int i1 = 10;
+    int i2 = 100;
+    double expect = 110.0;
     double result = addition.calculate(i1, i2);
     assertEquals(expect, result);
   }
 
   @Test
-  void testCalculate2() {
+  void intPrimitivesAdditionWithCastingTest() {
+    int i1 = 10;
+    int i2 = 100;
+    double expect = 110.0;
     double result = addition.calculate((Integer) i1, (Integer) i2);
     assertEquals(expect, result);
   }
@@ -41,7 +47,8 @@ class AdditionTest {
   // вызывается calculate(double d1, double d2)
   // если возможно идет преобразование к примитивам, а не оберткам
   @Test
-  void testCalculate3() {
+  void doubleAndlongPrimitivesAdditionTest() {
+    double expect = 110.0;
     double result = addition.calculate(10, 100L);
     assertEquals(expect, result);
   }
@@ -49,7 +56,8 @@ class AdditionTest {
   // не компилируется. java не может решить какой из аргументов нужно преобразовать
   // и какой в итоге метод вызвать
   /*  @Test
-  void testCalculate4() {
+  void intPrimitivesAdditionWithCastingOnlyOneParameterTest() {
+    double expect = 110.0;
     double result = addition.calculate((Integer) i1, i2);
     assertEquals(expect, result);
   }*/
--- module1/src/test/net/broscorp/override/overload/MultiplicationTest.java
+++ module1/src/test/net/broscorp/override/overload/MultiplicationTest.java
@@ -7,33 +7,39 @@ import org.junit.jupiter.api.Test;
 class MultiplicationTest {
 
   Multiplication multiplication = new Multiplication();
-  double d1 = 10.0;
-  double d2 = 2.0;
-  int i1 = 10;
-  int i2 = 2;
-  double expect = 20.0;
-
 
   @Test
-  void calculate() {
+  void doublePrimitivesMultiplicationTest() {
+    double d1 = 10.0;
+    double d2 = 2.0;
+    double expect = 20.0;
     double result = multiplication.calculate(d1, d2);
     assertEquals(expect, result);
   }
 
   @Test
-  void testCalculate() {
+  void doublePrimitivesMultiplicationWithCastingTest() {
+    double d1 = 10.0;
+    double d2 = 2.0;
+    double expect = 20.0;
     double result = multiplication.calculate((Double) d1, (Double) d2);
     assertEquals(expect, result);
   }
 
   @Test
-  void testCalculate1() {
+  void intPrimitivesMultiplicationTest() {
+    int i1 = 10;
+    int i2 = 2;
+    double expect = 20.0;
     double result = multiplication.calculate(i1, i2);
     assertEquals(expect, result);
   }
 
   @Test
-  void testCalculate2() {
+  void intPrimitivesMultiplicationWithCastingTest() {  double d1 = 10.0;
+    int i1 = 10;
+    int i2 = 2;
+    double expect = 20.0;
     double result = multiplication.calculate((Integer) i1, (Integer) i2);
     assertEquals(expect, result);
   }
