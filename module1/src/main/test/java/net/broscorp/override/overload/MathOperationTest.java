1195f306f6b28c3ab75562b443b69f78eaee8112
MentTea <mentteabags@gmail.com>
2020-09-08 15:44:01 +0300

    fix 1.2 "override.overload"

--- module1/src/main/test/java/net/broscorp/override/overload/MathOperationTest.java
+++ module1/src/main/test/java/net/broscorp/override/overload/MathOperationTest.java
@@ -12,10 +12,9 @@ public class MathOperationTest {
   /**
    * Эту аннотацию @Override Мы должны дать Java понять, что мы не ошиблись и не назвали метод
    * названием, которое уже есть, из-за невнимательности. Чтобы сказать, что мы знаем, что делаем и
-   * действительно хотим переопределить метод, нужно указать @Override явно.
-   * Таким образом, если вы совершите распространённую ошибку, например: - Опечатка в имени метода -
-   * Не правильно задаёте параметры То вы будете предупреждены об этом. Так же улучшает читаемость
-   * кода.
+   * действительно хотим переопределить метод, нужно указать @Override явно. Таким образом, если вы
+   * совершите распространённую ошибку, например: - Опечатка в имени метода - Не правильно задаёте
+   * параметры То вы будете предупреждены об этом. Так же улучшает читаемость кода.
    */
   @Test
   public void multiDoubleMethodDoubleTest() {
@@ -58,4 +57,52 @@ public class MathOperationTest {
     Integer second = 7;
     assertEquals(14, sumRealization.doubleMethod(first, second));
   }
+
+  /**
+   * Во время того как мы передаём, примитив "double" и обёртку, будет вызван метод, который
+   * принимает два примитива double.
+   */
+  @Test
+  public void sumDoubleMethodDoubleAndIntegerTest() {
+    double first = 7;
+    Integer second = 7;
+    assertEquals(14, sumRealization.doubleMethod(first, second));
+  }
+
+  /**
+   * Во время того как мы передаём, примитив "double" и обёртку, будет вызван метод, который
+   * принимает два примитива double.
+   */
+  @Test
+  public void multiDoubleMethodDoubleAndIntegerTest() {
+    double first = 7;
+    Integer second = 7;
+    assertEquals(49, multiRealization.doubleMethod(first, second));
+  }
+
+  /**
+   * Во время того как мы передаём, примитив "int" и обёртку, программа не понимает какую из
+   * реализаций interface ей брать, таким образом она путается между двумя методами , первый из
+   * которых принимает два Примитива Int, а второй две Обёртки Integer.
+   * Так как они по сути одинаковы.
+   */
+//  @Test
+//  public void sumDoubleMethodIntPlusIntegerTest() {
+//    int first = 7;
+//    Integer second = 7;
+//    assertEquals(14, sumRealization.doubleMethod(first, second));
+//  }
+
+  /**
+   * Во время того как мы передаём, примитив "int" и обёртку, программа не понимает какую из
+   * реализаций interface ей брать, таким образом она путается между двумя методами , первый из
+   * которых принимает два Примитива Int, а второй две Обёртки Integer.
+   * Так как они по сути одинаковы.
+   */
+//  @Test
+//  public void multiDoubleMethodIntPlusIntegerTest() {
+//    int first = 7;
+//    Integer second = 7;
+//    assertEquals(49, multiRealization.doubleMethod(first, second));
+//  }
 }
