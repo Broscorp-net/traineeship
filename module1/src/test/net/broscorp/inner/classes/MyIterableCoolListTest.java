950d8203fa2216bf3451aa5e03e3706ad731da5f
QM <zdorenkos@gmail.com>
2020-09-10 23:25:36 +0300

    add test, checking order after copy

--- module1/src/test/net/broscorp/inner/classes/MyIterableCoolListTest.java
+++ module1/src/test/net/broscorp/inner/classes/MyIterableCoolListTest.java
@@ -20,4 +20,20 @@ public class MyIterableCoolListTest {
     }
     assertEquals(result, expect);
   }
+
+  @Test
+  void checkCorrectOrderCopy() {
+    MyIterableCoolList<Number> myCoolList = new MyIterableCoolList<>();
+    myCoolList.add(10);
+    myCoolList.add(20d);
+    myCoolList.add(30L);
+    MyIterableCoolList<Number> myCoolListCopy = new MyIterableCoolList<>();
+    for (Number number : myCoolList) {
+      myCoolListCopy.add(number);
+    }
+    // так можно?
+    for (int i = 0; i < myCoolList.size(); i++) {
+      assertEquals(myCoolList.get(i), myCoolListCopy.get(i));
+    }
+  }
 }
