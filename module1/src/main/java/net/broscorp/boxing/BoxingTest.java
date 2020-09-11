2da2e0c26d2dc454685108d4869d45461eb6c5c4
Gerzonianzes <ol.ccherednik@gmail.com>
2020-09-11 17:08:37 +0300

    redone

--- module1/src/main/java/net/broscorp/boxing/BoxingTest.java
+++ module1/src/main/java/net/broscorp/boxing/BoxingTest.java
@@ -1,14 +1,14 @@
 package net.broscorp.boxing;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class BoxingTest {
   @Test
   public void compareWrappersInt() {
     Integer int1 =  new Integer(11);
     Integer int2 = 11;
-    Assert.assertTrue(int1 == int2);
+    Assertions.assertTrue(int1 == int2);
     //Минус сравнивания по "==" он сравнивает ссылку на обьекты а не их значение
   }
 
@@ -16,13 +16,13 @@ public class BoxingTest {
   public void boxing() {
     int boxInt = 98;
     Integer integer = boxInt;
-    Assert.assertSame(boxInt, integer);
+    Assertions.assertSame(boxInt, integer);
   }
 
   @Test
   public void unBoxing() {
     Long lung = 88L;
     long unLong = lung;
-    Assert.assertSame(lung, unLong);
+    Assertions.assertSame(lung, unLong);
   }
 }
