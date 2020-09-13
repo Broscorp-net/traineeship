e3325a6283305f22619bfe68d20f0f3e6f551a4b
Nikita <32339836+nacenik@users.noreply.github.com>
2020-09-13 19:27:21 +0300

    Used simple array in MyCoolList.

--- module1/src/main/java/net/broscorp/generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/generics/MyCoolList.java
@@ -1,25 +1,50 @@
 package net.broscorp.generics;
 
 import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.function.Function;
 
 public class MyCoolList<T extends Number> {
 
   private T[] objectArray;
   private int size;
+  private int pointer;
+  private static int i;
 
+  /**
+   * Constructor.
+   */
   public MyCoolList() {
-    size = 0;
+    size = 10;
+    pointer = 0;
     objectArray = (T[]) new Number[size];
   }
 
+  /**
+   * Add object to list.
+   * @param object - object;
+   */
   public void add(T object) {
-    resize(++size);
-    objectArray[size - 1] = object;
+    if (pointer + 1 >= size) {
+      size *= 1.5;
+      resize(size);
+    }
+    objectArray[pointer++] = object;
+
   }
 
+  /**
+   * get object by index.
+   * @param index - index
+   * @return T
+   */
   public T get(int index) {
-    return objectArray[index];
+    if (indexTest(index)) {
+      return objectArray[index];
+    }
+    throw new ArrayIndexOutOfBoundsException();
   }
 
   /**
@@ -27,11 +52,12 @@ public class MyCoolList<T extends Number> {
    * @param index - index
    */
   public void remove(int index) {
-    if (index >= 0 && index < size) {
-      for (int i = index; i < size - 1; i++) {
-        objectArray[i] = objectArray[i + 1];
+    if (indexTest(index)) {
+      T[] tmp = objectArray;
+      if (--pointer > index) {
+        System.arraycopy(tmp, index, tmp, index - 1, pointer - index);
       }
-      resize(--size);
+      tmp[pointer] = null;
     } else {
       throw new ArrayIndexOutOfBoundsException();
     }
@@ -45,17 +71,71 @@ public class MyCoolList<T extends Number> {
    */
   public <R extends Number> MyCoolList<R> map(Function f) {
     MyCoolList<R> myCoolList = new MyCoolList<>();
-    for (T t: objectArray) {
+    for (int i = 0; i < pointer; i++) {
+      T t = objectArray[i];
       myCoolList.add((R) f.apply(t));
     }
+
     return myCoolList;
   }
 
   public int size() {
-    return size;
+    return pointer;
+  }
+
+  private static void returnI() {
+    System.out.println(i);
   }
 
   private void resize(int newSize) {
     objectArray = Arrays.copyOf(objectArray, newSize);
   }
+
+  private boolean indexTest(int index) {
+    return index >= 0 && index <= pointer;
+  }
+
+  public Iterator<T> iterator() {
+    return new MyCoolListIterator();
+  }
+
+  /* в Java есть 2 типа вложенных классов:
+  * Inner class.
+  * Static nested class.
+  *
+  * Отличия между ними:
+  * 1. Static nested class не хранит ссылку на внешний класс.
+  * Inner class хранит ссылку на внешний класс.
+  * 2. Static nested class может существовать без объекта внешнего класса,но при обращении к нему
+  * необходимо указывать внешний класс.
+  * Inner class не может существовать без объекта внешнего класса,так же не может быть
+  * создан в статическом методе внешнего класса
+  * 3. Static nested class может обращаться только к статическим полям и методам внешнего класса.
+  * Inner class имеет доступ ко всем полям и методам внешнего класса.
+  * 4. Inner class не может содержать статические методы и переменные. Static nested class может
+  * содержать статические и нестатические переменные и методы
+  *   */
+
+  private class MyCoolListIterator implements Iterator<T> {
+
+    private int elementPointer;
+
+    @Override
+    public boolean hasNext() {
+      return elementPointer < pointer;
+    }
+
+    @Override
+    public T next() {
+      if (elementPointer >= pointer) {
+        throw new NoSuchElementException();
+      }
+      if (elementPointer >= objectArray.length) {
+        throw new ConcurrentModificationException();
+      }
+      return objectArray[elementPointer++];
+    }
+  }
 }
+
+
--- module1/src/main/test/net/broscorp/generics/MyCoolListTest.java
+++ module1/src/main/test/net/broscorp/generics/MyCoolListTest.java
@@ -7,6 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
 import java.util.function.Function;
 
 class MyCoolListTest {
@@ -74,5 +75,14 @@ class MyCoolListTest {
   @Test
   void sizeTest() {
     assertEquals(size, myCoolList.size());
+  }
+
+  @Test
+  void iteratorTest() {
+    int pointer = 0;
+    Iterator<Integer> integerIterator = myCoolList.iterator();
+    while (integerIterator.hasNext()){
+      assertEquals(integerIterator.next(), myCoolList.get(pointer++));
+    }
   }
 }
