43117b2652407e7ca0af020bae3f9084ac948e0b
NikitaBatalskiy <45694965+NikitaBatalskiy@users.noreply.github.com>
2020-09-14 15:23:14 +0200

    Revert "primitives" and "checkstyle"

--- module1/src/test/java/net/broscorp/boxing/BoxingTest.java
+++ module1/src/test/java/net/broscorp/boxing/BoxingTest.java
@@ -2,31 +2,43 @@ package net.broscorp.boxing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class BoxingTest {
-  Integer integer1 = new Integer(5);
-  Integer integer2 = new Integer(5);
 
   @Test
   public void shouldBeFalseWhenCompareWrappersLikeThis() {
+    Integer integer1 = new Integer(5);
+    Integer integer2 = new Integer(5);
     assertFalse(integer1 == integer2);
   }
 
   @Test
+  public void shouldBeTrueWhenCompareWrappersLikeThis() {
+    Integer integer1 = new Integer(5);
+    Integer integer2 = integer1;
+    assertTrue(integer1 == integer2);
+  }
+
+  @Test
   public void shouldBeTrueWhenCompareWrappersWithEquals() {
+    Integer integer1 = new Integer(5);
+    Integer integer2 = new Integer(5);
     assertEquals(integer1, integer2);
   }
 
   @Test
-  public void boxing() {
-    int someInt = 5;
+  public void autoboxing() {
+    Integer integer1 = new Integer(5);
+    int someInt = integer1;
     assertEquals(integer1, someInt);
   }
 
   @Test
   public void unboxing() {
+    Integer integer1 = new Integer(5);
     int someInt = 5;
     assertEquals(someInt, integer1);
   }
