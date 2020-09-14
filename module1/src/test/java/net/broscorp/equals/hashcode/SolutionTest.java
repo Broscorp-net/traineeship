29084d58112ca37e20ec1bb5eeada85751b4514e
Aleksander <sg.wofl@gmail.com>
2020-09-14 10:37:23 +0300

    fix mistakes

--- module1/src/test/java/net/broscorp/equals/hashcode/SolutionTest.java
+++ module1/src/test/java/net/broscorp/equals/hashcode/SolutionTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 public class SolutionTest {
 
   @Test
-  public void testHashcodeExcetedTrue() {
+  public void testHashcodeExcectedTrue() {
     Solution firstSol = new Solution(1, 2);
     Solution secondSol = new Solution(1, 2);
 
@@ -21,7 +21,7 @@ public class SolutionTest {
   }
 
   @Test
-  public void testEqualsExcetedTrue() {
+  public void testEqualsExcectedTrue() {
     Solution firstSol = new Solution(1, 2);
     Solution secondSol = new Solution(1, 2);
 
@@ -34,9 +34,13 @@ public class SolutionTest {
   }
 
   @Test
-  public void testFindSolutionWithSameHashcodeButNotEqualsExceptedTrue() {
+  public void testFindSolutionWithSameHashcodeButNotEqualsExcectedTrue() {
     List<Solution> solutions =
         Arrays.asList(new Solution(10, 20), new Solution(20, 10), new Solution(15, 15));
+
+    System.out.println("вот список разных объектов:" + solutions + " мы видем что они разные"
+        + " но их hashcode одинаковый " + solutions.get(0).hashCode() + " "
+        + solutions.get(1).hashCode() + " " + solutions.get(2).hashCode());
 
     for (int i = 0; i < solutions.size() - 1; i++) {
       Solution firstSol = solutions.get(i);
@@ -45,5 +49,52 @@ public class SolutionTest {
       Assertions.assertEquals(firstSol.hashCode(), secondSol.hashCode());
       Assertions.assertNotEquals(firstSol, secondSol);
     }
+  }
+  
+  @Test
+  public void testEqualsReflexivityExcectedIsTrue() {
+    Solution sol = new Solution(1, 2);
+    Assertions.assertTrue(sol.equals(sol));
+  }
+  
+  @Test
+  public void testEqualsWithNullExcectedIsFalse() {
+    Solution sol = new Solution(1, 2);
+    Assertions.assertFalse(sol.equals(null));
+  }
+  
+  @Test
+  public void testEqualsSymmetryExcectedIsTrue() {
+    Solution firstSol = new Solution(1, 2);
+    Solution secondSol = new Solution(1, 2);
+
+    Assertions.assertTrue(firstSol.equals(secondSol));
+    Assertions.assertTrue(secondSol.equals(firstSol));
+  }
+  
+  @Test
+  public void testEqualsTransitivityExcectedIsTrue() {
+    Solution firstSol = new Solution(1, 2);
+    Solution secondSol = new Solution(1, 2);
+    Solution thirdSol = new Solution(1, 2);
+
+    Assertions.assertTrue(firstSol.equals(secondSol));
+    Assertions.assertTrue(secondSol.equals(thirdSol));
+    Assertions.assertTrue(firstSol.equals(thirdSol));
+  }
+  
+  @Test
+  public void testEqualsConsistencyTrue() {
+    Solution firstSol = new Solution(1, 2);
+    Solution secondSol = new Solution(1, 2);
+    
+    for (int i = 0; i < 10; i++) {
+      Assertions.assertTrue(firstSol.equals(secondSol));
+    }
+    
+    secondSol.setFirstValue(10);
+    secondSol.setSecondValue(20);
+
+    Assertions.assertFalse(firstSol.equals(secondSol));
   }
 }
