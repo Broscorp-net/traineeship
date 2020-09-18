42d131fca7ab2004f963619b6ecbda861b57ef45
NikitaBatalskiy <45694965+NikitaBatalskiy@users.noreply.github.com>
2020-09-18 21:05:21 +0200

    removed javadoc

--- module1/src/main/java/net/broscorp/bytecode/SomeClass.java
+++ module1/src/main/java/net/broscorp/bytecode/SomeClass.java
@@ -1,11 +1,6 @@
 package net.broscorp.bytecode;
 
 public class SomeClass {
-  /**
-   * Constructor parameters.
-   *
-   * @param args args
-   */
   public static void main(String[] args) {
     int number = 0;
     for (int i = 0; i < 100; i++) {
