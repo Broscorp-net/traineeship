e7aeeaab91112cd6eb57e1dfbd5aab6c0d07a89c
NikitaBatalskiy <45694965+NikitaBatalskiy@users.noreply.github.com>
2020-09-24 11:34:08 +0200

    forgot the dot

--- module1/src/main/java/net/broscorp/bytecode/SomeClass.java
+++ module1/src/main/java/net/broscorp/bytecode/SomeClass.java
@@ -2,7 +2,7 @@ package net.broscorp.bytecode;
 
 public class SomeClass {
 
-  /** This javadoc is required by checkstyle */
+  /** This javadoc is required by checkstyle. */
   public static void main(String[] args) {
     int number = 0;
     for (int i = 0; i < 100; i++) {
