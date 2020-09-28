ccbbfd62461ed6ed7a7db18a4a26f829e07c477a
NikitaBatalskiy <45694965+NikitaBatalskiy@users.noreply.github.com>
2020-09-28 18:36:30 +0200

    fixed checkstyle problems

--- module1/src/main/java/net/broscorp/override/overload/MathMultiplying.java
+++ module1/src/main/java/net/broscorp/override/overload/MathMultiplying.java
@@ -4,21 +4,21 @@ public class MathMultiplying implements MathOperation {
 
   @Override
   public double operation(double x, double y) {
-    return x*y;
+    return x * y;
   }
 
   @Override
   public double operation(Double x, Double y) {
-    return x*y;
+    return x * y;
   }
 
   @Override
   public double operation(int x, int y) {
-    return x*y;
+    return x * y;
   }
 
   @Override
   public double operation(Integer x, Integer y) {
-    return x*y;
+    return x * y;
   }
 }
