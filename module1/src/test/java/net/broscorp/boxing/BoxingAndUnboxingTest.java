7ea9335c3f8d8b3cc99c647b03afb41229ab4b84
Hryhorii Perets <grperets@gmail.com>
2021-03-17 17:24:40 +0200

    Added test

---
+++ module1/src/test/java/net/broscorp/boxing/BoxingAndUnboxingTest.java
@@ -0,0 +1,49 @@
+package net.broscorp.boxing;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+
+public class BoxingAndUnboxingTest {
+
+
+  /*
+При сравнении классов-обёрток оператором == или !=,
+ происходит сравнение по ссылкам, а не по значениям
+ */
+  @Test
+  @DisplayName("Should return true")
+  public void souldReturnTrueTest(){
+    Integer one = 10;
+    Integer two = 10;
+    assertTrue(one == two);
+
+  }
+
+  @Test
+  @DisplayName("Should return false")
+  public void souldReturnFalseTest(){
+    Integer one = new Integer(10);
+    Integer two = new Integer(10);
+    assertFalse(one == two);
+
+  }
+
+
+  /*
+  Unboxing null всегда бросает NullPointerException.
+  Boxing имеет более высокий приоритет, чем сравнение ссылок
+   */
+
+  @Test
+  @DisplayName("Should throw NullPointerException")
+  public void souldThrowNullPointerExceptionTest(){
+    Integer one = null;
+    int i = 0;
+    assertThrows(NullPointerException.class, ()->Math.max(one, i));
+
+  }
+
+}
