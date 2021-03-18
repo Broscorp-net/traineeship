9b55d913a11cc7beac8b13b25b6dea1bf77fea1a
Vlad))) <vladik3641@gmail.com>
2021-03-18 14:24:09 +0200

    update to 5

--- module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
+++ module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
@@ -1,7 +1,7 @@
 package net.broscorp.primitives;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class PrimitivesTest {
 
@@ -9,55 +9,55 @@ public class PrimitivesTest {
   public void byteOverload() {
     byte a = Byte.MAX_VALUE;
     byte b = (byte) (a + 1);
-    Assert.assertTrue(b < a);
+    Assertions.assertTrue(b < a);
   }
 
   @Test
   public void shortOverload() {
     short a = Short.MAX_VALUE;
     short b = (short) (a + 1);
-    Assert.assertTrue(b < a);
+    Assertions.assertTrue(b < a);
   }
 
   @Test
   public void intOverload() {
     int a = Integer.MAX_VALUE;
     int b = a + 1;
-    Assert.assertTrue(b < a);
+    Assertions.assertTrue(b < a);
   }
 
   @Test
   public void shortByte() {
     short a = Short.MAX_VALUE;
     byte b = (byte) a;
-    Assert.assertNotEquals(a, b);
+    Assertions.assertNotEquals(a, b);
   }
 
   @Test
   public void integerShort() {
     int a = Integer.MAX_VALUE;
     short b = (short) a;
-    Assert.assertNotEquals(a, b);
+    Assertions.assertNotEquals(a, b);
   }
 
   @Test
   public void longInteger() {
     long a = Long.MAX_VALUE;
     int b = (int) a;
-    Assert.assertNotEquals(a, b);
+    Assertions.assertNotEquals(a, b);
   }
 
   @Test
   public void floatDouble() {
     float a = 10.1f;
     double b = a;
-    Assert.assertNotEquals(10.1, b);
+    Assertions.assertNotEquals(10.1, b);
 
   }
 
   @Test
   public void floatingPointOperation() {
     float a = 0.6f + 0.4f;
-    Assert.assertNotEquals(1, a);
+    Assertions.assertNotEquals(1, a);
   }
 }
