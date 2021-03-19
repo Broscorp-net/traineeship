0b4f99dae59dd2c1690d95adb1d1e01159d9dae1
Vadim Bobylov <davyjonessss@ukr.net>
2021-03-19 15:06:22 +0200

    add some tests for numbers

--- module1/src/test/java/net/broscorp/MyTest.java
+++ module1/src/test/java/net/broscorp/MyTest.java
@@ -6,7 +6,30 @@ import org.junit.jupiter.api.Test;
 class MyTest {
 
   @Test
-  void test() {
-    Assertions.assertTrue(true);
+  void numericOverflow() {
+    int maxInteger = Integer.MAX_VALUE;
+    int biggerThanMaxValue = maxInteger + 1000;
+    Assertions.assertTrue(maxInteger > biggerThanMaxValue);
+  }
+
+  @Test
+  void convertLongToInt() {
+    long num1 = 1000L;
+    int num2 = (int) num1;
+    Assertions.assertEquals(num1, num2);
+  }
+
+  @Test
+  void convertFloatToDouble() {
+    float num1 = 10.2323f;
+    double num2 = num1;
+    Assertions.assertEquals(num1, num2);
+  }
+
+  @Test
+  void errorWithFloatAndDouble(){
+    float num1 = 2.0f - 1.1f;
+    double num2 = 2.0 - 1.1;
+    Assertions.assertNotEquals(num1,num2);
   }
 }
