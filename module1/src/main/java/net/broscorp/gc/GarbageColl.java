c7e6246477b2e2083a5972e1fec12fff1c6855d8
[Your <[Your>
2021-03-25 00:50:42 +0200

    add results.md

--- module1/src/main/java/net/broscorp/gc/GarbageColl.java
+++ module1/src/main/java/net/broscorp/gc/GarbageColl.java
@@ -2,6 +2,18 @@ package net.broscorp.gc;
 
 public class GarbageColl {
 
+  public static void main(String[] args) {
+    for (int i = 0; i < 50000; i++) {
+      System.out.println(i);
+      GarbageColl nullObj = new GarbageColl();
+      GarbageColl first = new GarbageColl();
+      GarbageColl second = new GarbageColl();
+      nullObj.setGarbageColl(null);
+//      first.setGarbageColl(second);
+//      second.setGarbageColl(first);
+    }
+  }
+
   private GarbageColl garbageColl;
 
   public GarbageColl getGarbageColl() {
