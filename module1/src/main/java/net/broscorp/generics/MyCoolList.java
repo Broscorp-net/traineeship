f61c2fcd0f1a8dc989ff720e62c37db82e062b60
Libenko Ilya <libenkouriy@gmail.com>
2021-03-26 00:32:37 +0200

    [generics] add some fixes

--- module1/src/main/java/net/broscorp/generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/generics/MyCoolList.java
@@ -1,37 +1,79 @@
 package net.broscorp.generics;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.function.Function;
 
 public class MyCoolList<T extends Number> {
-  private List<T> list = new ArrayList<>();
+  private static final int DEFAULT_CAPACITY = 10;
+  private Object[] elements;
+  private int size = 0;
 
+  public MyCoolList() {
+    this.elements = new Object[DEFAULT_CAPACITY];
+  }
+
+  /**
+   * Method add element to list.
+   * @param number input
+   */
   public void add(T number) {
-    list.add(number);
+    if (size == elements.length) {
+      ensureCapacity();
+    }
+    elements[size++] = number;
   }
 
+  /**
+   * Method get element by index from list.
+   * @param index input
+   * @return
+   */
   public T get(int index) {
-    return list.get(index);
+    checkSize(index);
+    return (T) elements[index];
   }
 
+  /**
+   * Method remove element by index from list.
+   * @param index input
+   * @return
+   */
   public T remove(int index) {
-    return list.remove(index);
+    checkSize(index);
+    Object item = elements[index];
+    int numElements = elements.length - (index + 1);
+    System.arraycopy(elements, index + 1, elements, index, numElements);
+    size--;
+    return (T) item;
   }
 
   /**
    * Method for casting a type in a list.
-   * @param f function
+   *
+   * @param f   function
    * @param <R> required type
    * @return list with required type
    */
   public <R extends Number> MyCoolList<R> map(Function<T, R> f) {
     MyCoolList<R> myCoolList = new MyCoolList();
-    list.forEach(x -> myCoolList.add(f.apply(x)));
+    for (int i = 0; i < elements.length; i++) {
+      myCoolList.add(f.apply((T) elements[i]));
+    }
     return myCoolList;
   }
 
   public int size() {
-    return list.size();
+    return size;
+  }
+
+  private void checkSize(int index) {
+    if (index >= size || index < 0) {
+      throw new IndexOutOfBoundsException("Index: " + index + ", Size " + index);
+    }
+  }
+
+  private void ensureCapacity() {
+    int newSize = elements.length * 2;
+    elements = Arrays.copyOf(elements, newSize);
   }
 }
