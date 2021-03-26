83c5c12e1696e4358070970dfc4008bfbb7f4fd9
Samoshost Ihor <samoshost.igor@gmail.com>
2021-03-26 17:59:50 +0200

    Fixed failing build classes

--- module1/src/main/java/net/broscorp/generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/generics/MyCoolList.java
@@ -1,28 +1,28 @@
 package net.broscorp.generics;
 
 import java.util.function.Function;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+// import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class MyCoolList {
 
-  public void add(Object o) {
-    throw new NotImplementedException();
-  }
+  //  public void add(Object o) {
+  //    throw new NotImplementedException();
+  //  }
 
-  public Object get(int index) {
-    throw new NotImplementedException();
-  }
+  //  public Object get(int index) {
+  //    throw new NotImplementedException();
+  //  }
 
-  public Object remove(int index) {
-    throw new NotImplementedException();
-  }
+  //  public Object remove(int index) {
+  //    throw new NotImplementedException();
+  //  }
 
-  public MyCoolList map(Function f) {
-    throw new NotImplementedException();
-  }
+  //  public MyCoolList map(Function f) {
+  //    throw new NotImplementedException();
+  //  }
 
-  public int size() {
-    throw new NotImplementedException();
-  }
+  //  public int size() {
+  //    throw new NotImplementedException();
+  //  }
 
 }
