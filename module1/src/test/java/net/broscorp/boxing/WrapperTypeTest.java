da107de469b73dbfd88d2a329ca689085ba59afe
onesterov <https://github.com/malibu87/traineeship.git>
2021-03-26 22:57:09 +0200

    boxing

---
+++ module1/src/test/java/net/broscorp/boxing/WrapperTypeTest.java
@@ -0,0 +1,36 @@
+package net.broscorp.boxing;
+
+import static java.lang.Integer.valueOf;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class WrapperTypeTest {
+
+  @Test
+  void givenTwoEqualWrapperTypeValues_whenCompare_thenReturnTrue() {
+    Integer firstValue = 56;
+    Integer secondValue = 56;
+
+    assertTrue(firstValue == secondValue);
+  }
+
+  @Test
+  void givenTwoEqualWrapperTypeValues_whenCompare_thenReturnFalse() {
+    Integer firstValue = valueOf(128);
+    Integer secondValue = valueOf(128);
+
+    assertFalse(firstValue == secondValue);
+  }
+
+  @Test
+  void givenWrapperTypeEqualNull_whenCompareWithZero_thenNullPointerExceptionThrown() {
+
+    assertThrows(NullPointerException.class, () -> {
+      Double value = null;
+      boolean result = value == 0;
+    });
+  }
+}
