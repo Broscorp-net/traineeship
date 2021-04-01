0acdd46279371d5ae40e14a30d600f5fc5929861
Hryhorii Perets <grperets@gmail.com>
2021-04-02 00:42:30 +0300

    Passed all tests

--- module1/src/main/java/net/broscorp/inner/classes/MyCoolCollection.java
+++ module1/src/main/java/net/broscorp/inner/classes/MyCoolCollection.java
@@ -4,20 +4,19 @@ import java.util.Iterator;
 import java.util.Spliterator;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import net.broscorp.generics.MyCoolList;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-/**
- * @author Hryhorii Perets
- */
 public class MyCoolCollection<E> implements Iterable<E> {
 
   private Node<E> head;
   private Node<E> tail;
 
 
+  /**
+   * Add item in collection.
+   * @param item value item's
+   */
   public void add(E item) {
-    Node<E> newNode = new Node<>(item, tail);
+    Node<E> newNode = new Node<>(item);
     if (isEmpty()) {
       head = newNode;
       tail = newNode;
@@ -27,26 +26,75 @@ public class MyCoolCollection<E> implements Iterable<E> {
     }
   }
 
+  /**
+   * Return item from collection.
+   * @param index value position
+   * @return value object's
+   */
   public E get(int index) {
     E item = null;
-    Iterator<E> iterator = iterator();
-    for (int i = 0; i <= index; i++) {
-     if (iterator.hasNext()) {
-      item = iterator.next();
-     }
-   }
-
+    if (index > -1 && index < size()) {
+      Iterator<E> iterator = iterator();
+      for (int i = 0; i <= index; i++) {
+        if (iterator.hasNext()) {
+          item = iterator.next();
+        }
+      }
+      return item;
+    }
     return item;
   }
 
+  /**
+   * Delete object from collection.
+   * @param index value position
+   * @return remote item
+   */
   public E remove(int index) {
-    throw new NotImplementedException();
+    E item = null;
+    if (index == 0) {
+      item = head.item;
+      head = head.next;
+      return item;
+    }
+
+    if (index > 0 && index < size()) {
+      Node<E> currentNode = head;
+      Node<E> previous = null;
+      for (int i = 0; i < index; i++) {
+        if (i == index - 1) {
+          previous = currentNode;
+        }
+        if (currentNode.getNext() != null) {
+          currentNode = currentNode.next;
+        }
+      }
+      previous.setNext(currentNode.getNext());
+      item = currentNode.item;
+      return item;
+    }
+    return item;
   }
 
-  public MyCoolList map(Function f) {
-    throw new NotImplementedException();
+  /**
+   * Map object in collection.
+   * @param f function (lambda)
+   * @return this collection
+   */
+  public MyCoolCollection<E> map(Function<E, E> f) {
+    Iterator<E> iterator = iterator();
+    Node<E> currentNode = head;
+    while (iterator.hasNext()) {
+      currentNode.setItem(f.apply(iterator.next()));
+      currentNode = currentNode.next;
+    }
+    return this;
   }
 
+  /**
+   * Return number object in collection.
+   * @return size collection
+   */
   public int size() {
     int size = 0;
     Iterator<E> iterator = iterator();
@@ -57,14 +105,16 @@ public class MyCoolCollection<E> implements Iterable<E> {
     return size;
   }
 
+  /**
+   * Return boolean value if collection is empty.
+   * @return false or true
+   */
   public boolean isEmpty() {
     if (this.head == null) {
       return true;
     }
     return false;
   }
-
-
 
   @Override
   public Iterator<E> iterator() {
@@ -86,9 +136,8 @@ public class MyCoolCollection<E> implements Iterable<E> {
     private E item;
     private Node<E> next;
 
-    public Node(E item, Node<E> next) {
+    public Node(E item) {
       this.item = item;
-      this.next = next;
     }
 
     public E getItem() {
@@ -107,9 +156,23 @@ public class MyCoolCollection<E> implements Iterable<E> {
       this.next = next;
     }
 
-
   }
 
+  /**
+   * Inner classes — внутренние классы, Non-static nested classes — нестатические вложенные классы.
+   *    Объект внутреннего класса не может существовать без объекта «внешнего» класса.
+   *    У объекта внутреннего класса есть доступ к переменным «внешнего» класса.
+   *    Объект внутреннего класса нельзя создать в статическом методе «внешнего» класса.
+   *    Внутренний класс не может содержать статические переменные и методы.
+   *    При создании объекта внутреннего класса важную роль играет его модификатор доступа.
+   *    Модификаторы доступа для внутренних классов работают так же, как и для обычных переменных.
+   * Static nested classes — статические вложенные классы.
+   *    Объект статического класса не хранит ссылку на конкретный экземпляр внешнего класса.
+   *    Статический вложенный класс может обращаться только к статическим полям внешнего класса.
+   *    Объектов, static nested classes, (в отличии от статических переменных, которые существует
+   *    в единственном экземпляре) мы можем создать сколько угодно.
+   * @param <E> the type elements in the collection
+   */
   public class MyCoolIterator<E> implements Iterator<E> {
 
     private Node<E> currentNode;
@@ -132,11 +195,5 @@ public class MyCoolCollection<E> implements Iterable<E> {
       currentNode = currentNode.getNext();
       return item;
     }
-
-    @Override
-    public void remove() {
-      throw new UnsupportedOperationException();
-    }
   }
-
 }
--- module1/src/test/java/net/broscorp/inner/classes/MyCoolCollectionTest.java
+++ module1/src/test/java/net/broscorp/inner/classes/MyCoolCollectionTest.java
@@ -1,14 +1,14 @@
 package net.broscorp.inner.classes;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-/**
- * @author Hryhorii Perets
- */
+
 class MyCoolCollectionTest {
 
   private MyCoolCollection<Integer> myCoolCollection;
@@ -23,18 +23,175 @@ class MyCoolCollectionTest {
   }
 
   @Test
-  void add() {
+  @DisplayName("Should add object to collection")
+  void shouldAddObjectToCollection() {
+    int expectedSize = 0;
+    int actualSize = 0;
+
+    assertEquals(expectedSize, actualSize);
+
     myCoolCollection.add(1);
-    myCoolCollection.add(5);
+    myCoolCollection.add(new Integer(5));
     myCoolCollection.add(10);
     myCoolCollection.add(7);
-    System.out.println(myCoolCollection.size());
-    System.out.println(myCoolCollection.get(2));
+
+    expectedSize = 4;
+    actualSize = myCoolCollection.size();
+
+    assertEquals(expectedSize, actualSize);
 
   }
 
   @Test
-  void get() {
+  @DisplayName("Should return object from collection")
+  void shouldGetObjectFromCollection() {
+    final int expectedValue1 = 5;
+    final int expectedValue2 = 7;
+
+    myCoolCollection.add(1);
+    myCoolCollection.add(5);
+    myCoolCollection.add(10);
+    myCoolCollection.add(7);
+
+    int actualValue1 = myCoolCollection.get(1);
+    int actualValue2 = myCoolCollection.get(3);
+
+    assertEquals(expectedValue1, actualValue1);
+    assertEquals(expectedValue2, actualValue2);
+
+  }
+
+  @Test
+  @DisplayName("Should not return object from collection")
+  void shouldGetNullFromCollection() {
+
+    myCoolCollection.add(1);
+    myCoolCollection.add(5);
+    myCoolCollection.add(10);
+    myCoolCollection.add(7);
+
+    Integer actualValue1 = myCoolCollection.get(-1);
+    Integer actualValue2 = myCoolCollection.get(5);
+
+    assertNull(actualValue1);
+    assertNull(actualValue2);
+
+  }
+
+  @Test
+  @DisplayName("Should remove object from collection")
+  void shouldRemoveObjectFromCollection() {
+
+    myCoolCollection.add(1);
+    myCoolCollection.add(5);
+    myCoolCollection.add(10);
+    myCoolCollection.add(7);
+
+    int expectedSize = 4;
+    final int expectedItem = 10;
+
+    int actualSize = myCoolCollection.size();
+
+    assertEquals(expectedSize, actualSize);
+
+    expectedSize = 3;
+
+    int actualItem = myCoolCollection.remove(2);
+
+    actualSize = myCoolCollection.size();
+
+    assertEquals(expectedItem, actualItem);
+    assertEquals(expectedSize, actualSize);
+
+  }
+
+  @Test
+  @DisplayName("Should not remove object from collection")
+  void shouldNotRemoveObjectFromCollection() {
+
+    final int expectedSize = 4;
+
+    myCoolCollection.add(1);
+    myCoolCollection.add(5);
+    myCoolCollection.add(10);
+    myCoolCollection.add(7);
+
+    myCoolCollection.remove(-5);
+
+    int actualSize = myCoolCollection.size();
+
+    assertEquals(expectedSize, actualSize);
+
+    myCoolCollection.remove(4);
+
+    actualSize = myCoolCollection.size();
+
+    assertEquals(expectedSize, actualSize);
+
+  }
+
+  @Test
+  @DisplayName("Should map base collection")
+  void shouldMapCollection() {
+
+    myCoolCollection.add(1);
+    myCoolCollection.add(5);
+    myCoolCollection.add(10);
+    myCoolCollection.add(7);
+
+    final int expectedSize = 4;
+    int expectedItem = 10;
+
+    int actualSize = myCoolCollection.size();
+    int actualItem = myCoolCollection.get(2);
+
+    assertEquals(expectedSize, actualSize);
+    assertEquals(expectedItem, actualItem);
+
+    expectedItem = 100;
+
+    myCoolCollection.map((x) -> x * 10);
+
+    actualSize = myCoolCollection.size();
+    actualItem = myCoolCollection.get(2);
+
+    assertEquals(expectedSize, actualSize);
+    assertEquals(expectedItem, actualItem);
+
+  }
+
+  @Test
+  @DisplayName("Should return the number objects in collection")
+  void shouldReturnSizeCollection() {
+
+    int expectedSize = 0;
+    int actualSize = myCoolCollection.size();
+
+    assertEquals(expectedSize, actualSize);
+
+    myCoolCollection.add(10);
+    myCoolCollection.add(5);
+    myCoolCollection.add(10);
+
+    expectedSize = 3;
+    actualSize = myCoolCollection.size();
+
+    assertEquals(expectedSize, actualSize);
+
+    myCoolCollection.add(210);
+    myCoolCollection.add(17);
+
+    expectedSize = 5;
+    actualSize = myCoolCollection.size();
+
+    assertEquals(expectedSize, actualSize);
+
+    myCoolCollection.remove(1);
+
+    expectedSize = 4;
+    actualSize = myCoolCollection.size();
+
+    assertEquals(expectedSize, actualSize);
 
   }
 }
