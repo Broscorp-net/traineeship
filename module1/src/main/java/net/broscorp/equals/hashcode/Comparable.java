9d46ac749eba0808923a45644b124735357c41bd
onesterov <https://github.com/malibu87/traineeship.git>
2021-04-02 01:15:13 +0300

    equals.hashcode

---
+++ module1/src/main/java/net/broscorp/equals/hashcode/Comparable.java
@@ -0,0 +1,115 @@
+package net.broscorp.equals.hashcode;
+
+import java.util.Objects;
+
+public class Comparable {
+
+  private String string;
+  private int integer;
+  private boolean bool;
+  private char character;
+
+  public Comparable() {
+  }
+
+  /**
+   * Constructor with all parameters.
+   *
+   * @param string    used as string value
+   * @param integer   used as integer value
+   * @param bool      used as boolean value
+   * @param character used as character value
+   */
+
+  public Comparable(String string, int integer, boolean bool, char character) {
+    this.string = string;
+    this.integer = integer;
+    this.bool = bool;
+    this.character = character;
+  }
+
+  public String getString() {
+    return string;
+  }
+
+  public void setString(String string) {
+    this.string = string;
+  }
+
+  public int getInteger() {
+    return integer;
+  }
+
+  public void setInteger(int integer) {
+    this.integer = integer;
+  }
+
+  public boolean isBool() {
+    return bool;
+  }
+
+  public void setBool(boolean bool) {
+    this.bool = bool;
+  }
+
+  public char getCharacter() {
+    return character;
+  }
+
+  public void setCharacter(char character) {
+    this.character = character;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + (bool ? 1231 : 1237);
+    result = prime * result + character;
+    result = prime * result + integer;
+    result = prime * result + string.length();
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj == null) {
+      return false;
+    }
+    if (getClass() != obj.getClass()) {
+      return false;
+    }
+    Comparable other = (Comparable) obj;
+    if (bool != other.bool) {
+      return false;
+    }
+    if (character != other.character) {
+      return false;
+    }
+    if (integer != other.integer) {
+      return false;
+    }
+    if (string == null) {
+      if (other.string != null) {
+        return false;
+      }
+    } else if (!string.equals(other.string)) {
+      return false;
+    }
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "Comparable{"
+        + "string='" + string + '\''
+        + ", integer=" + integer
+        + ", bool=" + bool
+        + ", character=" + character
+        + '}';
+  }
+
+}
---
+++ module1/src/test/java/net/broscorp/equals/hashcode/ComparableTest.java
@@ -0,0 +1,127 @@
+package net.broscorp.equals.hashcode;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+
+class ComparableTest {
+
+  @Test
+  void givenComparableObject_whenCompareWithNull_thenFalseReturned() {
+    Comparable comparable = new Comparable();
+
+    assertFalse(comparable.equals(null));
+  }
+
+  @Test
+  void givenComparableObject_whenCompareWithItself_thenTrueReturned() {
+    Comparable comparable = new Comparable("Comparable", 444, false, 'T');
+
+    assertTrue(comparable.equals(comparable));
+  }
+
+  /*
+   * Transitivity
+   *
+   * */
+
+  @Test
+  void givenThreeEqualObjects_whenCompareAll_thenTrueReturned() {
+    Comparable first = new Comparable("Comparable", 404, true, 'q');
+    Comparable second = new Comparable("Comparable", 404, true, 'q');
+    Comparable third = new Comparable("Comparable", 404, true, 'q');
+
+    assertTrue(first.equals(second));
+    assertTrue(second.equals(third));
+    assertTrue(first.equals(third));
+  }
+
+
+  /*
+   * Symmetry
+   *
+   * */
+
+  @Test
+  void givenTwoEqualObjects_whenReversCompare_thenTrueReturned() {
+    Comparable first = new Comparable("Comparable", 4, true, 'r');
+    Comparable second = new Comparable("Comparable", 4, true, 'r');
+
+    assertTrue(first.equals(second));
+    assertTrue(second.equals(first));
+  }
+
+  @Test
+  void givenTwoDifferentObjects_whenReversCompare_thenFalseReturned() {
+    Comparable first = new Comparable("First", 1, true, 'n');
+    Comparable second = new Comparable("Second", 17, false, 'k');
+
+    assertFalse(first.equals(second));
+    assertFalse(second.equals(first));
+  }
+
+  /*
+   * Consistency
+   *
+   * */
+  @Test
+  void givenTwoEqualsObject_whenCompareWithDifferentThirdObject_thenFalseReturned() {
+    Comparable first = new Comparable("FirstComparable", 11, false, 'k');
+    Comparable second = new Comparable("FirstComparable", 11, false, 'k');
+    Comparable third = new Comparable("Comparable", 404, true, 'q');
+
+    assertTrue(first.equals(second));
+    assertFalse(first.equals(third));
+
+    assertTrue(first.equals(second));
+    assertFalse(first.equals(third));
+  }
+
+  /*
+   * Hashcode should be equal
+   * */
+
+  @Test
+  void givenObject_whenCompareHashCode_thenTrueReturned() {
+    Comparable comparable = new Comparable("Comparable", 444, false, 'T');
+    int first = comparable.hashCode();
+    int second = comparable.hashCode();
+
+    assertEquals(first, second);
+  }
+
+  @Test
+  void givenTwoEqualObject_whenCompareHashCode_thenTruReturned() {
+    Comparable first = new Comparable("FirstComparable", 11, false, 'k');
+    Comparable second = new Comparable("FirstComparable", 11, false, 'k');
+    int firstHashcode = first.hashCode();
+    int secondHashcode = second.hashCode();
+
+    assertTrue(first.equals(second));
+    assertEquals(firstHashcode, secondHashcode);
+  }
+
+  @Test
+  void givenListObjects_whenCompareDifferentObjectsOfList_thenHashCodesIsEqual() {
+    List<Comparable> objects = new ArrayList<>(
+        Arrays.asList(new Comparable[]{new Comparable("bar", 14, true, 'a'),
+            new Comparable("arb", 14, true, 'a'), new Comparable("some", 14, true, 'a'),
+            new Comparable("done", 14, true, 'a')}));
+
+    objects.stream().collect(Collectors.groupingBy(comparable -> comparable.hashCode()))
+        .forEach((key, list) -> {
+          list.forEach(x -> System.out.println("Вот объект " + x.toString() + ","));
+          System.out.println("они разные, но их хеш совпадает");
+          list.forEach(x -> {
+            assertEquals(list.get(0).hashCode(), x.hashCode());
+            assertFalse(list.get(0).equals(list.get(1)));
+          });
+        });
+  }
+}
