9e1b1fd7094ad81af937b65e0b1a0c3de1cf0413
NikitaBatalskiy <45694965+NikitaBatalskiy@users.noreply.github.com>
2021-04-05 20:49:34 +0300

    primitives done

---
+++ module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
@@ -0,0 +1,61 @@
+package net.broscorp.primitives;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class PrimitivesTest {
+
+  @Test
+  public void shouldBeNegative() {
+    int maxInteger = Integer.MAX_VALUE;
+    int overMax = maxInteger + 1;
+
+    assertTrue(overMax < 0);
+  }
+
+  @Test
+  public void shouldBeNegativeShortVersion() {
+    short maxShort = Short.MAX_VALUE;
+    short overMax = (short) (maxShort + 1);
+
+    assertTrue(overMax < 0);
+  }
+
+  @Test
+  public void longToIntConversion() {
+    long maxLong = Long.MAX_VALUE;
+
+    int longToInt = (int) maxLong;
+
+    assertNotEquals(maxLong, longToInt);
+  }
+
+  @Test
+  public void longToShortConversion() {
+    long maxLong = Long.MAX_VALUE;
+
+    short longToShort = (short) maxLong;
+
+    assertNotEquals(maxLong, longToShort);
+  }
+
+  @Test
+  public void floatToDoubleConversion() {
+    float someFloat = 158.168f;
+
+    double floatToDouble = someFloat;
+
+    assertNotEquals(158.168, floatToDouble);
+  }
+
+  @Test
+  public void doubleToFloatConversion() {
+    double someDouble = 158.168;
+
+    float doubleToFloat = (float) someDouble;
+
+    assertNotEquals(158.168, doubleToFloat);
+  }
+}
