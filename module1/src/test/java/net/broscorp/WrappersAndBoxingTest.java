cb54821a2372420bf14d0bbc7f9626438579b0eb
Саша Ворощук <voroshchuk721@gmail.com>
2021-04-07 14:50:57 +0300

    Fixed checkstyle and add Asserts

--- module1/src/test/java/net/broscorp/WrappersAndBoxingTest.java
+++ module1/src/test/java/net/broscorp/WrappersAndBoxingTest.java
@@ -1,4 +1,22 @@
-package net.broscorp.gamelife;
+package net.broscorp;
 
-public class WrappersAndBoxingTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class WrappersAndBoxingTest {
+
+  @Test
+  void test() {
+    Integer a1 = 1;
+    Integer a2 = 1;
+    Assertions.assertEquals(a1,a2);
+    //true
+    Integer b1 = 128;
+    Integer b2 = 128;
+    Assertions.assertEquals(b1,b2);
+    //false
+    Integer i = null;
+    Assertions.assertNull(i);
+    //NullPointerException
+  }
 }
