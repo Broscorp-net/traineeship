9e402df373ad35e553cda44f05389588dc9f6571
Саша Ворощук <voroshchuk721@gmail.com>
2021-04-07 14:54:58 +0300

    Add Asserts

--- module1/src/test/java/net/broscorp/MyTest.java
+++ module1/src/test/java/net/broscorp/MyTest.java
@@ -7,41 +7,36 @@ class MyTest {
 
   @Test
   void testNumericOverflow() {
-    Assertions.assertTrue(true);
     byte b = 120;
     b += 19;
-    System.out.println(b);
+    Assertions.assertTrue(b<0);
   }
 
   @Test
-  void testTypes(){
+  void testTypes() {
     long a = 4;
     int b = (int) a;
-    System.out.println(b);
-    System.out.println("[" + Long.MIN_VALUE +" , " +  Long.MAX_VALUE + "]");
-    System.out.println("[" + Integer.MIN_VALUE +" , " +  Integer.MAX_VALUE + "]");
+    Assertions.assertEquals(a,b);
     long a2 = -214748364812L;
     int b2 = (int) a2;
-    System.out.println(b2+ " Problem : numeric type overflow");
+    Assertions.assertNotEquals(a2,b2," Problem : numeric type overflow");
 
-    float c =5f;
+    float c = 5f;
     double d = c;
-    System.out.println(d);
-    System.out.println("[" + Float.MIN_VALUE +" , " +  Float.MAX_VALUE + "]");
-    System.out.println("[" + Double.MIN_VALUE +" , " +  Double.MAX_VALUE + "]");
+    Assertions.assertEquals(c,d);
     float c2 = 34.456f;
     double d2 = c2;
-    System.out.println(d2+ " If one of the operands of the operation is of the double type," +
-            " then the second operand is also converted to the double type");
+    Assertions.assertEquals(c2,d2," If one of the operands of the operation is of the double type,"
+            + " then the second operand is also converted to the double type");
   }
 
   @Test
-  void testFallibility(){
+  void testFallibility() {
     int a = 2147483647;
     float b = a;
-    System.out.println(b);
+    Assertions.assertEquals(a,b);
     double c = 2.0 - 1.1;
-    System.out.println(c+ " Although the expected result is 0.9");
+    Assertions.assertTrue(c != 0.9);
   }
 
 
