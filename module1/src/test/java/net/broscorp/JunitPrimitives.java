b9ac3012d03ec5322a4923b8a0d7c19904a31d29
AlexandrChesnokov <alexandr.chesnokov1@gmail.com>
2021-04-13 19:20:57 +0300

    Удалил класс StyledClass, перенес переменные в методы, добавил тест для чисел с плавающей точкой.

--- module1/src/test/java/net/broscorp/JunitPrimitives.java
+++ module1/src/test/java/net/broscorp/JunitPrimitives.java
@@ -1,7 +1,6 @@
 package net.broscorp;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -11,48 +10,39 @@ import org.junit.jupiter.api.TestInstance;
 @DisplayName("Junit5 primitives")
 public class JunitPrimitives {
 
-  private byte testByte;
-  private byte result;
-  private long bigLong;
-  private long smallLong;
-  private float testFloat;
-
-  @BeforeAll
-  void setUp() {
-    testByte = 120;
-    bigLong = 9223372036854775L;
-    smallLong = 202777289L;
-    testFloat = 18.4f;
-  }
-
   @Test
   void whenByteOverflow() {
+    byte testByte = 120;
     testByte += 12;
   }
 
   @Test
   void whenLongValueIsGreaterThanMaximumValueOfInt() {
+    long bigLong = 9223372036854775L;
     int anInt = (int) bigLong;
     Assertions.assertNotEquals(anInt, bigLong);
   }
 
   @Test
   void whenLongValueIsWithinThanMaximumValueOfInt() {
+    long smallLong = 202777289L;
     int anInt = (int) smallLong;
     Assertions.assertEquals(anInt, smallLong);
   }
 
   @Test
   void whenFloatCastToDouble() {
+    float testFloat = 18.4f;
     double testDouble = testFloat;
     System.out.println("float " + testFloat + " cast to double " + testDouble);
     Assertions.assertEquals(testDouble, testFloat);
   }
 
-
-
-
-
-
+  @Test
+  void floatingPointError() {
+    double first = 2.0;
+    double second = 1.1;
+    System.out.println(first - second);
+  }
 
 }
