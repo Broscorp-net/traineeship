7f251765b3e73c000d2c6e5e7ee6736a70ecb50b
NikitaBatalskiy <45694965+NikitaBatalskiy@users.noreply.github.com>
2021-04-18 00:22:36 +0300

    Bytecode task done

---
+++ module1/src/main/java/net/broscorp/bytecode/BytecodeClass.java
@@ -0,0 +1,23 @@
+package net.broscorp.bytecode;
+
+public class BytecodeClass {
+
+  public static void main(String[] args) {
+    int increment = 0;
+    while (increment < 10) {
+      increment++;
+    }
+
+    printHello();
+  }
+
+  public static void printHello() {
+    System.out.println("Hello :)");
+  }
+
+  public static void newMethod() {
+    int iterator = 0;
+    iterator++;
+    System.out.println(iterator);
+  }
+}
