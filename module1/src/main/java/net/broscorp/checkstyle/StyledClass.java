0d1da68c2bc093d17e7491538450b9630e8b354c
[Your <[Your>
2021-06-10 14:46:59 +0300

    for success build

--- module1/src/main/java/net/broscorp/checkstyle/StyledClass.java
+++ module1/src/main/java/net/broscorp/checkstyle/StyledClass.java
@@ -2,7 +2,7 @@ package net.broscorp.checkstyle;
 
 public class StyledClass {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
     System.out.println("Hello checkstyle");
   }
 }
