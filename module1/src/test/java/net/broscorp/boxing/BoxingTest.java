3010188ab5a10f68d5a2e40c82c99e557cbc9a8e
Vitamin_68 <wetalm68@gmail.com>
2021-07-24 23:56:33 +0300

    Add all tests. All test passed. Build success.

--- module1/src/test/java/net/broscorp/boxing/BoxingTest.java
+++ module1/src/test/java/net/broscorp/boxing/BoxingTest.java
@@ -1,5 +1,47 @@
 package net.broscorp.boxing;
 
-public class BoxingTest {
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BoxingTest {
+
+  //  переменные типа Byte, Short, Integer и Long хранятся в кэше
+  //  если находятся в диапазоне от -128 до +127
+  //  Character от 0 до 127
+  //  присоздании новой переменной JVM проверяет кэш,
+  //  для Integer диапазон можно изменить при запуске
+  @DisplayName("oneWrap == twoWrap")
+  @Test
+  void testCompareSameWrappers() {
+    Integer oneWrap = 100;
+    Integer twoWrap = 100;
+    assertSame(oneWrap, twoWrap);
+  }
+
+  @DisplayName("oneWrap != twoWrap, v.1")
+  @Test
+  void testCompareDifferentWrappersOne() {
+    Integer oneWrap = 200;
+    Integer twoWrap = 200;
+    assertNotSame(oneWrap, twoWrap);
+  }
+
+  @DisplayName("oneWrap != twoWrap, v.2")
+  @Test
+  void testCompareDifferentWrappersTwo() {
+    Integer oneWrap = new Integer(100);
+    Integer twoWrap = new Integer(100);
+    assertNotSame(oneWrap, twoWrap);
+  }
+
+  @DisplayName("Unboxing wrapper throw Exception")
+  @Test
+  void testUnboxingWrapperNpe() {
+    Integer wrap = null;
+    assertThrows(NullPointerException.class, () -> System.out.println(wrap.intValue()));
+  }
 }
