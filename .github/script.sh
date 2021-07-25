d3c60d6a163977aea492fdccdc8c151f2c3c5718
RomanSmyrnov <rs2015.ua@gmail.com>
2021-07-25 23:28:11 +0300

    Add action script

--- .github/script.sh
+++ .github/script.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 git fetch origin master:master
-#changed_files=$(git diff --name-only --diff-filter=ACMRT master | grep .class$ | xargs)
-changed_files=$(git diff --name-only --diff-filter=ACMRT master | grep '.class$ | .ldd$' | xargs)
+changed_files=$(git diff --name-only --diff-filter=ACMRT master | grep .class$ | xargs)
 if [ -z "$changed_files" ]
 then
 echo "Trash files not found"
 exit 0;
 else
-  echo " Error: Trash files found!!!!"
+  echo " Error: Trash files found!!!! Please delete trash files, and try again."
 exit 1;
 fi
