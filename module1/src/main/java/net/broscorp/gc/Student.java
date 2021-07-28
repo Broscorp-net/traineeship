846e425e14bbfea1e391f42d2ad2b7a9665c263a
Viktor <lebedevit4@gmail.com>
2021-07-28 18:30:34 +0300

    gc

---
+++ module1/src/main/java/net/broscorp/gc/Student.java
@@ -0,0 +1,64 @@
+package net.broscorp.gc;
+
+public class Student {
+
+    static private int ID;
+
+    final private String name;
+
+    public Student() {
+        this.name = "Student" + ID++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println(name + " says : " + " bye!");
+    }
+}
+
+
+class Faculty {
+    final private String name;
+
+    public Student student;
+
+    public Faculty(String name) {
+        this.name = name;
+        this.student = new Student();
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Faculty of " + name + " destroy...");
+    }
+}
+
+class GC{
+
+    static GC SAVE;
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("execute");
+        SAVE = this;
+    }
+}
---
+++ module1/src/main/java/net/broscorp/gc/results.md
@@ -0,0 +1,9 @@
+Results
+===
+
+###1.gc deletes the object if there is no link
+
+###2.the finalize method fires when the object is cleaned
+
+###3.the collector needs some time to complete the cleaning
+ 
