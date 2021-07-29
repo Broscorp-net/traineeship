5c5eb57c0b1a372e961fcd5ca7da313b30d3e8f6
Viktor <lebedevit4@gmail.com>
2021-07-29 14:23:03 +0300

    made a few changes

--- module1/src/test/java/net/broscorp/valueref/FooTest.java
+++ module1/src/test/java/net/broscorp/valueref/FooTest.java
@@ -1,11 +1,14 @@
 package net.broscorp.valueref;
+
 import java.util.ArrayList;
+
 import java.util.Arrays;
+
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class FooTest {
