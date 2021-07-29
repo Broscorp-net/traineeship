8674f59758f9e6778ad4de8df84dd8362973903b
Andrii Kniaziev <andrey.knyazev1995@gmail.com>
2021-07-29 10:02:22 +0300

    Unused files removed

--- module1/src/test/java/net/broscorp/primitives/PrimitivesTypesTest.java
+++ module1/src/test/java/net/broscorp/primitives/PrimitivesTypesTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class PrimitivesTest {
+public class PrimitivesTypesTest {
 
   @Test
   public void addToIntMaxValueTest() {
