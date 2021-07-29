88a774e5ad33fb2a50093ae6d1e60bb120b79903
Viktor <lebedevit4@gmail.com>
2021-07-29 14:11:01 +0300

    fix google format style

--- module1/src/test/java/net/broscorp/valueref/FooTest.java
+++ module1/src/test/java/net/broscorp/valueref/FooTest.java
@@ -9,19 +9,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FooTest {
-    @Test
-    void foo() {
-        Foo foo = new Foo();
-        int i = 1;
+  @Test
+  void foo() {
+    Foo foo = new Foo();
+    int i = 1;
 
-        List<String> list = new ArrayList<>();
+    List<String> list = new ArrayList<>();
 
-        foo.foo(i, list);
-        //assertSame compares references to objects
-        assertEquals(1, i);
-        //Variables on the stack exist as long as the method in which they were created is executed
-        assertEquals(1, list.size());
-        assertEquals(Arrays.asList("d"), list);
-    }
-
+    foo.foo(i, list);
+    // assertSame compares references to objects
+    assertEquals(1, i);
+    // Variables on the stack exist as long as the method in which they were created is executed
+    assertEquals(1, list.size());
+    assertEquals(Arrays.asList("d"), list);
+  }
 }
