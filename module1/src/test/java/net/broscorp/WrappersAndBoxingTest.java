2b2ac35f19e1cce8bc9008945e59437ce623a920
Evgeniy <46program@gmail.com>
2021-07-29 23:16:48 +0300

    Create test class WrappersAndBoxingTest

---
+++ module1/src/test/java/net/broscorp/WrappersAndBoxingTest.java
@@ -0,0 +1,59 @@
+package net.broscorp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class WrappersAndBoxingTest {
+
+  //1.	Написать тест, в котором сравнение двух оберточных типов с одинаковым значением через __==__ даёт true.
+
+  @Test
+  public void comparisonOfTwoCharactersWithTheSameValueTrueTest() {
+    Character characterFirst = 'A';
+    Character characterSecond = 'A';
+    Assertions.assertTrue(characterFirst == characterSecond);
+  }
+
+  //2.  Написать тест, в котором сравнение двух оберточных типов с одинаковым значением через __==__ даёт false.
+  // (дополнительная похвала если придумать два таких варианта)
+
+  @Test
+  public void comparisonOfTwoCharactersWithTheSameValueFalseTest() {
+    Character characterFirst = new Character('A');
+    Character characterSecond = new Character('A');
+    Assertions.assertFalse(characterFirst == characterSecond);
+  }
+
+  /**
+   * Cache to support the object identity semantics of autoboxing for values between 0 and 127
+   */
+  @Test
+  public void comparisonOfTwoCharactersWithTheSameValueFalseTestNew() {
+    Character characterFirst = Character.valueOf('Я');
+    Character characterSecond = Character.valueOf('Я');
+    Assertions.assertFalse(characterFirst == characterSecond);
+  }
+
+
+  /**
+   * Cache to support the object identity semantics of autoboxing for values between -128 and 127
+   * (inclusive) as required by JLS.
+   */
+  @Test
+  public void comparisonOfTwoIntegersWithTheSameValueFalseTest() {
+    Integer firstInt = 300;
+    Integer secondInt = firstInt;
+    secondInt = 300;
+    Assertions.assertFalse(firstInt == secondInt);
+  }
+
+  //3.	Написать тест в котором неявный анбоксинг вызывает исключение.
+
+  @Test
+  public void implicitUnboxingThrowsAnExceptionTest(){
+    Assertions.assertThrows(IllegalArgumentException.class, () ->{
+      char[] chars = Character.toChars(-1);
+    });
+  }
+
+}
