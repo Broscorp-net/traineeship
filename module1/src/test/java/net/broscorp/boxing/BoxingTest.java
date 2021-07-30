0a3841c96fde80523a0a93890518535457a8e4ff
Viktor <lebedevit4@gmail.com>
2021-07-30 11:23:28 +0300

    .

--- module1/src/test/java/net/broscorp/boxing/BoxingTest.java
+++ module1/src/test/java/net/broscorp/boxing/BoxingTest.java
@@ -1,11 +1,11 @@
 package net.broscorp.boxing;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
