cd5f6e20b72b7373018e2392f5e52fb56424b494
RomanSmyrnov <rs2015.ua@gmail.com>
2021-07-31 00:09:59 +0300

    Add script and new action

---
+++ .github/script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+git fetch origin master:master
+changed_files=$(git diff --name-only --diff-filter=ACMRT master | grep -e .class$ -e .iml$ -e .war$ -e .jar$ -e .xml$| xargs)
+if [ -z "$changed_files" ]
+then
+echo "Trash files not found"
+exit 0;
+else
+  echo " Error: Trash files found!!!! Please delete trash files:"
+  echo $changed_files
+exit 1;
+fi
