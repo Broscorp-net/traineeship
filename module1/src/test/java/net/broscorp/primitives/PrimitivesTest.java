fed6def7a0f64f46f7d345df5f11f40277221d8f
Viktor <lebedevit4@gmail.com>
2021-07-30 11:00:02 +0300

    done

--- module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
+++ module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
@@ -1,45 +1,44 @@
 package net.broscorp.primitives;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 public class PrimitivesTest {
-    @Test
-    void overloop() throws Exception {
+  @Test
+  void overloop() throws Exception {
 
-        byte c = Byte.MAX_VALUE;
+    byte c = Byte.MAX_VALUE;
 
-        assertEquals(-128, ++c);
+    assertEquals(-128, ++c);
+  }
 
-    }
+  @Test
+  void conversionInt() {
 
-    @Test
-    void conversionInt() {
+    long l = 922337203685477580L;
+    int i = (int) l;
 
-        long l = 922337203685477580L;
-        int i = (int) l;
+    assertEquals(-858993460, i);
+  }
 
-        assertEquals(-858993460, i);
-    }
+  @Test
+  void conversionDouble() {
+    float f = 2345.23452345f;
 
-    @Test
-    void conversionDouble() {
-        float f = 2345.23452345f;
+    double d = f;
 
-        double d = f;
+    assertEquals(2345.234619140625, d);
+  }
 
-        assertEquals(2345.234619140625, d);
-    }
+  @Test
+  void infelicity() {
+    double a = 0.1;
 
+    double b = 0.2;
 
-    @Test
-    void infelicity() {
-        double a = 0.1;
+    double d = a + b;
 
-        double b = 0.2;
-
-        double d = a + b;
-
-        assertEquals(0.30000000000000004, d);
-    }
-
+    assertEquals(0.30000000000000004, d);
+  }
 }
