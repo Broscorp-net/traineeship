d5623b2045e4ad7365bb6cea6f138a1729da13af
Viktor <lebedevit4@gmail.com>
2021-07-30 12:32:45 +0300

    code style fix

--- module1/src/main/java/net/broscorp/gc/results.md
+++ module1/src/main/java/net/broscorp/gc/results.md
@@ -1,9 +1,9 @@
 Results
 ===
 
-###1.gc deletes the object if there is no link
+### 1.gc deletes the object if there is no link
 
-###2.the finalize method fires when the object is cleaned
+### 2.the finalize method fires when the object is cleaned
 
-###3.the collector needs some time to complete the cleaning
+### 3.the collector needs some time to complete the cleaning
  
--- module1/src/test/java/net/broscorp/gc/GarbageTest.java
+++ module1/src/test/java/net/broscorp/gc/GarbageTest.java
@@ -4,71 +4,66 @@ import org.junit.jupiter.api.Test;
 
 public class GarbageTest {
 
+  @Test
+  void testingGC() {
 
-    @Test
-    void testingGC() {
+    Faculty f1 = new Faculty("Physics");
+    System.out.println("creating faculty " + f1.getName() + "...");
 
+    Faculty f2 = new Faculty("Biology");
+    System.out.println("creating faculty " + f2.getName() + "...");
 
-        Faculty f1 = new Faculty("Physics");
-        System.out.println("creating faculty " + f1.getName() + "...");
+    Faculty f3 = new Faculty("Math");
+    System.out.println("creating faculty " + f3.getName() + "...");
 
-        Faculty f2 = new Faculty("Biology");
-        System.out.println("creating faculty " + f2.getName() + "...");
+    Faculty f4 = new Faculty("Chemistry");
+    System.out.println("creating faculty " + f4.getName() + "...");
+    f4 = null;
+    Faculty f5 = new Faculty("Logic");
+    System.out.println("creating faculty " + f5.getName() + "...");
 
-        Faculty f3 = new Faculty("Math");
-        System.out.println("creating faculty " + f3.getName() + "...");
+    Faculty f6 = new Faculty("Science");
+    System.out.println("creating faculty " + f6.getName() + "...");
+    f6 = null;
+    Faculty f7 = new Faculty("Medicine");
+    System.out.println("creating faculty " + f7.getName() + "...");
 
-        Faculty f4 = new Faculty("Chemistry");
-        System.out.println("creating faculty " + f4.getName() + "...");
-        f4 = null;
-        Faculty f5 = new Faculty("Logic");
-        System.out.println("creating faculty " + f5.getName() + "...");
+    Faculty f8 = new Faculty("Geography");
+    System.out.println("creating faculty " + f8.getName() + "...");
+    f8 = null;
+    Faculty f9 = new Faculty("Geology");
+    System.out.println("creating faculty " + f9.getName() + "...");
 
-        Faculty f6 = new Faculty("Science");
-        System.out.println("creating faculty " + f6.getName() + "...");
-        f6 = null;
-        Faculty f7 = new Faculty("Medicine");
-        System.out.println("creating faculty " + f7.getName() + "...");
+    Faculty f10 = new Faculty("Technology");
+    System.out.println("creating faculty " + f10.getName() + "...");
 
-        Faculty f8 = new Faculty("Geography");
-        System.out.println("creating faculty " + f8.getName() + "...");
-        f8 = null;
-        Faculty f9 = new Faculty("Geology");
-        System.out.println("creating faculty " + f9.getName() + "...");
+    Faculty f11 = new Faculty("Nuclear");
+    System.out.println("creating faculty " + f11.getName() + "...");
 
-        Faculty f10 = new Faculty("Technology");
-        System.out.println("creating faculty " + f10.getName() + "...");
+    Faculty f12 = new Faculty("Space");
+    System.out.println("creating faculty " + f12.getName() + "...");
+    f12 = null;
+    Faculty f13 = new Faculty("Galactic");
+    System.out.println("creating faculty " + f13.getName() + "...");
 
-        Faculty f11 = new Faculty("Nuclear");
-        System.out.println("creating faculty " + f11.getName() + "...");
+    Faculty f14 = new Faculty("Building");
+    System.out.println("creating faculty " + f14.getName() + "...");
 
-        Faculty f12 = new Faculty("Space");
-        System.out.println("creating faculty " + f12.getName() + "...");
-        f12 = null;
-        Faculty f13 = new Faculty("Galactic");
-        System.out.println("creating faculty " + f13.getName() + "...");
+    System.gc();
+  }
 
-        Faculty f14 = new Faculty("Building");
-        System.out.println("creating faculty " + f14.getName() + "...");
+  @Test
+  void saveTest() throws InterruptedException {
+    GC.SAVE = new GC();
 
-        System.gc();
+    GC.SAVE = null;
+    System.gc();
+    Thread.sleep(5000);
 
+    if (GC.SAVE != null) {
+      System.out.println("i'm here");
+    } else {
+      System.out.println("loose");
     }
-
-    @Test
-    void saveTest() throws InterruptedException {
-        GC.SAVE = new GC();
-
-        GC.SAVE = null;
-        System.gc();
-        Thread.sleep(5000);
-
-
-        if (GC.SAVE != null){
-            System.out.println("i'm here");
-        }else{
-            System.out.println("loose");
-        }
-    }
-
+  }
 }
