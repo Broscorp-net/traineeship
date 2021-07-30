c80be6b4d06cebdb3a64089d96e95369057ca14f
Viktor <lebedevit4@gmail.com>
2021-07-30 11:20:26 +0300

    changed test class, delete style class

--- module1/src/test/java/net/broscorp/boxing/BoxingTest.java
+++ module1/src/test/java/net/broscorp/boxing/BoxingTest.java
@@ -1,52 +1,57 @@
 package net.broscorp.boxing;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
 import org.junit.jupiter.api.Test;
 
-import javax.lang.model.type.NullType;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-
 public class BoxingTest {
-    @Test
-    void testSame(){
+  @Test
+  void testSame() {
 
-        Integer i = 18;
-        Integer i2 = 18;
+    Integer i = 18;
+    Integer i2 = 18;
 
-        assertSame(i, i2);
+    assertTrue(i == i2);
+  }
 
-    }
+  @Test
+  void testDiffer() {
 
-    @Test
-    void testDiffer(){
+    Integer i = 188;
+    Integer i2 = 188;
 
-        Integer i = 188;
-        Integer i2 = 188;
+    assertFalse(i == i2);
+    // in this example, the assumed value is out of the scope of the cache -> create a new object
+    // every time
+  }
 
-        assertNotSame(i, i2);
-//in this example, the assumed value is out of the scope of the cache -> create a new object every time
-    }
+  @Test
+  void testDiffer2() {
 
-    @Test
-    void testDiffer2(){
+    Integer i = new Integer(18);
+    Integer i2 = new Integer(18);
 
-        Integer i = new Integer(18);
-        Integer i2 = new Integer(18);
+    assertFalse(i == i2);
 
-        assertNotSame(i,i2);
+    // create a new object every time
+  }
 
-        //create a new object every time
-    }
+  @Test
+  void testUnboxException() {
+    Integer i = null;
 
-    @Test
-    void testUnboxException(){
-        Integer i = null;
+    assertThrows(
+        Exception.class,
+        () -> {
+          int i2 = i;
+        });
 
-        assertThrows(Exception.class, () -> {int i2 = i; });
+    //        catch exception '_'
 
-//        catch exception '_'
-
-
-    }
-
+  }
 }
