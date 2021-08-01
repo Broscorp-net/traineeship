4b066aadc5f6b6daddfdc959c66539f40c627b88
Evgeniy <46program@gmail.com>
2021-08-01 21:32:10 +0300

    valuerf test

--- module1/src/test/java/net/broscorp/valueref/FooTest.java
+++ module1/src/test/java/net/broscorp/valueref/FooTest.java
@@ -1,8 +1,8 @@
 package net.broscorp.valueref;
 
-import java.lang.reflect.Array;
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
