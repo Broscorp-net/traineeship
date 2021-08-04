f8a72fbef044c823162078bc6825aad4e51f73c4
Danylo Havrylchenko <havrylchenko.d@gmail.com>
2021-08-05 00:09:48 +0300

    Changed description, splitted test into two

--- module1/src/test/java/net/broscorp/valueref/FooTest.java
+++ module1/src/test/java/net/broscorp/valueref/FooTest.java
@@ -6,32 +6,42 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
+///primitive integer is stored in a stack memory because of his low size
+//this way integers may be accessed and operated faster
+
+//list, on the other hand, is stored in heap ram memory due to its variable length and stored items size
+
+//by passing integer as a function arg, i am passing an actual value of integer, so function does not
+//know about what the actual address of the variable is
+
+//by passing a list as a function arg, i am actually passing the address in memory, which references
+//the lists' start
+
+//that is why modifying integer makes no effect on the integer outside function
+//but modifying list actually modifies the list
+
 public class FooTest {
+
+  private final Foo garbageAdder = new Foo();
 
   @Test
   public void referenceTest() {
-    Foo garbageAdder = new Foo();
-
     int x = 0;
+
+    garbageAdder.foo(x, new ArrayList<>());
+
+    assertEquals(0, x);
+
+  }
+
+  @Test
+  public void valueTest() {
     List<String> list = new ArrayList<>();
 
-    garbageAdder.foo(x, list);
+    garbageAdder.foo(0, list);
 
-    //primitive integer is stored in a processor stack memory because of his low size
-    //this way integers may be accessed and operated faster
-
-    //list, on the other hand, is stored in heap ram memory due to its variable length and stored items size
-
-    //by passing integer as a function arg, i am passing an actual value of integer, so function does not
-    //know about what the actual address of the variable is
-
-    //by passing a list as a function arg, i am actually passing the address in memory, which references
-    //the lists' start
-
-    //that is why modifying integer makes no effect on the integer outside function
-    //but modifying list actually modifies the list
-    assertEquals(0, x);
     assertEquals("d", list.get(0));
   }
+
 
 }
