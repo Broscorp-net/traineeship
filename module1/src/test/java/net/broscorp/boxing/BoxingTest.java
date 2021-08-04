60ce619ec35522bee63c4f2791b057610b97a904
Danylo Havrylchenko <havrylchenko.d@gmail.com>
2021-08-04 23:56:09 +0300

    Splitted one test into two

--- module1/src/test/java/net/broscorp/boxing/BoxingTest.java
+++ module1/src/test/java/net/broscorp/boxing/BoxingTest.java
@@ -23,13 +23,15 @@ public class BoxingTest {
     Integer wrappedInt2 = new Integer(12);
 
     assertFalse(wrappedInt1 == wrappedInt2);
+  }
 
+  @Test
+  public void fakeInternMustReturnFalseTest() {
     List<Integer> list = new ArrayList<>();
     list.add(129);
     list.add(129);
 
     assertFalse(list.get(0) == list.get(1));
-
   }
 
   @Test
