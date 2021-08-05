2b600e222d3d52c422abfb81061535c508d46604
Viktor <lebedevit4@gmail.com>
2021-08-05 17:02:19 +0300

    add reader for main method

--- module1/src/main/java/net/broscorp/gamelife/GameOfLife.java
+++ module1/src/main/java/net/broscorp/gamelife/GameOfLife.java
@@ -1,10 +1,26 @@
 package net.broscorp.gamelife;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class GameOfLife {
 
-  public void game(String fileNameInput, String fileNameOutput){
-
+  public void game(String fileNameInput, String fileNameOutput) {
+    CellState[][] arr = null;
+    try (BufferedReader br = new BufferedReader(new FileReader(fileNameInput))) {
+      int rows = Integer.parseInt(br.readLine());
+      int cols = Integer.parseInt(br.readLine());
+      int countOfIteration = Integer.parseInt(br.readLine());
+      arr = new CellState[cols][rows];
+      for (int i = 0; i < 3; i++) {
+        String[] strArr = br.readLine().trim().split(" ");
+        for (int j = 0; j < 3; j++) {
+          arr[i][j] = CellState.from(strArr[j]);
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
-
 }
