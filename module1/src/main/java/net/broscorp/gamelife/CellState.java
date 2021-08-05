ce95d1ee31a913e0eda8b725b26cd5887bdec4c4
Viktor <lebedevit4@gmail.com>
2021-08-05 17:13:14 +0300

    code style fix

--- module1/src/main/java/net/broscorp/gamelife/CellState.java
+++ module1/src/main/java/net/broscorp/gamelife/CellState.java
@@ -1,25 +1,23 @@
 package net.broscorp.gamelife;
 
-
 enum CellState {
-    ALIVE("X"),
-    DEAD("O"),
-    RESURRECT("R"),
-    DYING("D");
+  ALIVE("X"),
+  DEAD("O"),
+  RESURRECT("R"),
+  DYING("D");
 
+  private final String value;
 
-    private final String value;
+  CellState(String value) {
+    this.value = value;
+  }
 
-    CellState(String value) {
-        this.value = value;
+  public static CellState from(String value) {
+    for (CellState e : values()) {
+      if (e.value.equalsIgnoreCase(value)) {
+        return e;
+      }
     }
-
-    public static CellState from(String value) {
-        for (CellState e : values()) {
-            if (e.value.equalsIgnoreCase(value)) {
-                return e;
-            }
-        }
-        throw new IllegalArgumentException("CellState with given value is not exist: " + value);
-    }
+    throw new IllegalArgumentException("CellState with given value is not exist: " + value);
+  }
 }
