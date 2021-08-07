f316bf551eefba735128878fd9bd72d8b90831b0
Danylo Havrylchenko <havrylchenko.d@gmail.com>
2021-08-07 13:29:40 +0300

    Fixed styling errors

--- module1/src/test/java/net/broscorp/valueref/FooTest.java
+++ module1/src/test/java/net/broscorp/valueref/FooTest.java
@@ -9,13 +9,14 @@ import org.junit.jupiter.api.Test;
 ///primitive integer is stored in a stack memory because of his low size
 //this way integers may be accessed and operated faster
 
-//list, on the other hand, is stored in heap ram memory due to its variable length and stored items size
+//list, on the other hand, is stored in heap ram memory due to its variable length and stored items
+//size
 
-//by passing integer as a function arg, i am passing an actual value of integer, so function does not
-//know about what the actual address of the variable is
+//by passing integer as a function arg, i am passing an actual value of integer, so function does
+//not know about what the actual address of the variable is
 
-//by passing a list as a function arg, i am actually passing the address in memory, which references
-//the lists' start
+//by passing a list as a function arg, i am actually passing the address in memory, which
+//references the lists' start
 
 //that is why modifying integer makes no effect on the integer outside function
 //but modifying list actually modifies the list
