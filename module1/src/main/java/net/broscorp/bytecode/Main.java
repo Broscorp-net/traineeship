92c79d058269650bee97e54859594227812a2d37
Viktor <lebedevit4@gmail.com>
2021-08-08 17:38:14 +0300

    first round

--- module1/src/main/java/net/broscorp/bytecode/Main.java
+++ module1/src/main/java/net/broscorp/bytecode/Main.java
@@ -1,26 +1,22 @@
 package net.broscorp.bytecode;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
-    public static int x;
+  public static int x;
 
-    public static void hello(){
-        System.out.println("Hello");
+  public static void hello() {
+    System.out.println("Hello");
+  }
+
+  public static void main(String[] args) {
+
+    Integer ii = 10;
+
+    for (int i = 0; i < 5; i++) {
+      System.out.println(x + i);
+      System.out.println(ii + i);
     }
 
-    public static void main(String[] args) {
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println(x + i);
-        }
-
-        Main.hello();
-
-
-    }
+    Main.hello();
+  }
 }
-
-
--- module1/src/main/java/net/broscorp/bytecode/script.cmd
+++ module1/src/main/java/net/broscorp/bytecode/script.cmd
@@ -1,5 +1,5 @@
 javac Main.java
 
-javap -c -s -verbose Main.class > decompile.txt
+javap -c -s -verbose Main.class >> decompile.txt
 
 echo \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\>>decompile.txt
