9ea9cd545e6b1f1e48d98b69ff4e78eab921bda2
Viktor <lebedevit4@gmail.com>
2021-08-08 17:44:49 +0300

    code style fixed

--- module1/src/main/java/net/broscorp/bytecode/Main.java
+++ module1/src/main/java/net/broscorp/bytecode/Main.java
@@ -8,8 +8,9 @@ public class Main {
   public static void hello() {
     System.out.println("Hello");
   }
-
-  /** @param args - string. */
+  /**
+   * @param args - string.
+   * */
   public static void main(String[] args) {
 
     for (int i = 0; i < 5; i++) {
