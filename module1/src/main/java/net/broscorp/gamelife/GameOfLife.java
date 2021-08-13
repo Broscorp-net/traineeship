9c1278631bb2e4f858b8f2927ae20c109362948f
nan0machinesun <nan0machinesun@gmail.com>
2021-08-13 13:45:01 +0300

    Complete

--- module1/src/main/java/net/broscorp/gamelife/GameOfLife.java
+++ module1/src/main/java/net/broscorp/gamelife/GameOfLife.java
@@ -1,6 +1,5 @@
 package net.broscorp.gamelife;
 
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -16,42 +15,52 @@ import java.util.stream.Stream;
 
 public class GameOfLife {
 
+  /** The method for calculating the Game of Life.
+   *
+   * @param fileNameInput contains initial data of the game.
+   * @param fileNameOutput contains the result of the calculation.
+   */
   public void game(String fileNameInput, String fileNameOutput) {
 
-    GameData data = parse(readFromFile(fileNameInput));
+    Configuration config = parse(readFromFile(fileNameInput));
 
-    int iterations = data.getIterations();
-    int rows = data.getRows();
-    int columns = data.getColumns();
-    byte[][] currentGeneration = data.getInitFigure();
+    int iterations = config.getIterations();
+    int rows = config.getRows();
+    int columns = config.getColumns();
+    byte[][] currentGeneration = config.getInitialFigure();
 
     while (iterations > 0) {
-      byte[][] nextGeneration = new byte[rows][columns];
-
-      for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-
-          int sum =
-            currentGeneration[mod(i - 1, rows)][mod(j - 1, columns)]
-              + currentGeneration[mod(i - 1, rows)][mod(j, columns)]
-              + currentGeneration[mod(i - 1, rows)][mod(j + 1, columns)]
-
-              + currentGeneration[mod(i, rows)][mod(j - 1, columns)]
-              + currentGeneration[mod(i, rows)][mod(j, columns)]
-              + currentGeneration[mod(i, rows)][mod(j + 1, columns)]
-
-              + currentGeneration[mod(i + 1, rows)][mod(j - 1, columns)]
-              + currentGeneration[mod(i + 1, rows)][mod(j, columns)]
-              + currentGeneration[mod(i + 1, rows)][mod(j + 1, columns)];
-
-          nextGeneration[i][j] = (sum == 4) ? currentGeneration[i][j] : (sum == 3 ? (byte) 1 : 0);
-
-        }
-      }
-      currentGeneration = nextGeneration;
+      currentGeneration = calculateNewGeneration(currentGeneration, rows, columns);
       iterations--;
     }
-    writeToFile(fileNameOutput, toString(currentGeneration));
+
+    writeToFile(fileNameOutput, convertToString(currentGeneration));
+  }
+
+  private byte[][] calculateNewGeneration(byte[][] previousGeneration, int rows, int columns) {
+    byte[][] nextGeneration = new byte[rows][columns];
+
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns; j++) {
+
+        int sum =
+              previousGeneration[mod(i - 1, rows)][mod(j - 1, columns)]
+            + previousGeneration[mod(i - 1, rows)][mod(j, columns)]
+            + previousGeneration[mod(i - 1, rows)][mod(j + 1, columns)]
+
+            + previousGeneration[mod(i, rows)][mod(j - 1, columns)]
+            + previousGeneration[mod(i, rows)][mod(j, columns)]
+            + previousGeneration[mod(i, rows)][mod(j + 1, columns)]
+
+            + previousGeneration[mod(i + 1, rows)][mod(j - 1, columns)]
+            + previousGeneration[mod(i + 1, rows)][mod(j, columns)]
+            + previousGeneration[mod(i + 1, rows)][mod(j + 1, columns)];
+
+        nextGeneration[i][j] = (sum == 4) ? previousGeneration[i][j] : (sum == 3 ? (byte) 1 : 0);
+
+      }
+    }
+    return nextGeneration;
   }
 
   private int mod(int index, int divisor) {
@@ -59,50 +68,26 @@ public class GameOfLife {
     return remainder < 0 ? remainder + divisor : remainder;
   }
 
-  private List<String> readFromFile(String fileName) {
-    try {
-      Stream<String> inputStream = new BufferedReader(new FileReader(getFilePath(fileName)))
-        .lines();
-      return inputStream.collect(Collectors.toList());
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException("File " + fileName + " not found", e);
-    }
-  }
-
-  private GameData parse(List<String> inputData) {
-    String argsLine = inputData.remove(0);
-    String[] args = argsLine.split(",");
-
+  private Configuration parse(List<String> inputData) {
+    // parse the first line into args
+    String[] args = inputData.remove(0).split(",");
     int rows = Integer.parseInt(args[0]);
     int columns = Integer.parseInt(args[1]);
     int iterations = Integer.parseInt(args[2]);
 
-    Byte[] oneDimensionalFigure =
-      Arrays.stream(String.join(" ", inputData).split(" "))
-        .map(s -> s.equals("X") ? (byte) 1 : 0)
-        .toArray(Byte[]::new);
+    // concatenate the rest of the list into one line and convert to 2D byte array.
+    byte[][] inputFigure = convertToMatrix(inputData, rows, columns);
 
-    byte[][] inputFigure = convertToMatrix(oneDimensionalFigure, rows, columns);
-    return new GameData(rows, columns, iterations, inputFigure);
+    return new Configuration(rows, columns, iterations, inputFigure);
   }
 
-  private void writeToFile(String fileName, String resultFigure) {
-    File file = new File(getFilePath(fileName));
-    try (PrintWriter out = new PrintWriter(new FileWriter(file, false))) {
-      out.println(resultFigure);
-    } catch (IOException e) {
-      throw new RuntimeException("File " + fileName + " not found", e);
-    }
-  }
-
-  private String toString(byte[][] figure) {
+  private String convertToString(byte[][] matrix) {
     StringBuilder figureBuilder = new StringBuilder();
-    int rows = figure.length;
-    int columns = figure[0].length;
+    int columns = matrix[0].length;
 
-    for (int i = 0; i < rows; i++) {
+    for (byte[] bytes : matrix) {
       for (int j = 0; j < columns; j++) {
-        char character = (figure[i][j] == 1) ? 'X' : 'O';
+        char character = (bytes[j] == 1) ? 'X' : 'O';
         figureBuilder.append(character).append(' ');
       }
       figureBuilder.deleteCharAt(figureBuilder.length() - 1).append('\n');
@@ -110,17 +95,43 @@ public class GameOfLife {
     return figureBuilder.deleteCharAt(figureBuilder.length() - 1).toString();
   }
 
-  private byte[][] convertToMatrix(Byte[] array, int rows, int columns) {
-    byte[][] result = new byte[rows][columns];
+  private byte[][] convertToMatrix(List<String> figure, int rows, int columns) {
+    String[] figureValues = String.join(" ", figure).split(" ");
+
+    if (figureValues.length != (rows * columns)) {
+      throw new RuntimeException("Invalid input data");
+    }
+
+    byte[][] matrix = new byte[rows][columns];
     int offset = 0;
 
     for (int i = 0; i < rows; i++, offset += columns) {
       for (int j = 0; j < columns; j++) {
-        result[i][j] = array[j + offset];
+        byte value = figureValues[j + offset].equals("X") ? (byte) 1 : 0;
+        matrix[i][j] = value;
       }
     }
 
-    return result;
+    return matrix;
+  }
+
+  private List<String> readFromFile(String inputFileName) {
+    try {
+      Stream<String> inputStream = new BufferedReader(new FileReader(getFilePath(inputFileName)))
+          .lines();
+      return inputStream.collect(Collectors.toList());
+    } catch (FileNotFoundException e) {
+      throw new RuntimeException("File " + inputFileName + " not found", e);
+    }
+  }
+
+  private void writeToFile(String outputFileName, String resultFigure) {
+    File file = new File(getFilePath(outputFileName));
+    try (PrintWriter out = new PrintWriter(new FileWriter(file, false))) {
+      out.println(resultFigure);
+    } catch (IOException e) {
+      throw new RuntimeException("File " + outputFileName + " not found", e);
+    }
   }
 
   private String getFilePath(String fileName) {
@@ -128,18 +139,18 @@ public class GameOfLife {
       .getFile() + fileName;
   }
 
-  private class GameData {
+  private class Configuration {
 
     private final int rows;
     private final int columns;
     private final int iterations;
-    private final byte[][] initFigure;
+    private final byte[][] initialFigure;
 
-    public GameData(int rows, int columns, int iterations, byte[][] initFigure) {
+    public Configuration(int rows, int columns, int iterations, byte[][] initialFigure) {
       this.rows = rows;
       this.columns = columns;
       this.iterations = iterations;
-      this.initFigure = initFigure;
+      this.initialFigure = initialFigure;
     }
 
     public int getRows() {
@@ -154,8 +165,8 @@ public class GameOfLife {
       return iterations;
     }
 
-    public byte[][] getInitFigure() {
-      return initFigure;
+    public byte[][] getInitialFigure() {
+      return initialFigure;
     }
   }
 
--- module1/src/test/java/net/broscorp/gamelife/GameOfLifeTest.java
+++ module1/src/test/java/net/broscorp/gamelife/GameOfLifeTest.java
@@ -11,7 +11,6 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-//@Disabled
 class GameOfLifeTest {
   GameOfLife game = new GameOfLife();
 
