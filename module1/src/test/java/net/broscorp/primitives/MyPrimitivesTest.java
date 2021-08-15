aaa324b53ca7446378c23af2a047c9ea01f0e909
IhorYutinDev <ihor.yutin.dev@gmail.com>
2021-08-15 10:21:21 +0300

    task2 primitives

---
+++ module1/src/test/java/net/broscorp/primitives/MyPrimitivesTest.java
@@ -0,0 +1,48 @@
+package net.broscorp.primitives;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MyPrimitivesTest {
+
+  //numeric type overflow tests
+  @Test
+  void intOverflowIncrementationTest() {
+    int a = Integer.MAX_VALUE;
+    assertEquals(Integer.MIN_VALUE, ++a);
+  }
+
+  @Test
+  void intOverflowMultiplicationTest() {
+    int a = 2_000_000;
+    int b = 2_000_000;
+    int c = a * b;
+
+    assertNotEquals(4_000_000_000_000L, c);
+    assertEquals(1385447424, c);
+  }
+
+  //conversion problems between primitive types test
+  @Test
+  void longToIntConversionProblemTest() {
+    long a = 2_000_000_000_000L;
+    int b = (int) a;
+
+    assertNotEquals(2_000_000_000_000L, b);
+    assertEquals(-1454759936, b);
+  }
+
+  //floating point error test
+  @Test
+  void floatingPointErrorTest() {
+    float a = 10;
+
+    float aDiv = a / 3; //expected result    3.3333333333333333
+                        // but actual result 3.3333332538604736
+
+    assertEquals(3.3333332538604736, aDiv, 0);
+  }
+
+}
