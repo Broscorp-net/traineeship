f95594df5f1c6d6f9c8dc0f847c48b1174e35bd2
dmitriy tverdokhlibov <dmitriytverdokhlibov@yahoo.com>
2021-08-16 19:27:24 +0200

    valueref

--- module1/src/test/java/net/broscorp/valueref/FooTest.java
+++ module1/src/test/java/net/broscorp/valueref/FooTest.java
@@ -1,6 +1,6 @@
 package net.broscorp.valueref;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
@@ -23,6 +23,8 @@ class FooTest {
   @Test
   void foo() throws Exception {
     obj.foo(10, list);
+    assertEquals(4, list.size()); // True
+
     /* добавляем элемент 'd' в list. Длинна list становиться 4 так
     как у нас было 3 элемента. Значение параметра i останется прежним после вызова
     метода foo().
@@ -31,6 +33,6 @@ class FooTest {
     это никак не влияет.
     Второй параметр передача по ссылке. Вызываемый метод не создает свою копию,
     а ссылается на исходное значение.*/
-    assertEquals(4, list.size()); // True
+
   }
 }
