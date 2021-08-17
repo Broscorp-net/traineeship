3519971df93b803a36c1aa651cac95e391a7696a
IhorYutinDev <ihor.yutin.dev@gmail.com>
2021-08-17 11:34:36 +0300

    task valueRef delete StyledClass, add commit

--- module1/src/test/java/net/broscorp/valueref/MyFooTest.java
+++ module1/src/test/java/net/broscorp/valueref/MyFooTest.java
@@ -10,15 +10,14 @@ import org.junit.jupiter.api.Test;
 public class MyFooTest {
 
   private List<String> list = new ArrayList<>();//created list as data structure in any place of memory
-  private int testIntArg = 20;
+  private int testIntArg = 20;//int - is primitive type, and situated in stack
 
   @Test
   void fooTest() {
-
-    // testIntArg = 35 in the scopes of method 'foo' after 20+15, but this result was destroyed after
-    // executing code in method 'foo' and returning (changes was local), and value testIntArg = 20 in the test
-    // list.add("d") writes value "d" in data structure, which situated not in the scopes of the method 'foo',
-    // because 'list' was declared not in the scopes of method 'foo'
+//    testIntArg = 35 in the scopes of method 'foo' after 20+15, but this result was destroyed after executing code
+//    in method 'foo' and returning (changes was local), and value testIntArg = 20 in the test
+//    list.add("d") writes value "d" in data structure, which situated not in the scopes of the method 'foo',
+//    because 'list' was declared not in the scopes of method 'foo'
     new Foo().foo(testIntArg,
         list);//after executing method and return testIntArg = 20, list has 1 element - "d" with index '0'
 
@@ -28,7 +27,7 @@ public class MyFooTest {
         list.get(0));//list.get(0).equals("d"), contains 1 element - "d", with index '0'
 
     assertNotEquals(35, testIntArg);//testIntArg not changed in the body of test, changes
-                                              //20+15=35 was local, only in the scopes of method 'foo'
+    //20+15=35 was local, only in the scopes of method 'foo'
 
     assertEquals(20, testIntArg);     //in test testIntArg = 20, method 'foo' not changed it;
   }
