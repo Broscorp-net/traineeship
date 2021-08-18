d199e93fd7519b81c1800bdf38945ffe360286e4
dmitriy tverdokhlibov <dmitriytverdokhlibov@yahoo.com>
2021-08-18 20:00:30 +0200

    equals.hashcode

---
+++ module1/src/main/java/net/broscorp/equals/hashcode/Person.java
@@ -0,0 +1,71 @@
+package net.broscorp.equals.hashcode;
+
+public class Person implements Cloneable {
+  private String name;
+  private int age;
+  private String inn;
+
+  public Person(String name, int age, String inn) {
+    this.name = name;
+    this.age = age;
+    this.inn = inn;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getInn() {
+    return inn;
+  }
+
+  public void setInn(String inn) {
+    this.inn = inn;
+  }
+
+  @Override
+  public String toString() {
+    return "Person name: " + this.name + ", age: " + this.age + ", inn: " +
+        this.inn;
+  }
+
+  @Override
+  public boolean equals(Object person) {
+    if (person == this) {
+      return true;
+    }
+
+    if (person == null) {
+      return false;
+    }
+
+    if(person.getClass().equals(Person.class)) {
+      Person currentPerson = (Person) person;
+      return this.name.equals(currentPerson.name)
+          && this.age == currentPerson.age
+          && this.inn.equals(currentPerson.inn);
+    }
+
+    return false;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = 17;
+    result = 31 * result + age;
+    result = 31 * result + (inn == null ? 0 : inn.hashCode());
+    return result;
+  }
+}
---
+++ module1/src/test/java/net/broscorp/equals/hashcode/PersonTest.java
@@ -0,0 +1,83 @@
+package net.broscorp.equals.hashcode;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PersonTest {
+  ArrayList<Person> persons;
+  StringBuilder message;
+  Person bob;
+  Person bob2;
+  Person bob3;
+  Person alice;
+  Person chris;
+  Person radim;
+  Person daniel;
+
+  @BeforeEach
+  void init() {
+    bob = new Person("Bob", 23, "12345");
+    bob2 = new Person("Bob", 23, "12345");
+    bob3 = new Person("Bob", 23, "12345");
+    alice = new Person("Alice", 23, "12345");
+    chris = new Person("Chris", 29, "12113");
+    radim = new Person("Radim", 32, "14113");
+    daniel = new Person("Daniel", 31,"13276");
+  }
+
+  @Test
+  void personInvariantTest() {
+    // reflexive
+    assertTrue(bob.equals(bob));
+
+    // symmetric
+    assertTrue(bob.equals(bob2) && bob2.equals(bob));
+
+    // transitive
+    assertTrue(bob.equals(bob2) && bob2.equals(bob3)
+        && bob.equals(bob3));
+
+    //consistent
+    assertTrue(bob.equals(bob2) && bob.equals(bob2)
+        && bob.equals(bob2) && bob.equals(bob2));
+
+    // null
+    assertFalse(bob.equals(null));
+  }
+
+  @Test
+  void sameHashcodeTest() {
+    persons = new ArrayList<>();
+    message = new StringBuilder();
+    persons.add(bob);
+    persons.add(bob2);
+    persons.add(alice);
+    persons.add(chris);
+    persons.add(radim);
+    persons.add(daniel);
+
+    for (int i = 0; i < persons.size(); i++) {
+      Person current = persons.get(i);
+      for (int j = 1; j < persons.size(); j++) {
+        Person next = persons.get(j);
+        if (!current.equals(next)) {
+          if (current.hashCode() == next.hashCode()) {
+            message.append("current person: ").append(current.getName())
+                .append(" not equals ").append("next person: ")
+                .append(next.getName()).append(" but hashcode equal ")
+                .append(current.hashCode()).append(" == ").append(next.hashCode());
+            break;
+          }
+        }
+      }
+    }
+
+
+  }
+
+
+}
