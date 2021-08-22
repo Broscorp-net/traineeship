ea38d732bff28bd4508fc5bb255aa3ef0925c8bc
Viktor <lebedevit4@gmail.com>
2021-08-22 17:26:22 +0300

    style code

--- module1/src/test/java/net/broscorp/strings/StringAppTest.java
+++ module1/src/test/java/net/broscorp/strings/StringAppTest.java
@@ -1,8 +1,8 @@
 package net.broscorp.strings;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
