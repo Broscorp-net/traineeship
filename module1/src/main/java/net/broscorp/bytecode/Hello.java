15a8f0e64584073f770726274e3c2ccf7dbbb06f
dmitriy tverdokhlibov <dmitriytverdokhlibov@yahoo.com>
2021-08-31 00:21:49 +0200

    fix problems with checkstyle

--- module1/src/main/java/net/broscorp/bytecode/Hello.java
+++ module1/src/main/java/net/broscorp/bytecode/Hello.java
@@ -1,7 +1,17 @@
 package net.broscorp.bytecode;
+/**
+ * Program Hello.
+ *
+ * @author  Dmitriy Tverdokhlibov
+ * @version  1.0
+ * @since 2021-08-30
+ */
 
 public class Hello {
-
+  /**.
+   * The class code changes and analyze
+   * how the bytecode changes
+   */
   public static void main(String[] args) {
     int count = 0;
     while (count < 1) {
