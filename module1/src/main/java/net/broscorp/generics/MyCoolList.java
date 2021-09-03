b27522eb6b8ce3db4ef857a55eb64e9e3f31e1bf
PavelHorishny <89547838+PavelHorishny@users.noreply.github.com>
2021-09-03 14:22:59 +0300

    add sun jars

--- module1/src/main/java/net/broscorp/generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/generics/MyCoolList.java
@@ -1,29 +1,29 @@
 package net.broscorp.generics;
 
 import java.util.function.Function;
-import jdk.jshell.spi.ExecutionControl.NotImplementedException;
-//import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+//import jdk.jshell.spi.ExecutionControl.NotImplementedException;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class MyCoolList {
 
-  public void add(Object o) throws NotImplementedException {
-    throw new NotImplementedException(" ");
+  public void add(Object o){
+    throw new NotImplementedException();
   }
 
-  public Object get(int index) throws NotImplementedException {
-    throw new NotImplementedException(" ");
+  public Object get(int index)  {
+    throw new NotImplementedException();
   }
 
   public Object remove(int index) throws NotImplementedException {
-    throw new NotImplementedException(" ");
+    throw new NotImplementedException();
   }
 
-  public MyCoolList map(Function f) throws NotImplementedException {
-    throw new NotImplementedException(" ");
+  public MyCoolList map(Function f)  {
+    throw new NotImplementedException();
   }
 
-  public int size() throws NotImplementedException {
-    throw new NotImplementedException(" ");
+  public int size()  {
+    throw new NotImplementedException();
   }
 
 }
