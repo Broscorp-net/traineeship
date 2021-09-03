2975d087fab5d51517d63f9d842e0f9bebca653c
PavelHorishny <89547838+PavelHorishny@users.noreply.github.com>
2021-09-03 18:57:08 +0300

    task

---
+++ module1/src/main/java/net/broscorp/boxing/Boxing.java
@@ -0,0 +1,54 @@
+package net.broscorp.boxing;
+
+public class Boxing {
+
+  /**{ @summary This is summary.}
+   * @param a int.
+   * @param b string.
+   * @return boolean.
+   */
+  public static boolean returnTrue(int a, String b) {
+    Integer i = a;
+    Integer j = Integer.parseInt(b);
+    if (i == j) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**{ @summary This is summary.}
+   * @param a int.
+   * @return boolean.
+   */
+  public static boolean returnFalseOne(int a) {
+    Integer i = new Integer(a);
+    Integer j = new Integer(a);
+    return i == j;
+  }
+
+  /**{ @summary This is summary.}
+   * @param a int.
+   * @return boolean.
+   * @throws Exception if less.
+   */
+  public static boolean returnFalseTwo(int a) throws Exception {
+    if (a > 127 || a < -128) {
+      Integer i = a;
+      Integer j = a;
+      return i == j;
+    } else {
+      throw new Exception();
+    }
+  }
+
+  /**{ @summary This is summary.}
+   * @return int.
+   */
+  public static int returnException() {
+    Integer i = null;
+    int j = i.intValue();
+    return j;
+  }
+
+}
---
+++ module1/src/test/java/net/broscorp/boxing/BoxingTest.java
@@ -0,0 +1,34 @@
+package net.broscorp.boxing;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class BoxingTest {
+
+  @Test
+  void returnException() {
+    Assertions.assertThrows(NullPointerException.class, () -> {
+      Integer i = null;
+      int j = i.intValue();
+      System.out.println(j);
+    });
+  }
+
+  @Test
+  void returnTrue() {
+    assertTrue(Boxing.returnTrue(5, "5"));
+  }
+
+  @Test
+  void returnFalseFirstVar() {
+    assertFalse(Boxing.returnFalseOne(5));
+  }
+
+  @Test
+  void returnFalseSecondVar() throws Exception {
+    assertFalse(Boxing.returnFalseTwo(129));
+  }
+}
