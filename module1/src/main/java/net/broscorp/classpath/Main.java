3ccca358cb20c8981506098c9d2f8096f8c275c7
IhorYutinDev <ihor.yutin.dev@gmail.com>
2021-09-07 14:56:32 +0300

    task classpath add Main and PrintText classes

---
+++ module1/src/main/java/net/broscorp/classpath/Main.java
@@ -0,0 +1,25 @@
+package net.broscorp.classpath;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+  public static String getText() {
+    System.out.println("Enter text, please");
+    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    String text = "";
+    try {
+      text = bufferedReader.readLine();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    return text;
+  }
+
+  public static void main(String[] args) {
+    PrintText.printToConsole(getText());
+  }
+
+}
---
+++ module1/src/main/java/net/broscorp/classpath/PrintText.java
@@ -0,0 +1,8 @@
+package net.broscorp.classpath;
+
+public class PrintText {
+
+  public static void printToConsole(String text) {
+    System.out.println(text);
+  }
+}
