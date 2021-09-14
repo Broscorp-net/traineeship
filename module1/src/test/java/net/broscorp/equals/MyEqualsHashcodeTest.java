318f7b26ae9c51464739942b68fe72acca2ec1fd
IhorYutinDev <ihor.yutin.dev@gmail.com>
2021-09-14 10:10:53 +0300

    Add test for 5 rules

--- module1/src/test/java/net/broscorp/equals/MyEqualsHashcodeTest.java
+++ module1/src/test/java/net/broscorp/equals/MyEqualsHashcodeTest.java
@@ -1,13 +1,71 @@
 package net.broscorp.equals;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import net.broscorp.equals.hashcode.Box;
 import org.junit.jupiter.api.Test;
 
 public class MyEqualsHashcodeTest {
+
+  //It is reflexive: for any non-null reference value x, x.equals(x) should return true
+  @Test
+  void boxReflexiveTest() {
+    Box box1 = new Box(20, 30);
+    Box box2 = box1;
+
+    assertEquals(box1, box2);
+  }
+
+  //It is symmetric: for any non-null reference values x and y, x.equals(y) should return true if
+  // and only if y.equals(x) returns true.
+  @Test
+  void boxSymmetricTest() {
+    Box box1 = new Box(20, 30);
+    Box box2 = new Box(20, 30);
+
+    assertEquals(box1, box2);
+    assertEquals(box2, box1);
+  }
+
+  //  It is transitive: for any non-null reference values x, y, and z, if x.equals(y)
+  //  returns true and y.equals(z) returns true, then x.equals(z) should return true
+  @Test
+  void boxTransitiveTest() {
+    Box box1 = new Box(20, 30);
+    Box box2 = box1;
+    Box box3 = box1;
+
+    assertEquals(box2, box1);
+    assertEquals(box3, box1);
+    assertEquals(box2, box3);
+  }
+
+  //It is consistent: for any non-null reference values x and y, multiple invocations of x.equals(y)
+  //consistently return true or consistently return false, provided no information used in equals
+  //comparisons on the objects is modified.
+  @Test
+  void boxConsistentlyTest() {
+    Box box1 = new Box(20, 30);
+    Box box2 = new Box(20, 30);
+
+    for (int i = 0; i < 10; i++) {
+      assertEquals(box1, box2);
+    }
+  }
+
+  //For any non-null reference value x, x.equals(null) should return false.
+  @Test
+  void boxNullReferenceTest() {
+    Box box1 = new Box(20, 30);
+    Box box2 = null;
+
+    assertFalse(box1.equals(box2));
+  }
+
 
   @Test
   void boxHashcodeTest() {
