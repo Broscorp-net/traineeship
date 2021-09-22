44f02873c9641b9cf9620876b04be235fd5955b5
PavelHorishny <89547838+PavelHorishny@users.noreply.github.com>
2021-09-22 23:07:46 +0300

    Script, bytecodes

---
+++ module1/src/main/java/net/broscorp/bytecode/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+  public static void main(String[] args) {
+    int num=0;
+    double a = 2.0;
+    String hi = "Hello!";
+    for (int i =0; i<10; i++){
+      num++;
+    }
+    double sum = num+a;
+    String test = hi.toLowerCase();
+    System.out.println(hi);
+  }
+
+}
---
+++ module1/src/main/java/net/broscorp/bytecode/script.cmd
@@ -0,0 +1,6 @@
+javac Test.java
+pause
+javap -c Test.class>check1.txt
+pause
+java Test
+pause
