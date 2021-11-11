9471627982deebd469765cfe99ab5c9e7b1122d6
pesua <pesua.mail@gmail.com>
2021-11-11 11:48:50 +0200

    Remove reference implementation

--- module1/src/main/java/net/broscorp/gcimpl/gc/GarbageCollectorImplementation.java
+++ module1/src/main/java/net/broscorp/gcimpl/gc/GarbageCollectorImplementation.java
@@ -1,12 +1,7 @@
 package net.broscorp.gcimpl.gc;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Deque;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import net.broscorp.gcimpl.model.ApplicationBean;
 import net.broscorp.gcimpl.model.HeapInfo;
@@ -16,26 +11,6 @@ public class GarbageCollectorImplementation implements GarbageCollector {
 
   @Override
   public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
-    return findUnreachableNodes(heap.getBeans(), stack.getStack());
-  }
-
-  private List<ApplicationBean> findUnreachableNodes(
-      Map<String, ApplicationBean> heap, Deque<StackInfo.Frame> stack) {
-    List<ApplicationBean> rootBeans =
-        stack.stream().flatMap(f -> f.getParameters().stream()).collect(Collectors.toList());
-    List<ApplicationBean> reachableBeans =
-        rootBeans.stream().flatMap(this::identifyLiveRelations).collect(Collectors.toList());
-
-    ArrayList<ApplicationBean> allBeans = new ArrayList<>(heap.values());
-    allBeans.removeAll(rootBeans);
-    allBeans.removeAll(reachableBeans);
-    return allBeans;
-  }
-
-  private Stream<ApplicationBean> identifyLiveRelations(ApplicationBean root) {
-    Collection<ApplicationBean> relations = root.getFieldValues().values();
-    return Stream.concat(
-        relations.stream().filter(r -> !relations.contains(r)).flatMap(this::identifyLiveRelations),
-        relations.stream());
+      return Collections.emptyList();
   }
 }
