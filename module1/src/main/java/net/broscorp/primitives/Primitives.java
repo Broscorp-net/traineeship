a2f455fbe3bee5604b4800237723f25dd09d0ff1
VladimirKivg <cheremuha1@gmal.com>
2021-11-16 01:51:35 +0200

    make class Primitives and tests at it. Also fixed style in all the project

---
+++ module1/src/main/java/net/broscorp/primitives/Primitives.java
@@ -0,0 +1,23 @@
+package net.broscorp.primitives;
+
+public class Primitives {
+
+  public int add(int a, int b) {
+    return a + b;
+  }
+
+  public int conversion(long a) {
+    return (int) a;
+  }
+
+  public double conversion2(float a) {
+    double b = a;
+    return b;
+  }
+
+  public double count(double a,double b) {
+    double number = 1;
+    return number + a + b;
+  }
+
+}
