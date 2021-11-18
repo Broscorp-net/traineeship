786db201381ffc18e62aef71a9cb7d7fb52d5946
07krava <dim-kravchu@ukr.net>
2021-11-18 11:51:02 +0200

    fix checkstyle

--- module1/src/main/java/net/broscorp/checkstyle/StyledClass.java
+++ module1/src/main/java/net/broscorp/checkstyle/StyledClass.java
@@ -1,7 +1,8 @@
 package net.broscorp.checkstyle;
 
 public class StyledClass {
-    public static void main(String[] args) {
-        System.out.println ("Hello checkstyle");
-    }
+
+  public static void main(String[] args) {
+    System.out.println("Hello checkstyle");
+  }
 }
