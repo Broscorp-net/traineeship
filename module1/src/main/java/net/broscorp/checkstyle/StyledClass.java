1d4c23dc892017292a47c102b05cdca612ce6376
Max <maxmailpol@gmail.com>
2021-11-20 20:00:33 +0200

    Checkstyle

--- module1/src/main/java/net/broscorp/checkstyle/StyledClass.java
+++ module1/src/main/java/net/broscorp/checkstyle/StyledClass.java
@@ -2,6 +2,6 @@ package net.broscorp.checkstyle;
 
 public class StyledClass {
   public static void main(String[] args) {
-     System.out.println("Hello checkstyle");
+    System.out.println("Hello checkstyle");
   }
 }
