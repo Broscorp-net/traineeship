629fc33fe8bb4ca2783ba45cef34d8ba4a692754
Denys Tolkachov <mrmarkutan@gmail.com>
2021-11-26 11:00:21 +0200

    Added javadoc to main method

--- module1/src/main/java/net/broscorp/classpath/ReadFromConsole.java
+++ module1/src/main/java/net/broscorp/classpath/ReadFromConsole.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class ReadFromConsole {
 
+  /**.
+   *Main method to read input and write it to the console
+   */
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Write your message: ");
