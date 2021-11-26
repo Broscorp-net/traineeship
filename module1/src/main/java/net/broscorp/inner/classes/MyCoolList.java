c259ff33f6ee0a2f33b3254c4d4f5aa0bac6d672
Denys Tolkachov <mrmarkutan@gmail.com>
2021-11-26 14:09:07 +0200

    Added javadoc comment

--- module1/src/main/java/net/broscorp/inner/classes/MyCoolList.java
+++ module1/src/main/java/net/broscorp/inner/classes/MyCoolList.java
@@ -2,7 +2,7 @@ package net.broscorp.inner.classes;
 
 import java.util.function.Function;
 
-public interface MyCoolList<T> extends Iterable<T>{
+public interface MyCoolList<T> extends Iterable<T> {
 
   void add(T t);
 
