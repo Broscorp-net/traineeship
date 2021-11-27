030a726733d3fcfc9517883a1c79f0951f43efe3
igor <igoryan0811@gmail.com>
2021-11-27 20:03:38 +0100

    primitives

--- module1/src/main/java/net/broscorp/primitives/Primitives.java
+++ module1/src/main/java/net/broscorp/primitives/Primitives.java
@@ -1,8 +1,9 @@
 package net.broscorp.primitives;
 
-import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 public class Primitives {
   public static int plusIntOverflow() {
