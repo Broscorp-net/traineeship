0cc60557ad6d20bf7c2c394178c1cd62d20842d6
igor <igoryan0811@gmail.com>
2021-11-27 20:12:55 +0100

    primitives

--- module1/src/main/java/net/broscorp/primitives/Primitives.java
+++ module1/src/main/java/net/broscorp/primitives/Primitives.java
@@ -1,6 +1,5 @@
 package net.broscorp.primitives;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -104,50 +103,5 @@ public class Primitives {
     float f1 = (float) 0.35;
     double d = f1;
     return d;
-  }
-
-  @Test
-  public void testPlusIntOverflow() {
-    assertEquals(Integer.MIN_VALUE, Primitives.plusIntOverflow());
-  }
-
-  @Test
-  public void testMinusIntOverflow() {
-    assertEquals(Integer.MAX_VALUE, Primitives.minusIntOverflow());
-  }
-
-  @Test
-  public void testPlusLongOverflow() {
-    assertEquals(Long.MIN_VALUE, Primitives.plusLongOverflow());
-  }
-
-  @Test
-  public void testMinusLongOverflow() {
-    assertEquals(Long.MAX_VALUE, Primitives.minusLongOverflow());
-  }
-
-  @Test
-  public void testPlusShortOverflow() {
-    assertEquals(Short.MIN_VALUE, Primitives.plusShortOverflow());
-  }
-
-  @Test
-  public void testMinusShortOverflow() {
-    assertEquals(Short.MAX_VALUE, Primitives.minusShortOverflow());
-  }
-
-  @Test
-  public void testConvertLongToIntMax() {
-    assertEquals(-1, Primitives.convertLongToIntMax());
-  }
-
-  @Test
-  public void testConvertLongToIntMin() {
-    assertEquals(0, Primitives.convertLongToIntMin());
-  }
-
-  @Test
-  public void testConvertLongToIntSome() {
-    assertEquals(-2147483648, Primitives.convertLongToIntSome());
   }
 }
---
+++ module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
@@ -0,0 +1,61 @@
+package net.broscorp.primitives;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PrimitivesTest {
+
+  @Test
+  public void testPlusIntOverflow() {
+    assertEquals(Integer.MIN_VALUE, Primitives.plusIntOverflow());
+  }
+
+  @Test
+  public void testMinusIntOverflow() {
+    assertEquals(Integer.MAX_VALUE, Primitives.minusIntOverflow());
+  }
+
+  @Test
+  public void testPlusLongOverflow() {
+    assertEquals(Long.MIN_VALUE, Primitives.plusLongOverflow());
+  }
+
+  @Test
+  public void testMinusLongOverflow() {
+    assertEquals(Long.MAX_VALUE, Primitives.minusLongOverflow());
+  }
+
+  @Test
+  public void testPlusShortOverflow() {
+    assertEquals(Short.MIN_VALUE, Primitives.plusShortOverflow());
+  }
+
+  @Test
+  public void testMinusShortOverflow() {
+    assertEquals(Short.MAX_VALUE, Primitives.minusShortOverflow());
+  }
+
+  @Test
+  public void testConvertLongToIntMax() {
+    assertEquals(-1, Primitives.convertLongToIntMax());
+  }
+
+  @Test
+  public void testConvertLongToIntMin() {
+    assertEquals(0, Primitives.convertLongToIntMin());
+  }
+
+  @Test
+  public void testConvertLongToIntSome() {
+    assertEquals(-2147483648, Primitives.convertLongToIntSome());
+  }
+  @Test
+  public void testConvertFloatToDouble() {
+    assertEquals(3.4028234663852886E38, Primitives.convertFloatToDouble());
+  }
+  @Test
+  public void testConvertFloatToDoubleSome() {
+    assertEquals(0.3499999940395355, Primitives.convertFloatToDoubleSome());
+  }
+}
