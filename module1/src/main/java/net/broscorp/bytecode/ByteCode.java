ce338ab83d42c426fddf2e01627def978b3435f4
Max <maxmailpol@gmail.com>
2021-12-05 21:57:28 +0200

    bytecode

--- module1/src/main/java/net/broscorp/bytecode/ByteCode.java
+++ module1/src/main/java/net/broscorp/bytecode/ByteCode.java
@@ -1,23 +1,27 @@
 package net.broscorp.bytecode;
 
 public class ByteCode {
+
+  /**
+  * Test class
+  */
   public static void main(String[] args) {
     int x = 1;
-      for (int i = 0; i < 5; i++) {
-          x++;
-      }
-      System.out.println(x);
+    for (int i = 0; i < 5; i++) {
+      x++;
+    }
+    System.out.println(x);
     someMethod();
     helloMethod();
   }
 
   static void someMethod() {
-      int a = 1;
-      int b = 2;
-      System.out.println(a + b);
+    int a = 1;
+    int b = 2;
+    System.out.println(a + b);
   }
 
   static void helloMethod() {
-      System.out.println("Hello world");
+    System.out.println("Hello world");
   }
 }
