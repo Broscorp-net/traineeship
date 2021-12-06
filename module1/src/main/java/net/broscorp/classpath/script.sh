a198c63b241936e08974d340b18820e20615ca4c
top802 <tatrun89@gmail.com>
2021-12-06 13:43:32 +0200

    wip classpath: fixed script

--- module1/src/main/java/net/broscorp/classpath/script.sh
+++ module1/src/main/java/net/broscorp/classpath/script.sh
@@ -1,5 +1,5 @@
 javac -d class ConsoleValue.java ConsolePrintValue.java
 java  -cp class net.broscorp.classpath.ConsoleValue
-jar cmf MANIFEST.MF printStuff.jar class/
+jar cfm printStuff.jar MANIFEST.MF -C class .
 java -jar printStuff.jar
 cmd /k
