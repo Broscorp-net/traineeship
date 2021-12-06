5467b48eafe1094fb22cd65ffb7e471ea73d1efd
top802 <tatrun89@gmail.com>
2021-12-07 00:35:38 +0200

    wip hashcode: added test for hashcode comparisons

--- module1/src/test/java/net/broscorp/equals/hashcode/EqualsHashCodeTest.java
+++ module1/src/test/java/net/broscorp/equals/hashcode/EqualsHashCodeTest.java
@@ -57,4 +57,31 @@ public class EqualsHashCodeTest {
     Assertions.assertEquals(false, playerList.get(1).equals(null));
   }
 
+
+//  Whenever it is invoked on the same object more than once during an execution of a Java application,
+//   the hashCode method must consistently return the same integer,
+//   provided no information used in equals comparisons on the object is modified.
+//   This integer need not remain consistent from one execution of an application to another execution of the same application.
+  @Test
+  void multipleInvokingHashCode() {
+    int hashCodeOne = playerList.get(0).hashCode();
+    int hashCodeTwo = playerList.get(0).hashCode();
+    Assertions.assertEquals(true, hashCodeOne == hashCodeTwo);
+  }
+
+//  If two objects are equal according to the equals(Object) method,
+//   then calling the hashCode method on each of the two objects must produce the same integer result.
+  @Test
+  void twoSameObjectHasSameHashCode () {
+    Assertions.assertEquals(playerList.get(4).equals(playerList.get(5)),
+        playerList.get(4).hashCode() == playerList.get(5).hashCode());
+  }
+
+//  It is not required that if two objects are unequal according to the equals(java.lang.Object) method,
+//  then calling the hashCode method on each of the two objects must produce distinct integer results.
+//  @Test
+//  void () {
+//
+//  }
+
 }
