bbb5271a90fb78879d2070d263c859077520ddff
top802 <tatrun89@gmail.com>
2021-12-17 17:00:59 +0200

    created game field

--- module1/src/main/java/net/broscorp/gamelife/GameOfLife.java
+++ module1/src/main/java/net/broscorp/gamelife/GameOfLife.java
@@ -10,6 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -17,32 +18,42 @@ import java.util.stream.Stream;
 public class GameOfLife {
 
   public static void main(String[] args) {
-    String fileName = "src/test/resources/inputGliderEasy.txt";
+
+    String[][] gameField = createGameFiledFromFile("inputGliderEasy.txt");
+
+  }
+
+  public void game(String fileNameInput, String fileNameOutput){
+
+    String[][] gameField = createGameFiledFromFile("inputGliderEasy.txt");
+  }
+
+  private static String[][] createGameFiledFromFile(String fileNameInput) {
+    String fileName = "src/test/resources/" + fileNameInput;
     List<String> lines = new ArrayList<>();
     try {
       lines = Files.readAllLines(Paths.get(fileName),
           StandardCharsets.UTF_8);
       System.out.println(lines);
-    } catch (IOException e) {
-      e.printStackTrace();
+    } catch (IOException exception) {
+      System.out.println("IOException! File don't found " + exception);
     }
-    String string = lines.get(3);
-    System.out.println(string);
+    String[] parametersFor2DArray = lines.get(0).split(",");
+    int width2DArray = Integer.parseInt(parametersFor2DArray[0]);
+    int high2DArray = Integer.parseInt(parametersFor2DArray[1]);
+    int amountIteration = Integer.parseInt(parametersFor2DArray[2]);
+    System.out.println(width2DArray + high2DArray + amountIteration);
+
     lines.remove(0);
-    lines.remove(1);
-    lines.remove(2);
-    System.out.println(lines);
+    List<String> stringsForBuildGameField = lines;
+    String[][] field = new String[width2DArray][high2DArray];
 
-//    String fileNameInput = "inputExample.txt";
-//    ClassLoader classLoader = GameOfLife.class.getClassLoader();
-//    Stream<String> gameStreamInput = new BufferedReader(
-//        new InputStreamReader(classLoader.getSystemResourceAsStream(fileNameInput))).lines();
-//    List<String> gameListExpected = gameStreamInput.collect(Collectors.toList());
-  }
-
-  public void game(String fileNameInput, String fileNameOutput){
-
-
+    for(int row = 0; row < width2DArray; row++){
+      for(int column = 0; column < high2DArray; column++){
+        field[row][column] = stringsForBuildGameField.get(column).split(" ")[column];
+      }
+    }
+    return field;
   }
 
 }
