0ee698ac5bb31066261b6d0257d2d7b6deea4968
borisk-bros <boris.kaminsky@gmail.com>
2022-01-02 16:18:37 +0300

    ExceptionsTest - done

--- module1/src/test/java/net/broscorp/exceptions/ExceptionsTest.java
+++ module1/src/test/java/net/broscorp/exceptions/ExceptionsTest.java
@@ -1,6 +1,8 @@
 package net.broscorp.exceptions;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,8 +13,21 @@ class ExceptionsTest {
   void testFileOpenWithException() {
     try (BufferedReader br =
         new BufferedReader(new FileReader("non_existing_path"))) {
+      // do nothing
     } catch (IOException e) {
-      System.out.println("Caught IOException");
+      // do nothing
+    } finally {
+      System.out.println("Bye");
+    }
+  }
+
+  @Test
+  void testCatchExceptionPredecessor() {
+    try (BufferedReader br =
+        new BufferedReader(new FileReader("non_existing_path"))) {
+      // do nothing
+    } catch (Throwable t) {
+      // do nothing
     } finally {
       System.out.println("Bye");
     }
