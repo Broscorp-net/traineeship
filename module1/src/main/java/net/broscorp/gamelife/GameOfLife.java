fc3764db7a37bc226b8a3b8c21042f82268a158a
borisk-bros <boris.kaminsky@gmail.com>
2022-01-10 11:53:16 +0300

    full implemented, debugging is needed

--- module1/src/main/java/net/broscorp/gamelife/GameOfLife.java
+++ module1/src/main/java/net/broscorp/gamelife/GameOfLife.java
@@ -1,5 +1,16 @@
 package net.broscorp.gamelife;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.FileWriter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class GameOfLife {
 
   // Following properties determines the game parameters and current status
@@ -19,7 +30,50 @@ public class GameOfLife {
   static final int LIVE_OVERPOPULATION_THRESHOLD = 3;
   static final int DEAD_REPRODUCTION_MATCH = 3;
 
-  void loadGame(String fileNameInput) {}
+  // Following constants represent the output coding of field values
+
+  static final char ALIVE = 'X';
+  static final char DEAD = 'O';
+  static final char DELIMITER = ' ';
+
+  void parseGameParameters(String firstLine) {
+
+    String[] params = firstLine.split(",");
+
+    xSize = Integer.parseInt(params[0]);
+    ySize = Integer.parseInt(params[1]);
+    iterations = Integer.parseInt(params[2]);
+  }
+
+  boolean parseValue(String fieldVal) {
+    return fieldVal.charAt(0) == ALIVE;
+  }
+
+  void parseGameField(List<String> gameListInput) {
+
+    field = new boolean[ySize][xSize];
+
+    for (int yLoc = 0; yLoc <= ySize - 1; yLoc++) {
+      for (int xLoc = 0; xLoc <= xSize - 1; xLoc++) {
+
+        String row = gameListInput.get(yLoc);
+        for (String fieldVal : row.split(" ")) {
+          field[yLoc][xLoc] = parseValue(fieldVal);
+        }
+      }
+    }
+  }
+
+  void loadGame(String fileNameInput) {
+
+    Stream<String> gameStreamInput = new BufferedReader(
+        new InputStreamReader(ClassLoader.getSystemResourceAsStream(fileNameInput))).lines();
+    List<String> gameListInput = gameStreamInput.collect(Collectors.toList());
+
+    parseGameParameters(gameListInput.remove(0));
+
+    parseGameField(gameListInput);
+  }
 
   boolean isAlive(int xLoc, int yLoc) {
     return field[xLoc][yLoc];
@@ -103,7 +157,37 @@ public class GameOfLife {
     field = nextIterField;
   }
 
-  void saveGame(String fileNameOutput) {}
+  void saveGame(String fileNameOutput) {
+
+    StringBuilder output = new StringBuilder();
+
+    for (int yLoc = 0; yLoc <= ySize - 1; yLoc++) {
+      for (int xLoc = 0; xLoc <= xSize - 1; xLoc++) {
+
+        char charAtField = field[yLoc][xLoc] ? ALIVE : DEAD;
+        output.append(charAtField);
+        output.append(DELIMITER);
+      }
+
+      output.append(System.lineSeparator());
+
+    }
+
+    // FOR DEBUG PURPOSES
+    System.out.println(output.toString());
+
+    File file = new File(GameOfLife.class.getClassLoader().getResource(".").getFile() + fileNameOutput);
+
+    // FOR DEBUG PURPOSES
+    System.out.println(file.getPath());
+
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+      writer.write(output.toString());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+
+  }
 
   public void game(String fileNameInput, String fileNameOutput){
 
