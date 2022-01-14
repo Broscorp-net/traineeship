42d57d8a81ad24bbe1deea7949d97e383f181c5d
borisk-bros <boris.kaminsky@gmail.com>
2022-01-14 17:59:42 +0200

    first proxy

--- module1/src/main/java/net/broscorp/gcimpl/GarbageCollectorImplementation.java
+++ module1/src/main/java/net/broscorp/gcimpl/GarbageCollectorImplementation.java
@@ -3,6 +3,7 @@ package net.broscorp.gcimpl;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,6 +13,22 @@ public class GarbageCollectorImplementation implements GarbageCollector {
   public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
     Collection<ApplicationBean> beans = heap.getBeans().values();
     Deque<StackInfo.Frame> frames = stack.getStack();
-    return Collections.emptyList();
+
+    List<ApplicationBean> markedForDeletion = new LinkedList<>();
+
+    // go over beans, if not referenced by any stack entries - should be marked for deletion
+    for (ApplicationBean bean : beans) {
+      boolean isReferenced = false;
+      for (StackInfo.Frame frame : frames) {
+        if (frame.getParameters().contains(bean)) {
+          isReferenced = true;
+          break;
+        }
+      }
+      if (!isReferenced) {
+        markedForDeletion.add(bean);
+      }
+    }
+    return markedForDeletion;
   }
 }
