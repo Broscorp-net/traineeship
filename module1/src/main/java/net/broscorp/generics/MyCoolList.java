0511cb888b5f577b8e6ff8f2e451cfd30024eed9
pesua <pesua.mail@gmail.com>
2022-01-16 18:32:21 +0300

    Checkstyle fix and java 12 support

--- module1/src/main/java/net/broscorp/generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/generics/MyCoolList.java
@@ -1,28 +1,27 @@
 package net.broscorp.generics;
 
 import java.util.function.Function;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class MyCoolList {
 
   public void add(Object o) {
-    throw new NotImplementedException();
+    throw new RuntimeException("Not implemented");
   }
 
   public Object get(int index) {
-    throw new NotImplementedException();
+    throw new RuntimeException("Not implemented");
   }
 
   public Object remove(int index) {
-    throw new NotImplementedException();
+    throw new RuntimeException("Not implemented");
   }
 
   public MyCoolList map(Function f) {
-    throw new NotImplementedException();
+    throw new RuntimeException("Not implemented");
   }
 
   public int size() {
-    throw new NotImplementedException();
+    throw new RuntimeException("Not implemented");
   }
 
 }
