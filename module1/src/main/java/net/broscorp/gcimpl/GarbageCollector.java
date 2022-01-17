b1a5111d044651e025d62113b75d8b98759f5f2a
Max <maxmailpol@gmail.com>
2022-01-17 23:06:03 +0200

    strings

--- module1/src/main/java/net/broscorp/gcimpl/GarbageCollector.java
+++ module1/src/main/java/net/broscorp/gcimpl/GarbageCollector.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface GarbageCollector {
 
   /**
-   * Collects the garbage for given application
+   * Collects the garbage for given application.
    * @param heap application's objects/beans
    * @param stack application's method calls
    * @return list of objects to delete
