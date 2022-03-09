a17c34abf2df73abeb6702a1210e696321f65b16
AleksandrVoronkov <qwerton00@gmail.com>
2022-03-09 20:46:22 +0200

    Primitives

--- module1/src/main/java/net/broscorp/primitives/FloatingPoint.java
+++
@@ -1,8 +0,0 @@
-package net.broscorp.primitives;
-
-public class FloatingPoint {
-
-  public double errorWhenMultiple(double number, int multipler) {
-    return number * multipler;
-  }
-}
--- module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
+++ module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
@@ -3,69 +3,44 @@ package net.broscorp.primitives;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PrimitivesTest {
 
-  private Conversion conversion;
-  private FloatingPoint floatingPoint;
-  private Owerflow owerflow;
+  @Test
+  void integerOverflowTest() {
+    int x = 2147483647;
 
-  @BeforeEach
-  void init() {
-    conversion = new Conversion();
-    owerflow = new Owerflow();
-    floatingPoint = new FloatingPoint();
+    assertEquals(x + 1, -2147483648);
   }
 
   @Test
   void shouldConvertLongToInt() {
-    assertEquals(34299, conversion.longToInt(34299L));
+    long l = 123415L;
+    int i = 123415;
+
+    assertEquals(l, i);
   }
 
   @Test
   void shouldConvertLongToIntIfLongVeryBig() {
-    assertEquals(124048589, conversion.longToInt(34299732874445L));
+    long l = 34299732874445L;
+    int i = (int) l;
+
+    assertNotEquals(l, i);
   }
 
   @Test
-  void shouldCcnvertDoubleToFloat() {
-    assertEquals(345.90f, conversion.doubleToFloat(345.90d));
+  void shouldConvertFloatToDouble() {
+    float f = 345f;
+
+    assertEquals(f, 345d);
   }
 
   @Test
   void shouldConvertDoubleToFloatIfDoubleBig() {
-    assertEquals(345.90787f, conversion.doubleToFloat(345.90787463654347835734345d));
-  }
+    float f = 345.90f;
 
-  @Test
-  public void maxIntegerOwerflowTest() {
-    assertEquals(-2147483648, owerflow.maxIntOwerflow(1));
-  }
-
-  @Test
-  public void maxLongOwerflowTest() {
-    assertEquals(-9223372036854775808L, owerflow.maxLongOwerflow(1));
-  }
-
-  @Test
-  public void maxByteOwerflowTest() {
-    assertEquals(-128, owerflow.maxByteOwerflow(1));
-  }
-
-  @Test
-  public void maxShortOwerflowTest() {
-    assertEquals(-32768, owerflow.maxShortOwerflow(1));
-  }
-
-  @Test
-  void errorWhenMultipleTest() {
-    assertNotEquals(3.999, floatingPoint.errorWhenMultiple(1.333, 3));
-  }
-
-  @Test
-  void shouldReturnDoubleInMultipleTest() {
-    assertEquals(3.9989999999999997, floatingPoint.errorWhenMultiple(1.333, 3));
+    assertNotEquals(f, 345.90d);
   }
 }
