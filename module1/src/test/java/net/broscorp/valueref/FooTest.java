8ad50fbd56d585fe4f57faf9a097ece5ede3fcaf
yevhenboiev <yevhenboiev@gmail.com>
2022-03-09 18:28:44 +0300

    Create test for class Foo

---
+++ module1/src/test/java/net/broscorp/valueref/FooTest.java
@@ -0,0 +1,25 @@
+package net.broscorp.valueref;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class FooTest {
+
+  Foo newObject = new Foo();
+
+  @Test
+  public void checkFoo() {
+    int i = 10;
+    List<String> array = new ArrayList<>();
+    newObject.foo(i, array);
+    assertEquals(10, i);
+    assertEquals(1, array.size());
+    assertEquals(array.get(0), "d");
+    /* В java все передается по значению. Когда мы передаем примитивный тип мы передаем копию int i, который хранится в памяти как значение;
+    * ArrayList это класс, и в переменной array мы храним ссылку на объект этого класса, по этому по этой ссылке мы можем изменять объект */
+  }
+
+}
