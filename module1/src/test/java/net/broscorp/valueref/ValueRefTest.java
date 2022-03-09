c2214357be3c267bfb9d3a782843596827cf3076
AleksandrVoronkov <qwerton00@gmail.com>
2022-03-09 15:06:31 +0200

    Value reference

---
+++ module1/src/test/java/net/broscorp/valueref/ValueRefTest.java
@@ -0,0 +1,40 @@
+package net.broscorp.valueref;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class ValueRefTest {
+
+  /**
+   * В методе foo в коллекцию добавляем элемент String со значением "d",
+   * а переменную i увеличиваем на 15
+   *
+   * В результате: i не увеличивается на 15, но в коллекцию элемент "d" попадает.
+   *
+   * Так происходит потому что java передает параметры в методы по значению,
+   * поэтому методы работают с копией данных.
+   * В данном случае мы работаем с копией значения переменной i,
+   * поэтому оригинальная i не изменяется. Но в случае с коллекцией,
+   * которая представляет из себя обьект - метод не создает копию
+   * коллекции, а работает с копией ссылки на оригинальную коллекцию,
+   * изменяя ее.
+   */
+  @Test
+  public void valueRefTest() {
+    Foo test = new Foo();
+    List<String> testList = new ArrayList<>();
+    int i = 0;
+
+    assertEquals(0, testList.size());
+    assertEquals(0, i);
+
+    test.foo(i, testList);
+
+    assertEquals(1, testList.size());
+    assertEquals("d", testList.get(0));
+    assertEquals(0, i);
+  }
+}
