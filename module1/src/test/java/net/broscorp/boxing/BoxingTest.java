6b48ebf17de5efc75701e9c36834e9d638a3a42a
antono0106 <antono0106@gmail.com>
2022-03-10 19:06:35 +0300

    created tests of boxing

---
+++ module1/src/test/java/net/broscorp/boxing/BoxingTest.java
@@ -0,0 +1,36 @@
+package net.broscorp.boxing;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoxingTest {
+    @Test
+    public void wrappersEquityMustBeTrue() {
+        Float someFloat = 5.0f;
+        Double someDouble = 5.0;
+        Assertions.assertTrue(someDouble == (float) someFloat);
+        //Assertions.assertTrue((Long) 10 == (Integer) 10);
+    }
+
+    @Test
+    public void wrappersEquityMustBeFalse() {
+        Integer someInteger = 5;
+        Long someLong = 5L;
+        Assertions.assertFalse(someInteger == (Number)someLong);
+    }
+
+    @Test
+    public void autoUnboxingException() {
+        Assertions.assertThrows(
+                NullPointerException.class,
+                () -> {
+                    Integer someInt = null;
+                    int max = someInt;
+                });
+    }
+
+}
