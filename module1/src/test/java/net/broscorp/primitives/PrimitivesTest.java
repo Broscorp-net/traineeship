7f8f13a832242d63cb4f15756dd3e1b262afaca6
Alexandr Nimko <shurick2211@gmail.com>
2022-03-10 07:23:38 +0200

    fix task

--- module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
+++ module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
@@ -7,15 +7,32 @@ public class PrimitivesTest {
 
   @Test
   void overflowTest() {
-    // при а выходящих за пределы Integer тест будет не пройден из-за переполнения
-    long a = Integer.MAX_VALUE;
-    int b = (int) a;
-    Assertions.assertEquals(b,a);
+    int a = Integer.MAX_VALUE;
+    int b = 555;
+    int c = Integer.MIN_VALUE;
+    c += b - 1;
+    b += a;
+    // При переполнении переменная не становиться равной Integer.MAX_VALUE
+    Assertions.assertNotEquals(b,a);
+    /* А меняет свое значение, после Integer.MAX_VALUE следующим идет Integer.MIN_VALUE,
+    а следующие значения прибавляются к нему.
+    */
+    Assertions.assertEquals(c,b);
   }
 
   @Test
-  void convertTest() {
-    // по преобразованию long в int см. метод overflowTest()
+  void convertLongIntTest() {
+    /* если переменная long > переменной int происходит потеря значения преобразуемой переменной
+    из-за переполнения int, описанного в методе overflowTest()
+    */
+    long a = Integer.MAX_VALUE + 789L;
+    int b = (int) a;
+    Assertions.assertNotEquals(b,a);
+  }
+
+  @Test
+  void convertDoubleFloatTest() {
+    // если переменная double > переменной float происходит урезание double до Float.MAX_VALUE
     double c = Float.MAX_VALUE + 555.555;
     float d;
     d = (float) c;
