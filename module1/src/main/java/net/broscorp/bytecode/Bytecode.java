2cfc9c1d0f69383934471f64ed3ccb85fda0c6bc
Alexandr Nimko <shurick2211@gmail.com>
2022-03-10 17:31:44 +0200

    Bytecode

---
+++ module1/src/main/java/net/broscorp/bytecode/Bytecode.java
@@ -0,0 +1,14 @@
+package net.broscorp.bytecode;
+
+public class Bytecode {
+  public static void main (String[] args) {
+    for (int i = 1; i < 4; i ++) {
+      printHello();
+    }
+  }
+
+  static void printHello() {
+    System.out.println( "Hello World!");
+  }
+
+}
---
+++ module1/src/main/java/net/broscorp/bytecode/compile.cmd
@@ -0,0 +1,1 @@
+javac  Bytecode.java
