3f88f82321b956899d4f3c204826f5c51bcf5749
karaedd <karaed.ml@mail.ru>
2022-03-10 09:26:08 +0200

    added some tests.

---
+++ module1/src/test/java/net/broscorp/primitives/MathematicsTest.java
@@ -0,0 +1,36 @@
+package net.broscorp.primitives;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MathematicsTest {
+
+  @Test
+  public void integerValueOverflows() {
+    int actual = Integer.MAX_VALUE + 1;
+    int expected = -2147483648;
+    Assertions.assertEquals(expected, actual);
+  }
+
+  @Test
+  public void longToInt() {
+    long actual = 2147483649L;
+    int expected = (int) actual;
+    Assertions.assertNotEquals(expected, actual);
+  }
+
+  @Test
+  public void doubleToFloat() {
+    double actual = 3.1415926535897932384626433832795028841971693993751058209749445923078164062862;
+    float expected = (float) actual;
+    Assertions.assertNotEquals(expected, actual);
+  }
+
+  @Test
+  public void faultFloatingPoint() {
+    double actual = 3.0 - 2.1;
+    double expected = 0.9;
+    Assertions.assertNotEquals(expected, actual);
+  }
+}
+
