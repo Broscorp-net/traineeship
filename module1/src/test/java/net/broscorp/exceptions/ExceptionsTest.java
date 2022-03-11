1b575fa6b28a0609ccb6c889b1cda57fb1beb5c9
mbondarchuk <mbondarchuk@unitedthinkers.net>
2022-03-11 12:24:50 +0200

    Task 'exception. solve.

---
+++ module1/src/test/java/net/broscorp/exceptions/ExceptionsTest.java
@@ -0,0 +1,28 @@
+package net.broscorp.exceptions;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+public class ExceptionsTest {
+
+  @Test
+  public void tryWithResource() {
+    try(BufferedReader reader = new BufferedReader(new FileReader("brokePath"))) {
+    } catch (IOException e) {
+    } finally {
+      System.out.println("Hello bro!");
+    }
+  }
+
+  @Test
+  public void catchAncestorException() {
+    try {
+      int number = 10;
+      for (int i = 5; i >= 0 ; i--) {
+        number /= i;
+      }
+    } catch (Exception e) { }
+  }
+}
