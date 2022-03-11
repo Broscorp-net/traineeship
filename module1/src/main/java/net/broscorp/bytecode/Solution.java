b3f7df34186f4c9f5b850a25ed8b4e13777e4265
mbondarchuk <mbondarchuk@unitedthinkers.net>
2022-03-11 07:39:55 +0200

    Task 'bytecode' solved.

---
+++ module1/src/main/java/net/broscorp/bytecode/Solution.java
@@ -0,0 +1,17 @@
+package net.broscorp.bytecode;
+
+public class Solution {
+
+  /**
+   * comment.
+   * */
+  public static void main(String[] args) {
+    int count = 0;
+    for (int i = 0; i < 10; i++) {
+      count++;
+    }
+    count = Integer.parseInt("100");
+    System.out.println("Hello World!");
+  }
+
+}
---
+++ module1/src/main/java/net/broscorp/bytecode/script.cmd
@@ -0,0 +1,3 @@
+javac Solution.java
+java Solution.java
+javap -c -s Solution.class > decompiled.txt
