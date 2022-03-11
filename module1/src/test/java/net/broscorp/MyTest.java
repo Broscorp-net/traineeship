f3101a6ec169d45b15412549847c2d6ddb49dada
Lysak Dmitrii <lysakdmtr@gmail.com>
2022-03-12 01:50:51 +0200

    location changed

--- module1/src/test/java/net/broscorp/MyTest.java
+++ module1/src/test/java/net/broscorp/MyTest.java
@@ -6,7 +6,41 @@ import org.junit.jupiter.api.Test;
 class MyTest {
 
   @Test
-  void test() {
-    Assertions.assertTrue(true);
+  public void testOverflow() {
+    int a = 2_000_000_000;
+    int b = 2_000_000_000;
+
+    int c = a + b;
+
+    Assertions.assertEquals(-294967296, c);
+  }
+
+  @Test
+  public void testLongToInt() {
+    long a = 254354645645L;
+
+    int b = (int) a;
+
+    Assertions.assertEquals(951575181, b);
+  }
+
+  @Test
+  public void testDoubleToFloat() {
+    double a = 1 * Math.pow(10, -300);
+
+    float b = (float) a;
+
+    Assertions.assertEquals(0F, b);
+  }
+
+  @Test
+  public void testAccuracy() {
+    float a = 0;
+
+    for (int i = 0; i < 16777219; i++) {
+      a++;
+    }
+
+    Assertions.assertEquals(1.6777216E7, a);
   }
 }
