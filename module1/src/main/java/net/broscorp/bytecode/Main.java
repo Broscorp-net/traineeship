8c2caf7dd5d296f7d7f08c739bab852534018e9c
GregoryKGitHub <98350839+GregoryKGitHub@users.noreply.github.com>
2022-03-11 15:59:17 +0200

    Update Main.java

--- module1/src/main/java/net/broscorp/bytecode/Main.java
+++ module1/src/main/java/net/broscorp/bytecode/Main.java
@@ -1,13 +1,10 @@
 package net.broscorp.bytecode;
 
 public class Main {
-
   static int a = 0;
-
   static void increment()  {
     a++;
   }
-
   public static void main(String[] args) {
     for (int i = 0; i < 20; i++)  {
       increment();
