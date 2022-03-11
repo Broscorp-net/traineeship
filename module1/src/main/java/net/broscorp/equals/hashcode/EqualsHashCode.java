0e1fc8deb7d82ea3528cbe8ba96f1c231ae0e858
antono0106 <antono0106@gmail.com>
2022-03-11 21:15:24 +0300

    created EqualsHashCode and EqualsHashCodeTest

---
+++ module1/src/main/java/net/broscorp/equals/hashcode/EqualsHashCode.java
@@ -0,0 +1,81 @@
+package net.broscorp.equals.hashcode;
+
+public class EqualsHashCode {
+  private int anInt;
+  private String string;
+  private Integer integer;
+
+  public EqualsHashCode() {}
+
+  /**
+   * Some custom class for testing equals and hashCode methods.
+   * @param anInt - some int param.
+   * @param string - some string param.
+   * @param integer - some wrapper param.
+   */
+  public EqualsHashCode(int anInt, String string, Integer integer) {
+    this.anInt = anInt;
+    this.string = string;
+    this.integer = integer;
+  }
+
+  public int getAnInt() {
+    return anInt;
+  }
+
+  public String getString() {
+    return string;
+  }
+
+  public Integer getInteger() {
+    return integer;
+  }
+
+  public void setAnInt(int anInt) {
+    this.anInt = anInt;
+  }
+
+  public void setString(String string) {
+    this.string = string;
+  }
+
+  public void setInteger(Integer integer) {
+    this.integer = integer;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    EqualsHashCode that = (EqualsHashCode) o;
+    return anInt == that.anInt && string.equals(that.string) && integer.equals(that.integer);
+  }
+
+  @Override
+  public int hashCode() {
+    int hash = 17;
+
+    hash = 31 * hash + getAnInt();
+    hash = 31 * hash + getString().hashCode();
+    hash = 31 * hash + getInteger().hashCode();
+
+    return hash;
+  }
+
+  @Override
+  public String toString() {
+    return "i = "
+        + anInt
+        + "\n"
+        + "string = "
+        + string
+        + "\n"
+        + "wrapper = "
+        + integer.toString()
+        + "\n";
+  }
+}
---
+++ module1/src/test/java/net/broscorp/equals/hashcode/EqualsHashCodeTest.java
@@ -0,0 +1,75 @@
+package net.broscorp.equals.hashcode;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Random;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EqualsHashCodeTest {
+
+  private EqualsHashCode eh1;
+  private EqualsHashCode eh2;
+  private EqualsHashCode eh3;
+  private EqualsHashCode eh4;
+
+  @BeforeEach
+  public void setUp() {
+    eh1 = new EqualsHashCode(1, "s", new Integer(1));
+    eh2 = new EqualsHashCode(1, "s", new Integer(1));
+    eh3 = new EqualsHashCode(1, "s", new Integer(1));
+    eh4 = new EqualsHashCode(1, "s", new Integer(2));
+  }
+
+  @Test
+  public void equalsAndHashCodeCheck() {
+    Assertions.assertEquals(eh1, eh1); // reflexivity
+    Assertions.assertEquals(eh1, eh2); // symmetric
+    Assertions.assertTrue(eh1.equals(eh2) && eh1.equals(eh3) && eh2.equals(eh3)); // transitivity
+
+    for (int i = 0; i < 5; i++) {
+      Assertions.assertEquals(eh1, eh2); // consistent
+    }
+
+    Assertions.assertNotEquals(eh1, null); // false for null
+
+    int ehOneHash = eh1.hashCode();
+
+    for (int i = 0; i < 5; i++) {
+      Assertions.assertEquals(ehOneHash, eh1.hashCode()); // same hashcode production
+    }
+
+    Assertions.assertTrue(
+        eh1.equals(eh2) && eh1.hashCode() == eh2.hashCode()); // equals and hashcode contract
+    Assertions.assertFalse(eh1.equals(eh4) && eh1.hashCode() == eh4.hashCode());
+  }
+
+  @Test
+  public void enumerationForObjects() {
+    while (true) {
+      byte[] bytes1 = new byte[7];
+      byte[] bytes2 = new byte[7];
+
+      new Random().nextBytes(bytes1);
+      new Random().nextBytes(bytes2);
+
+      EqualsHashCode e1 =
+          new EqualsHashCode(
+              new Random().nextInt(10 - 1) + 1,
+              new String(bytes1, StandardCharsets.UTF_8),
+              new Integer(new Random().nextInt(10 - 1) + 1));
+
+      EqualsHashCode e2 =
+          new EqualsHashCode(
+              new Random().nextInt(10 - 1) + 1,
+              new String(bytes2, StandardCharsets.UTF_8),
+              new Integer(new Random().nextInt(10 - 1) + 1));
+
+      if (!e1.equals(e2) && e1.hashCode() == e2.hashCode()) {
+        System.out.println(e1 + "\n and \n" + e2 + "\n" + "are not equal, but have same hashcode");
+        Assertions.assertEquals(e1.hashCode(), e2.hashCode());
+        break;
+      }
+    }
+  }
+}
