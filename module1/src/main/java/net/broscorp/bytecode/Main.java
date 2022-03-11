457de417cb0ad69b885a67d93d382f7f92696c7e
GregoryKGitHub <98350839+GregoryKGitHub@users.noreply.github.com>
2022-03-11 16:08:50 +0200

    Update Main.java

--- module1/src/main/java/net/broscorp/bytecode/Main.java
+++ module1/src/main/java/net/broscorp/bytecode/Main.java
@@ -2,9 +2,14 @@ package net.broscorp.bytecode;
 
 public class Main {
   static int a = 0;
+
   static void increment()  {
     a++;
   }
+
+  /**
+   * Bytecode increment sample.
+   */
   public static void main(String[] args) {
     for (int i = 0; i < 20; i++)  {
       increment();
