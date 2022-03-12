f57c0f1f3e3d00cd234789cd351d3a7187b2d0ce
Lysak Dmitrii <lysakdmtr@gmail.com>
2022-03-12 18:47:37 +0200

    valueref

--- module1/src/test/java/net/broscorp/MyTest.java
+++ module1/src/test/java/net/broscorp/MyTest.java
@@ -1,12 +1,30 @@
 package net.broscorp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import net.broscorp.valueref.Foo;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MyTest {
 
+  Foo foo = new Foo();
+
   @Test
-  void test() {
-    Assertions.assertTrue(true);
+  public void testNull_null_NPE() {
+    Assertions.assertThrows(NullPointerException.class, () -> foo.foo(0, null));
+  }
+
+  @Test
+  public void testIntegerNull_null_NPE() {
+    Assertions.assertThrows(NumberFormatException.class, () -> foo.foo(Integer.parseInt(null), new ArrayList<>()));
+  }
+
+  @Test
+  public void testFixedSizeList() {
+    List<String> list = Arrays.asList("a", "b", "c");
+
+    Assertions.assertThrows(UnsupportedOperationException.class, () -> foo.foo(0, list));
   }
 }
