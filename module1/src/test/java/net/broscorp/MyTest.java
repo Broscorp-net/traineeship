68572f9647b4459502cfaca85029dcfa78b5537d
Lysak Dmitrii <lysakdmtr@gmail.com>
2022-03-12 14:39:49 +0200

    boxing

--- module1/src/test/java/net/broscorp/MyTest.java
+++ module1/src/test/java/net/broscorp/MyTest.java
@@ -6,7 +6,32 @@ import org.junit.jupiter.api.Test;
 class MyTest {
 
   @Test
-  void test() {
-    Assertions.assertTrue(true);
+  public void testEquality() {
+    Integer a = 1;
+    Integer b = 1;
+
+    boolean result = (a == b);
+
+    Assertions.assertTrue(result);
+  }
+
+  @Test
+  public void testInequality() {
+    Integer a = 130;
+    Integer b = a;
+    b = 130;
+
+    boolean result = (a == b);
+
+    Assertions.assertFalse(result);
+  }
+
+  @Test
+  public void testUnboxing() {
+    Integer a = null;
+
+    Assertions.assertThrows(NullPointerException.class, () -> {
+      int b = a;
+    });
   }
 }
