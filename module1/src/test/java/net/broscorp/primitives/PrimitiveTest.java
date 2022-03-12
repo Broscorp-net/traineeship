a46f5f287041a9526338de6db68ea56eef683837
AzizDeveloper <61232898+AzizDeveloper@users.noreply.github.com>
2022-03-12 10:04:35 +0500

    Primitives

--- module1/src/test/java/net/broscorp/primitives/PrimitiveTest.java
+++ module1/src/test/java/net/broscorp/primitives/PrimitiveTest.java
@@ -2,7 +2,8 @@ package net.broscorp.primitives;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class PrimitiveTest {
 
