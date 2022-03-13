c16c07cf6d9483a40aec736359f418d85fd7a38a
Lysak Dmitrii <lysakdmtr@gmail.com>
2022-03-13 21:02:24 +0200

    exceptions

--- module1/src/test/java/net/broscorp/MyTest.java
+++ module1/src/test/java/net/broscorp/MyTest.java
@@ -1,12 +1,33 @@
 package net.broscorp;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MyTest {
 
   @Test
-  void test() {
-    Assertions.assertTrue(true);
+  void testIOException() {
+    try {
+      File file = new File("./nonExistingFile.txt");
+      InputStream is = new FileInputStream(file);
+    } catch (IOException ex) {
+      System.out.println(ex.getMessage());
+    } finally {
+      System.out.println("Goodbye");
+    }
+  }
+
+  @Test
+  void testAncestors() {
+    try {
+      File file = new File("./nonExistingFile.txt");
+      InputStream is = new FileInputStream(file);
+    } catch (Exception ex) {
+      System.out.println(ex.getMessage());
+    }
   }
 }
