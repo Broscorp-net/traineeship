08ea42b71b901ad2a605015deba39e746d071603
yevhenboiev <yevhenboiev@gmail.com>
2022-03-13 18:09:25 +0300

    String

--- module1/src/test/java/net/broscorp/strings/StringRealizationTest.java
+++ module1/src/test/java/net/broscorp/strings/StringRealizationTest.java
@@ -42,11 +42,14 @@ class StringRealizationTest {
   void printSong() {
     assertEquals("15 little bugs in the code, \n"
             + "15 little bugs in the code. \n"
-            + "Take one down, patch it around 20 little bugs in the code.20 little bugs in the code, \n"
+            + "Take one down, patch it around 20 little bugs in the code.20 little bugs in the code"
+            + ", \n"
             + "20 little bugs in the code. \n"
-            + "Take one down, patch it around 17 little bugs in the code.17 little bugs in the code, \n"
+            + "Take one down, patch it around 17 little bugs in the code.17 little bugs in the code"
+            + ", \n"
             + "17 little bugs in the code. \n"
-            + "Take one down, patch it around 25 little bugs in the code.25 little bugs in the code, \n"
+            + "Take one down, patch it around 25 little bugs in the code.25 little bugs in the code"
+            + ", \n"
             + "25 little bugs in the code. \n"
             + "Take one down, patch it around 25 little bugs in the code.",
         stringRealization.printSong(15, 4));
