ff5bf01245c0cd0eebdeda0367586c4c26c2f1b8
Lysak Dmitrii <lysakdmtr@gmail.com>
2022-03-13 16:25:02 +0200

    bytecode

---
+++ module1/src/main/java/net/broscorp/bytecode/SimpleMeth.java
@@ -0,0 +1,12 @@
+package net.broscorp.bytecode;
+
+public class SimpleMeth{
+
+  public static void main(String[] args) {
+    int count = 0;
+    for (int i = 0; i < 10; i++) {
+      count++;
+    }
+    System.out.println("Hello");
+  }
+}
