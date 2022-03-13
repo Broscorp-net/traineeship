01940bd9d91773e7253a9d9817a66328aec9a9de
Lysak Dmitrii <lysakdmtr@gmail.com>
2022-03-13 16:26:12 +0200

    bytecode

--- module1/src/main/java/net/broscorp/bytecode/cmd.cmd
+++ module1/src/main/java/net/broscorp/bytecode/cmd.cmd
@@ -4,4 +4,3 @@ cd c:\Users\DLysak\IdeaProjects\traineeship\module1\src\main\java\
 javac net/broscorp/bytecode/SimpleMeth.java
 java net.broscorp.bytecode.SimpleMeth
 javap -c net.broscorp.bytecode.SimpleMeth > ./net/broscorp/bytecode/bytecode2.txt
-pause 1000
