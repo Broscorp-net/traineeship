a0acf59ba66dd7e2e71ae6e838d8501e3d3a49ac
Lysak Dmitrii <lysakdmtr@gmail.com>
2022-03-13 16:37:40 +0200

    bytecode

--- module1/src/main/java/net/broscorp/bytecode/SimpleMeth.java
+++ module1/src/main/java/net/broscorp/bytecode/SimpleMeth.java
@@ -2,6 +2,9 @@ package net.broscorp.bytecode;
 
 public class SimpleMeth {
 
+  /**
+   *Print "Hello", compile and decompile.
+   */
   public static void main(String[] args) {
     int count = 0;
     for (int i = 0; i < 10; i++) {
