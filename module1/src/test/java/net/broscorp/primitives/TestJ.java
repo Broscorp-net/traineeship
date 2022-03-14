8a8372855103ca49faa52257c74f76ccc84d1458
”SmithAndWesson” <karevandrey90@gmail.com>
2022-03-14 16:35:13 +0200

    primitives

--- module1/src/test/java/net/broscorp/primitives/TestJ.java
+++ module1/src/test/java/net/broscorp/primitives/TestJ.java
@@ -6,41 +6,44 @@ import org.junit.jupiter.api.Test;
 
 public class TestJ {
 
-  public static int inc(int i){
+  public static int inc(int i) {
     return ++i;
   }
 
-  public int intValue(long i){
+  public int intValue(long i) {
     int x = (int) i;
     return x;
   }
 
-  public float floatValue(double d){
+  public float floatValue(double d) {
     float f = (float) d;
     return f;
   }
 
   @Test
-  public void isIntegerOverflow(){
+  public void isIntegerOverflow() {
     int i =  1;//Integer.MAX_VALUE;
     Assertions.assertFalse(inc(i) == Integer.MIN_VALUE, "Переполнение численного типа");
   }
 
   @Test
-  public void checkLongToInt(){
+  public void checkLongToInt() {
     long i = 1;//Long.MAX_VALUE;
     Assertions.assertEquals(intValue(i), i, "Изначальное значение не поместилось");
   }
 
   @Test
-  public void checkDoubleToFloatOverflow(){
+  public void checkDoubleToFloatOverflow() {
     double d = 1.1; //Double.MAX_VALUE;
-    if (d == Double.POSITIVE_INFINITY) return;
-    Assertions.assertFalse(floatValue(d) == Float.POSITIVE_INFINITY, "Изначальное значение не поместилось");
+    if (d == Double.POSITIVE_INFINITY) {
+      return;
+    }
+    Assertions.assertFalse(floatValue(d) == Float.POSITIVE_INFINITY,
+        "Изначальное значение не поместилось");
   }
 
   @Test
-  public void checkDoubleToFloatAccuracy(){
+  public void checkDoubleToFloatAccuracy() {
     double d = 1.1;//222222.2;
     System.out.println("act " + (double) floatValue(d));
     System.out.println("exp " + d);
