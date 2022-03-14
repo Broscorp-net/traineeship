6f337d5c5c68b41238632e3d16c1fa782cb68a02
Lysak Dmitrii <lysakdmtr@gmail.com>
2022-03-14 18:08:09 +0200

    generics

--- module1/src/main/java/net/broscorp/generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/generics/MyCoolList.java
@@ -1,8 +1,6 @@
 package net.broscorp.generics;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.function.Function;
 
 public class MyCoolList<E extends Number> {
