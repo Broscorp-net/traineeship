b925c2a90e7defbed3dc85a0ba1123f451444b1b
mbondarchuk <mbondarchuk@unitedthinkers.net>
2022-03-14 13:50:53 +0200

    solve gcimpl solved.

--- module1/src/main/java/net/broscorp/gcimpl/GarbageCollectorImplementation.java
+++ module1/src/main/java/net/broscorp/gcimpl/GarbageCollectorImplementation.java
@@ -1,10 +1,9 @@
 package net.broscorp.gcimpl;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Deque;
 import java.util.List;
-import java.util.Map;
 
 public class GarbageCollectorImplementation implements GarbageCollector {
 
@@ -12,6 +11,36 @@ public class GarbageCollectorImplementation implements GarbageCollector {
   public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
     Collection<ApplicationBean> beans = heap.getBeans().values();
     Deque<StackInfo.Frame> frames = stack.getStack();
-    return Collections.emptyList();
+    List<ApplicationBean> result = new ArrayList<>();
+
+    for (ApplicationBean bean : beans) {
+      if (!isReachable(frames, bean)) {
+        result.add(bean);
+      }
+    }
+    return result;
+  }
+
+  private boolean isReachable(Deque<StackInfo.Frame> frames, ApplicationBean bean) {
+    for (StackInfo.Frame frame : frames) {
+      List<ApplicationBean> rootBeans = frame.getParameters();
+      if (isReachableUtil(rootBeans, bean)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private boolean isReachableUtil(Collection<ApplicationBean> rootBeans, ApplicationBean bean) {
+    if (rootBeans.contains(bean)) {
+      return true;
+    }
+    for (ApplicationBean beanFromFrame : rootBeans) {
+      Collection<ApplicationBean> childBeans = beanFromFrame.getFieldValues().values();
+      if (isReachableUtil(childBeans, bean)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
