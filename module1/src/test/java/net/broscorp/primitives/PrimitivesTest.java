17c6e3ef3e445580cb0e0c1e93f4f0cdfe9f5347
neovendano <kolesnyk.vik@gmail.com>
2022-03-16 01:46:37 +0200

    Primitives

--- module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
+++ module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
@@ -5,30 +5,30 @@ import org.junit.jupiter.api.Test;
 
 public class PrimitivesTest {
   @Test
-  void IntMaxOverflow(){
+  void intMaxOverflow() {
     int max = Integer.MAX_VALUE;
-    Assertions.assertFalse(max<max+1);
+    Assertions.assertFalse(max < max + 1);
   }
 
   @Test
-  void LongToIntOverflow(){
+  void longToIntOverflow() {
     int max = Integer.MAX_VALUE;
-    long convertLong = (long)max*2;
+    long convertLong = (long)max * 2;
     int convertInt = (int)convertLong;
     Assertions.assertNotEquals(convertLong, convertInt);
   }
 
   @Test
-  void DoubleToFloatOverflow(){
+  void doubleToFloatOverflow() {
     float max = Float.MAX_VALUE;
-    double convertDouble = (double)max*2;
+    double convertDouble = (double)max * 2;
     float convertFloat = (float)convertDouble;
     Assertions.assertNotEquals(convertDouble, convertFloat);
   }
 
   @Test
-  void FloatAccuracyError(){
+  void floatAccuracyError() {
     float oneToSeven = 0.142857f;
-    Assertions.assertNotEquals(oneToSeven*7, 0.999999);
+    Assertions.assertNotEquals(oneToSeven * 7, 0.999999);
   }
 }
