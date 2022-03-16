7d35eb3018abdeac3156b7931d58ee19688cafb1
antono0106 <antono0106@gmail.com>
2022-03-16 20:53:26 +0300

    added ConsoleReader, ConsoleWriter, script.cmd

--- module1/script.cmd
+++ module1/script.cmd
@@ -1,1 +1,3 @@
-javac
+javac -d target/classes src/main/java/net/broscorp/classpath/ConsoleReader.java
+javac -d target/classes src/main/java/net/broscorp/classpath/ConsoleWriter.java
+
--- module1/src/main/java/net/broscorp/classpath/ConsoleWriter.java
+++ module1/src/main/java/net/broscorp/classpath/ConsoleWriter.java
@@ -7,17 +7,15 @@ import java.net.Socket;
 
 public class ConsoleWriter {
   public static void main(String[] args) throws IOException {
-    try {
-      ServerSocket ss = new ServerSocket(6666);
-      Socket s = ss.accept(); // establishes connection
+    try (ServerSocket ss = new ServerSocket(6666)) {
+      Socket s = ss.accept(); // waits for connection
       DataInputStream dis = new DataInputStream(s.getInputStream());
+
       String str = dis.readUTF();
       while (!str.equals("quit") && !str.equals("exit")) {
         System.out.println("message= " + str);
         str = dis.readUTF();
       }
-
-      ss.close();
     } catch (Exception e) {
       System.out.println(e);
     }
