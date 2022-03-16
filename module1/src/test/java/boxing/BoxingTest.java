26cfeb89567c52736a30fe384391e33d8cebeb57
Yelyzaveta Lubenets <el1111zavetaaaaa@gmail.com>
2022-03-16 13:14:52 +0200

    edit code style in boxing package edit

--- module1/src/test/java/boxing/BoxingTest.java
+++ module1/src/test/java/boxing/BoxingTest.java
@@ -5,40 +5,40 @@ import org.junit.jupiter.api.Test;
 
 public class BoxingTest {
 
-    @Test
-    public void compareTwoObjectsExpectedTrueResult() {
-        Integer integer1 = 11;
+  @Test
+  public void compareTwoObjectsExpectedTrueResult() {
+    Integer integer1 = 11;
 
-        Integer integer2 = 11;
+    Integer integer2 = 11;
 
-        boolean comparison = (integer1 == integer2);
-        Assertions.assertEquals(true, comparison);
-    }
+    boolean comparison = (integer1 == integer2);
+    Assertions.assertEquals(true, comparison);
+  }
 
-    @Test
-    public void compareTwoObjectsExpectedFalseResult() {
-        Integer integer1 = 500;
+  @Test
+  public void compareTwoObjectsExpectedFalseResult() {
+    Integer integer1 = 500;
 
-        Integer integer2 = 500;
+    Integer integer2 = 500;
 
-        boolean comparison = (integer1 == integer2);
-        Assertions.assertEquals(false, comparison);
-    }
+    boolean comparison = (integer1 == integer2);
+    Assertions.assertEquals(false, comparison);
+  }
 
-    @Test
-    public void compareTwoObjectsExpectedFalseResultSecondVariant() {
-        Integer integer1 = new Integer(5);
+  @Test
+  public void compareTwoObjectsExpectedFalseResultSecondVariant() {
+    Integer integer1 = new Integer(5);
 
-        Integer integer2 = new Integer(5);
+    Integer integer2 = new Integer(5);
 
-        boolean comparison = (integer1 == integer2);
-        Assertions.assertEquals(false, comparison);
-    }
+    boolean comparison = (integer1 == integer2);
+    Assertions.assertEquals(false, comparison);
+  }
 
-    @Test
-    public void gettingExceptionFromUnboxing() {
-        Integer integer1 = null;
+  @Test
+  public void gettingExceptionFromUnboxing() {
+    Integer integer1 = null;
 
-        Assertions.assertThrows(NullPointerException.class, () -> integer1.intValue());
-    }
+    Assertions.assertThrows(NullPointerException.class, () -> integer1.intValue());
+  }
 }
