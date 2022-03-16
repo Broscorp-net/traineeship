5bc81a9d8c2f04cc5ace13daa7c997ff32e60368
Alexandr Nimko <shurick2211@gmail.com>
2022-03-16 18:49:28 +0200

    fix3

---
+++ module1/src/test/java/net/broscorp/boxing/BoxingTest.java
@@ -0,0 +1,39 @@
+package net.broscorp.boxing;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class BoxingTest {
+
+  @Test
+  void trueEqualTest() {
+    int c = 43;
+    Integer a = c;
+    Integer b = c;
+    Assertions.assertTrue(a == b);
+  }
+
+  @Test
+  void falseEqualTest() {
+    Integer a = new Integer(43);
+    Integer b = Integer.valueOf(43);
+    Assertions.assertFalse(a == b);
+  }
+
+  @Test
+  void secondFalseEqualTest() {
+    Integer a = new Integer(43);
+    Integer b = 43;
+    Assertions.assertFalse(a == b);
+  }
+
+
+
+  @Test
+  void unBoxingTest() {
+    Integer a = null;
+    NullPointerException e = Assertions.assertThrows(NullPointerException.class,() -> {
+      int b = a;
+    });
+  }
+}
