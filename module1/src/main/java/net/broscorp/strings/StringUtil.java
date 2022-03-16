93c9008bccc300a1697f16f3168b6e9ec7991fd4
Aleksandr Volin <alexvolin28@gmail.com>
2022-03-16 13:23:50 +0200

    Write StringUtil and StringUtilTest!

---
+++ module1/src/main/java/net/broscorp/strings/StringUtil.java
@@ -0,0 +1,98 @@
+package net.broscorp.strings;
+
+import java.util.Random;
+
+public class StringUtil {
+
+    /**
+     * Check string is palindrome
+     * @param string
+     * @return
+     */
+    public boolean isPalindrome(String string) {
+        char[] chars = string.toCharArray();
+        int i = 0;
+        int k = chars.length - 1;
+        while (i < chars.length) {
+            if (chars[i] == ' ') {
+                i++;
+            }
+            if (chars[k] == ' ') {
+                k--;
+            }
+            if (chars[i] != chars[k]) {
+                return false;
+            }
+            i++;
+            k--;
+        }
+        return true;
+    }
+
+    /**
+     * Return only Hello.
+     * @param string
+     * @return
+     */
+    public String stringHello(String string) {
+        return string.substring(0, 5);
+    }
+
+    /**
+     * Delete l from string.
+     * @param string
+     * @return
+     */
+    public String stringWithOutL(String string) {
+        return string.replaceAll("l", "");
+    }
+
+    /**
+     * Change o in string on q.
+     * @param string
+     * @return
+     */
+    public String changeOToQ(String string) {
+        return string.replaceAll("o", "q");
+    }
+
+    /**
+     * Method read how many letters in every word in string.
+     * @param string
+     * @return
+     */
+    public String wordsAndLetter(String string) {
+        String[] strings = string.split(" ");
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (String s : strings) {
+            stringBuilder.append(s + "=" + s.length() + "\\n");
+        }
+        return stringBuilder.toString();
+    }
+
+    /**
+     * Methods has 2 values. First is count of bugs. Second is count of couplets. And return text of song.
+     * @param countOfBugs
+     * @param countOfCouplets
+     * @return
+     */
+    public String bugSong(int countOfBugs, int countOfCouplets) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Random random = new Random((long) countOfBugs * countOfCouplets);
+        for (int i = 0; i < countOfCouplets; i++){
+            int future = countOfBugs - 10 + random.nextInt(20);
+            if(future < 0){
+                future = 0;
+            }
+            stringBuilder.append(countOfBugs + " little bugs in the code,\n" +
+                countOfBugs + " little bugs in the code.\nTake one down, patch it around " +
+                future + " little bugs in the code.\n");
+            if(future == 0){
+                break;
+            }
+            countOfBugs = future;
+        }
+        return stringBuilder.toString();
+    }
+}
---
+++ module1/src/test/java/net/broscorp/strings/StringUtilTest.java
@@ -0,0 +1,58 @@
+package net.broscorp.strings;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StringUtilTest {
+
+    private StringUtil stringUtil;
+    private static final String HELLOWORLD = "Hello world";
+
+    @BeforeEach
+    void create(){
+        stringUtil = new StringUtil();
+    }
+
+    @Test
+    void isPalindrome() {
+        assertTrue(stringUtil.isPalindrome("b l a bl a al b alb"));
+        assertFalse(stringUtil.isPalindrome(HELLOWORLD));
+    }
+
+    @Test
+    void stringHello() {
+        assertEquals(stringUtil.stringHello(HELLOWORLD), "Hello");
+    }
+
+    @Test
+    void stringWithOutL() {
+        assertEquals(stringUtil.stringWithOutL(HELLOWORLD), "Heo word");
+    }
+
+    @Test
+    void changeOToQ() {
+        assertEquals(stringUtil.changeOToQ(HELLOWORLD), "Hellq wqrld");
+    }
+
+    @Test
+    void wordsAndLetter() {
+        assertEquals(stringUtil.wordsAndLetter(HELLOWORLD), "Hello=5\\nworld=5\\n");
+    }
+
+    @Test
+    void bugSong() {
+        String str = "3 little bugs in the code,\n" +
+            "3 little bugs in the code.\n" +
+            "Take one down, patch it around 2 little bugs in the code.\n" +
+            "2 little bugs in the code,\n" +
+            "2 little bugs in the code.\n" +
+            "Take one down, patch it around 8 little bugs in the code.\n" +
+            "8 little bugs in the code,\n" +
+            "8 little bugs in the code.\n" +
+            "Take one down, patch it around 6 little bugs in the code.\n";
+        assertEquals(stringUtil.bugSong(3,3), str);
+    }
+}
