46a449df6017af1feb4e42819873767b520558d4
antono0106 <antono0106@gmail.com>
2022-03-17 16:27:15 +0300

    fixed primitives task

--- module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
+++ module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
@@ -27,6 +27,21 @@ class PrimitivesTest {
   }
 
   @Test
+  public void arithmeticalOperationsWithFloats() {
+    double f1 = 0.0;
+    for (int i = 1; i <= 11; i++) {
+      f1 += .1;
+    }
+
+    double f2 = 0.1 * 11;
+
+    System.out.println("f1 = " + f1);
+    System.out.println("f2 = " + f2);
+
+    Assertions.assertNotEquals(f1, f2);
+  }
+
+  @Test
   public void accuracyTest() {
     System.out.println("float of 2.121313241412441124: " + 2.121313241412441124f);
     System.out.println("double of 2.121313241412441124: " + 2.121313241412441124d);
