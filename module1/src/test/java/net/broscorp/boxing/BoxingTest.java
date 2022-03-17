0115410c409133bcb20feed75a1a170c2e3bd433
karaedd <karaed.ml@mail.ru>
2022-03-17 09:31:51 +0200

    deleted tests for String

--- module1/src/test/java/net/broscorp/boxing/BoxingTest.java
+++ module1/src/test/java/net/broscorp/boxing/BoxingTest.java
@@ -2,7 +2,6 @@ package net.broscorp.boxing;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 public class BoxingTest {
   @Test
@@ -20,27 +19,10 @@ public class BoxingTest {
   }
 
   @Test
-  public void compareStringTrue() {
-    String actual = "Hello world!";
-    String expected = "Hello world!";
-    Assertions.assertSame(expected, actual);
-  }
-
-  @Test
-  public void compareStringFalse() {
-    String actual = "Hello world!";
-    String expected = new String("Hello world!");
-    Assertions.assertNotSame(expected, actual);
-  }
-
-  @Test
   public void unboxingBoxingException() {
     Integer actual = null;
-    Assertions.assertThrows(NullPointerException.class, new Executable() {
-      @Override
-      public void execute() {
-        int expected = actual;
-      }
+    Assertions.assertThrows(NullPointerException.class, () -> {
+      int expected = actual;
     });
   }
 }
