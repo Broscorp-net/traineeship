fd468b33a121750eaa3a8d374558c540cfa8aa46
Lysak Dmitrii <lysakdmtr@gmail.com>
2022-03-18 21:14:31 +0200

    generics

--- module1/src/test/java/net/broscorp/MyTest.java
+++ module1/src/test/java/net/broscorp/MyTest.java
@@ -1,45 +1,12 @@
 package net.broscorp;
 
-import net.broscorp.generics.MyCoolList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class MyTest {
 
   @Test
-  void testAdding() {
-    MyCoolList<Integer> myCoolList = new MyCoolList<>();
-    myCoolList.add(6);
-    Assertions.assertEquals(myCoolList.get(0), 6);
-  }
-
-  @Test
-  void testRemoving() {
-    MyCoolList<Integer> myCoolList = new MyCoolList<>();
-    for (int i = 0; i < 5; i++) {
-      myCoolList.add(i);
-    }
-    Assertions.assertEquals(myCoolList.remove(4), 4);
-    Assertions.assertEquals(myCoolList.remove(0), 0);
-    Assertions.assertEquals(myCoolList.remove(1), 2);
-  }
-
-  @Test
-  void testMapping() {
-    MyCoolList<Integer> myCoolList = new MyCoolList<>();
-    for (int i = 0; i < 5; i++) {
-      myCoolList.add(i);
-    }
-    myCoolList = myCoolList.map((it) -> it * 2);
-    Assertions.assertEquals(myCoolList.get(4), 8);
-  }
-
-  @Test
-  void testSize() {
-    MyCoolList<Integer> myCoolList = new MyCoolList<>();
-    for (int i = 0; i < 5; i++) {
-      myCoolList.add(i);
-    }
-    Assertions.assertEquals(myCoolList.size(), 5);
+  void test() {
+    Assertions.assertTrue(true);
   }
 }
---
+++ module1/src/test/java/net/broscorp/generics/GenericsTests.java
@@ -0,0 +1,45 @@
+package net.broscorp.generics;
+
+import net.broscorp.generics.MyCoolList;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GenericsTests {
+
+  @Test
+  void testAdding() {
+    MyCoolList<Integer> myCoolList = new MyCoolList<>();
+    myCoolList.add(6);
+    Assertions.assertEquals(myCoolList.get(0), 6);
+  }
+
+  @Test
+  void testRemoving() {
+    MyCoolList<Integer> myCoolList = new MyCoolList<>();
+    for (int i = 0; i < 5; i++) {
+      myCoolList.add(i);
+    }
+    Assertions.assertEquals(myCoolList.remove(4), 4);
+    Assertions.assertEquals(myCoolList.remove(0), 0);
+    Assertions.assertEquals(myCoolList.remove(1), 2);
+  }
+
+  @Test
+  void testMapping() {
+    MyCoolList<Integer> myCoolList = new MyCoolList<>();
+    for (int i = 0; i < 5; i++) {
+      myCoolList.add(i);
+    }
+    myCoolList = myCoolList.map((it) -> it * 2);
+    Assertions.assertEquals(myCoolList.get(4), 8);
+  }
+
+  @Test
+  void testSize() {
+    MyCoolList<Integer> myCoolList = new MyCoolList<>();
+    for (int i = 0; i < 5; i++) {
+      myCoolList.add(i);
+    }
+    Assertions.assertEquals(myCoolList.size(), 5);
+  }
+}
