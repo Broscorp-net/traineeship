f56cecc4c7cd7d516ad43a309821991bfe8827d7
Lysak Dmitrii <lysakdmtr@gmail.com>
2022-03-18 21:22:05 +0200

    strings

---
+++ module1/src/test/java/net/broscorp/strings/StringsTests.java
@@ -0,0 +1,46 @@
+package net.broscorp.strings;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class StringsTests {
+
+  private StringUtils stringUtils;
+
+  @BeforeEach
+  void create() {
+    stringUtils = new StringUtils();
+  }
+
+  @Test
+  void testPolindrom() {
+    Assertions.assertTrue(stringUtils.isPolindrom("avfg gf va"));
+    Assertions.assertFalse(stringUtils.isPolindrom("fdsfdsf"));
+  }
+
+  @Test
+  void testNoWorldHello() {
+    Assertions.assertEquals(stringUtils.noWorldHello(), "Hello");
+  }
+
+  @Test
+  void testNoMoreLWillBeHere() {
+    Assertions.assertEquals(stringUtils.noMoreLWillBeHere(), "Heo Word");
+  }
+
+  @Test
+  void testQInsteadO() {
+    Assertions.assertEquals(stringUtils.pasteQInsteadO(), "Hellq Wqrld");
+  }
+
+  @Test
+  void testLetterCounter() {
+    Assertions.assertEquals(stringUtils.letterCounter(), "{Hello=5, World=5}");
+  }
+
+  @Test
+  void testSingSong() {
+    Assertions.assertEquals(stringUtils.singSong(10, 10).split("\n").length, 30);
+  }
+}
