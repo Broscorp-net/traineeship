0ae14fad52834bfb9b3b4f932623ede6bdb06406
Shurick2211 <47493485+Shurick2211@users.noreply.github.com>
2022-03-19 15:26:30 +0200

    fix

--- module1/src/main/java/net/broscorp/bytecode/Bytecode.java
+++ module1/src/main/java/net/broscorp/bytecode/Bytecode.java
@@ -1,8 +1,8 @@
-package net.broscorp.bytecode;
 
 public class Bytecode {
+  
   public static void main (String[] args) {
-    for (int i = 1; i < 4; i ++) {
+    for (int i = 1; i < 4; i++) {
       printHello();
     }
   }
