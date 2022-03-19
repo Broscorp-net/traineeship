100ec42ef3ab6c319d53863c040997ab399994ed
Alexandr Nimko <shurick2211@gmail.com>
2022-03-19 15:03:56 +0200

    2

--- module1/src/main/java/net/broscorp/classpath/ClassPathMain.java
+++ module1/src/main/java/net/broscorp/classpath/ClassPathMain.java
@@ -2,7 +2,12 @@
 import java.util.Scanner;
 
 public class ClassPathMain {
-  public static void main(String args[]) {
+
+  /**
+   * Main method.
+   * @param args String
+   */
+  public static void main(String[] args) {
     System.out.println("Enter string?");
     Scanner scanner = new Scanner(System.in);
     String input = scanner.nextLine();
