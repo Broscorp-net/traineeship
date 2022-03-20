661f6483516a23187b8582030b7a119456c0d38d
Alexandr Nimko <shurick2211@gmail.com>
2022-03-20 18:52:01 +0200

    4

--- module1/src/main/java/net/broscorp/bytecode/Bytecode.java
+++ module1/src/main/java/net/broscorp/bytecode/Bytecode.java
@@ -2,15 +2,19 @@ public class Bytecode {
 
   /**
    * Main method.
+   * Изменение цикла for на while, и имени переменной деассемблированный байткод не поменяло.
+   * Изменения видны только при изменении имени метода с printHello() на hello()
    * @param args String[]
    */
   public static void main(String[] args) {
-    for (int i = 1; i < 4; i++) {
-      printHello();
+    int a = 1;
+    while (a < 4) {
+      hello();
+      a++;
     }
   }
 
-  static void printHello() {
+  static void hello() {
     System.out.println("Hello World!");
   }
 
