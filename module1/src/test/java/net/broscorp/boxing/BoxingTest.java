7a0c22f72e81e0223fa3887185dd2afda96aa63f
Dan2000 <noisyboy2000@gmail.com>
2022-03-23 17:03:58 +0200

    boxing

--- module1/src/test/java/net/broscorp/boxing/BoxingTest.java
+++ module1/src/test/java/net/broscorp/boxing/BoxingTest.java
@@ -9,20 +9,22 @@ public class BoxingTest {
     Integer a = new Integer(1);
     Integer b = a;
     Assertions.assertTrue(a == b);
-}
+  }
+
   @Test
   public void checkNotEqualWrappersTypes() {
     Integer a = new Integer(1);
     Integer b = new Integer(1);
     Assertions.assertFalse(a == b);
   }
+
   @Test
   public void checkUnboxingError() {
     try {
       Integer a = null;
       Double b = Double.valueOf(a);
     } catch (NullPointerException e) {
-        Assertions.assertTrue(true);
+      Assertions.assertTrue(true);
     }
   }
 
