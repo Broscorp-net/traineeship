1dbeed3aeaafeb125cf3695d5b0af061534dc020
Nikola-jpg <84804348+Nikola-jpg@users.noreply.github.com>
2022-03-23 10:32:12 +0200

    Update Person.java
    ...

--- module1/src/main/java/net/broscorp/equals/hashcode/Person.java
+++ module1/src/main/java/net/broscorp/equals/hashcode/Person.java
@@ -8,36 +8,12 @@ public class Person {
   private int age;
 
   /**.
-   * Person constructor.
+   * Default constructor.
    */
   public Person(String firstName, String lastName, int age) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.age = age;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public String getFirstName() {
-    return firstName;
-  }
-
-  public String getLastName() {
-    return lastName;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
-
-  public void setFirstName(String firstName) {
-    this.firstName = firstName;
-  }
-
-  public void setLastName(String lastName) {
-    this.lastName = lastName;
   }
 
   @Override
