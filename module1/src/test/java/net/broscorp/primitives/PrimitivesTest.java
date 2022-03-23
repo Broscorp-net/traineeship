f23adc0814ac7bdbe3e48b64e3c0ecf494680656
Dan2000 <noisyboy2000@gmail.com>
2022-03-23 16:02:01 +0200

    primitives

--- module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
+++ module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
@@ -9,6 +9,7 @@ public class PrimitivesTest {
     int m = Integer.MAX_VALUE;
     Assertions.assertTrue(m + 1 < 0);
   }
+
   @Test
   public void checkConvertLongToInt() {
     int max = Integer.MAX_VALUE;
@@ -16,19 +17,21 @@ public class PrimitivesTest {
     int convertedInt = (int)convertedLong;
     Assertions.assertNotEquals(convertedLong, convertedInt);
   }
+
   @Test
   public void checkConvertDoubleToFloat() {
-      float max = Float.MAX_VALUE;
-      double convertDouble = (double)max*2;
-      float convertFloat = (float)convertDouble;
-      Assertions.assertNotEquals(convertDouble, convertFloat);
+    float max = Float.MAX_VALUE;
+    double convertDouble = (double)max * 2;
+    float convertFloat = (float)convertDouble;
+    Assertions.assertNotEquals(convertDouble, convertFloat);
   }
+
   @Test
   public void checkFloatError() {
-      float f = 0.0f;
-      for (int i=1; i <= 10; i++) {
-          f += 0.1;
-      }
-      Assertions.assertNotEquals(f, 1.0f);
+    float f = 0.0f;
+    for (int i = 1; i <= 10; i++) {
+      f += 0.1;
+    }
+    Assertions.assertNotEquals(f, 1.0f);
   }
 }
