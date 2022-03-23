937903e50eca15ac72ee3bb85083b2a78f468eb0
Nikola-jpg <84804348+Nikola-jpg@users.noreply.github.com>
2022-03-23 10:31:29 +0200

    Update PersonTest.java
    Добавил описания котракта между equals and hashcode.

--- module1/src/test/java/net/broscorp/hashcode/PersonTest.java
+++ module1/src/test/java/net/broscorp/hashcode/PersonTest.java
@@ -17,17 +17,20 @@ public class PersonTest {
     persons.add(new Person("Derf", "Fred", 30));
   }
 
+  // Если два объекта равны, они должны иметь одинаковый хэш-код.
   @Test
   public void checkEqualsAndHashCode() {
     Assertions.assertEquals(persons.get(0), persons.get(1));
-    Assertions.assertNotEquals(persons.get(0), persons.get(2));
+    Assertions.assertEquals(persons.get(0).hashCode(), persons.get(1).hashCode());
   }
 
   @Test
   public void foundIdenticalHashCode() {
     Assertions.assertEquals(persons.get(0).hashCode(), persons.get(1).hashCode());
+    Assertions.assertEquals(persons.get(0), persons.get(1));
   }
 
+  // Если два объекта имеют одинаковый хэш-код, они могут быть равны или не равны.
   @Test
   public void foundDifferentObjectWithIdenticalHashCode() {
     Assertions.assertEquals(persons.get(2).hashCode(), persons.get(3).hashCode());
