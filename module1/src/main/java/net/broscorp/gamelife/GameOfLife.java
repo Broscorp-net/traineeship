a3726adb60eb858654d52eb6b901b63bafe26715
Lysak Dmitrii <lysakdmtr@gmail.com>
2022-03-24 18:43:08 +0200

    gamelife

--- module1/src/main/java/net/broscorp/gamelife/GameOfLife.java
+++ module1/src/main/java/net/broscorp/gamelife/GameOfLife.java
@@ -19,7 +19,15 @@ public class GameOfLife {
    * Start game simulation with input data from fileNameInput and write it to fileNameOutput.
    */
   public void game(String fileNameInput, String fileNameOutput) {
-    try (Scanner scanner = new Scanner(new File(fileNameInput))) {
+    fileReader(fileNameInput);
+    for (int i = 0; i < countOfIterations; i++) {
+      processIteration();
+    }
+    fileWriter(fileNameOutput);
+  }
+
+  private void fileReader(String fileNameInput) {
+    try (Scanner scanner = new Scanner(new File("./target/test-classes/" + fileNameInput))) {
       String infoLine = scanner.next();
       String[] info = infoLine.split(",");
       heightOfField = Integer.parseInt(info[0]);
@@ -37,19 +45,81 @@ public class GameOfLife {
     } catch (FileNotFoundException ex) {
       ex.printStackTrace();
     }
-    for (int i = 0; i < countOfIterations; i++) {
-      for (int j = 0; j < heightOfField; j++) {
-        for (int k = 0; k < widthOfField; k++) {
-          isAlive(j, k);
-          if (j == heightOfField - 1 && k == widthOfField - 1) {
-            for (int l = 0; l < heightOfField; l++) {
-              currentFields[l] = Arrays.copyOf(nextStepFields[l], widthOfField);
-            }
+  }
+
+  private void processIteration() {
+    for (int j = 0; j < heightOfField; j++) {
+      for (int k = 0; k < widthOfField; k++) {
+        isAlive(j, k);
+        if (j == heightOfField - 1 && k == widthOfField - 1) {
+          for (int l = 0; l < heightOfField; l++) {
+            currentFields[l] = Arrays.copyOf(nextStepFields[l], widthOfField);
           }
         }
       }
     }
-    try (FileOutputStream fos = new FileOutputStream(fileNameOutput)) {
+  }
+
+  private void isAlive(int j, int k) {
+    int count = 0;
+    int topLine;
+    int bottomLine;
+    int leftLine;
+    int rightLine;
+
+    if (k - 1 < 0) {
+      leftLine = widthOfField - 1;
+    } else {
+      leftLine = k - 1;
+    }
+    if (j - 1 < 0) {
+      topLine = heightOfField - 1;
+    } else {
+      topLine = j - 1;
+    }
+    if (k + 1 == widthOfField) {
+      rightLine = 0;
+    } else {
+      rightLine = k + 1;
+    }
+    if (j + 1 == heightOfField) {
+      bottomLine = 0;
+    } else {
+      bottomLine = j + 1;
+    }
+
+    int[] xCoords = {topLine, j, bottomLine};
+    int[] yCoords = {leftLine, k, rightLine};
+
+    count = isNeighboursAlive(xCoords, yCoords);
+    nextStepFields[j][k] = willBeAlive(j, k, count);
+  }
+
+  private int isNeighboursAlive(int[] xCoords, int[] yCoords) {
+    int count = 0;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < 3; j++) {
+        if (i == 1 && j == 1) {
+          continue;
+        }
+        if (currentFields[xCoords[i]][yCoords[j]] == 'X') {
+          ++count;
+        }
+      }
+    }
+    return count;
+  }
+
+  private char willBeAlive(int j, int k, int count) {
+    if ((currentFields[j][k] == 'O' && count == 3) ||
+        (currentFields[j][k] == 'X' && count > 1 && count < 4)) {
+      return 'X';
+    }
+    return 'O';
+  }
+
+  private void fileWriter(String fileNameOutput) {
+    try (FileOutputStream fos = new FileOutputStream("./target/test-classes/" + fileNameOutput)) {
       for (int i = 0; i < heightOfField; i++) {
         fos.write(Arrays.toString(currentFields[i]).replaceAll("\\W", " ").replaceAll("\\s{2}", " ")
             .trim().getBytes());
@@ -57,71 +127,6 @@ public class GameOfLife {
       }
     } catch (IOException ex) {
       ex.printStackTrace();
-    }
-  }
-
-  private void isAlive(int j, int k) {
-    int count = 0;
-    int highLine;
-    int bottomLine;
-    int leftLine;
-    int rightLine;
-    if (j - 1 < 0) {
-      highLine = heightOfField - 1;
-    } else {
-      highLine = j - 1;
-    }
-    if (j + 1 == heightOfField) {
-      bottomLine = 0;
-    } else {
-      bottomLine = j + 1;
-    }
-    if (k - 1 < 0) {
-      leftLine = widthOfField - 1;
-    } else {
-      leftLine = k - 1;
-    }
-    if (k + 1 == widthOfField) {
-      rightLine = 0;
-    } else {
-      rightLine = k + 1;
-    }
-    if (currentFields[highLine][leftLine] == 'X') {
-      count++;
-    }
-    if (currentFields[highLine][k] == 'X') {
-      count++;
-    }
-    if (currentFields[highLine][rightLine] == 'X') {
-      count++;
-    }
-    if (currentFields[j][leftLine] == 'X') {
-      count++;
-    }
-    if (currentFields[j][rightLine] == 'X') {
-      count++;
-    }
-    if (currentFields[bottomLine][leftLine] == 'X') {
-      count++;
-    }
-    if (currentFields[bottomLine][k] == 'X') {
-      count++;
-    }
-    if (currentFields[bottomLine][rightLine] == 'X') {
-      count++;
-    }
-
-    if (currentFields[j][k] == 'O' && count == 3) {
-      nextStepFields[j][k] = 'X';
-      return;
-    } else {
-      nextStepFields[j][k] = 'O';
-    }
-
-    if (currentFields[j][k] == 'X' && count > 1 && count < 4) {
-      nextStepFields[j][k] = 'X';
-    } else {
-      nextStepFields[j][k] = 'O';
     }
   }
 }
