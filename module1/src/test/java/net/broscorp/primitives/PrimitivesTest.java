5d6066f5dbe941944eae179fa07b599dac8f202f
blacksempai <benjaminfranclin1998@gmail.com>
2022-03-24 03:02:14 +0200

    Fix style

--- module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
+++ module1/src/test/java/net/broscorp/primitives/PrimitivesTest.java
@@ -1,6 +1,8 @@
 package net.broscorp.primitives;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.api.Test;
 
 class PrimitivesTest {
