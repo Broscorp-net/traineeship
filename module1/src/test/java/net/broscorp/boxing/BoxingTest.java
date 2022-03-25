be0021f48920017299e44d3261839e0130763c82
Dan2000 <noisyboy2000@gmail.com>
2022-03-25 17:10:39 +0200

    styles checked

--- module1/src/test/java/net/broscorp/boxing/BoxingTest.java
+++ module1/src/test/java/net/broscorp/boxing/BoxingTest.java
@@ -4,26 +4,27 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BoxingTest {
-    @Test
-    public void checkEqualWrappersTypes() {
-        Integer a = new Integer(1);
-        Integer b = a;
-        Assertions.assertTrue(a == b);
-    }
-    @Test
-    public void checkNotEqualWrappersTypes() {
-        Integer a = new Integer(1);
-        Integer b = new Integer(1);
-        Assertions.assertFalse(a == b);
-    }
-    @Test
-    public void checkUnboxingError() {
-        try {
-            Integer a = null;
-            Double b = Double.valueOf(a);
-        } catch (NullPointerException e) {
-            Assertions.assertTrue(true);
-        }
-    }
+  @Test
+  public void checkEqualWrappersTypes() {
+    Integer a = new Integer(1);
+    Integer b = a;
+    Assertions.assertTrue(a == b);
+  }
 
+  @Test
+  public void checkNotEqualWrappersTypes() {
+    Integer a = new Integer(1);
+    Integer b = new Integer(1);
+    Assertions.assertFalse(a == b);
+  }
+
+  @Test
+  public void checkUnboxingError() {
+    try {
+      Integer a = null;
+      Double b = Double.valueOf(a);
+    } catch (NullPointerException e) {
+      Assertions.assertTrue(true);
+    }
+  }
 }
