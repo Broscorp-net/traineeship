d318ff025bd92bd05d606724d41553d9b4cc2924
Dan2000 <noisyboy2000@gmail.com>
2022-03-30 16:16:24 +0300

    only print

---
+++ module1/src/main/java/net/broscorp/bytecode/clases/Main.java
@@ -0,0 +1,7 @@
+package net.broscorp.bytecode.clases;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Script is working");
+    }
+}
---
+++ module1/src/main/java/net/broscorp/bytecode/script/script.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+java Main.java
+javap -c Main
