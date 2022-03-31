a1ac08bd77ef766622cbfd27b46a85e4f085c0a3
blacksempai <benjaminfranclin1998@gmail.com>
2022-04-01 00:39:19 +0300

    fix style

--- module1/src/main/java/net/broscorp/bytecode/Bytecode.java
+++ module1/src/main/java/net/broscorp/bytecode/Bytecode.java
@@ -2,6 +2,9 @@ package net.broscorp.bytecode;
 
 public class Bytecode {
 
+  /**.
+   *  Universe in a nutshell
+   */
   public static void main(String[] args) {
     int entropy = 1 << 4; //aka 16
     int answer = getTheAnswerToTheUltimateQuestionOfLifeAndTheUniverseAndEverything();
