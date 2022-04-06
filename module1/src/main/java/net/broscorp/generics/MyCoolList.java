9edb27e73c5ad3e6ecaa4d59a21f023b72904d31
Dan2000 <noisyboy2000@gmail.com>
2022-04-06 16:46:30 +0300

    generics

--- module1/src/main/java/net/broscorp/generics/MyCoolList.java
+++ module1/src/main/java/net/broscorp/generics/MyCoolList.java
@@ -2,26 +2,43 @@ package net.broscorp.generics;
 
 import java.util.function.Function;
 
-public class MyCoolList {
+public class MyCoolList<T extends Number> {
+  private int init_size = 15;
+  private Object[] array = new Object[init_size];
+  private int index = 0;
 
-  public void add(Object o) {
-    throw new RuntimeException("Not implemented");
+  public void add(T item) {
+    if (index == array.length - 1) {
+      Object[] newArray = new Object[init_size * 2];
+      System.arraycopy(array, 0, newArray, 0, index);
+      array = newArray;
+    }
+    array[index++] = item;
   }
 
-  public Object get(int index) {
-    throw new RuntimeException("Not implemented");
+  public T get(int index) {
+    return (T) array[index];
   }
 
-  public Object remove(int index) {
-    throw new RuntimeException("Not implemented");
+  public T remove(int position) {
+    Object o = array[position];
+    for (int i = position; i < index; i++)
+      array[i] = array[i+1];
+    array[index] = null;
+    index--;
+    return (T) o;
   }
 
   public MyCoolList map(Function f) {
-    throw new RuntimeException("Not implemented");
+    MyCoolList list = new MyCoolList();
+    for (Object obj : array) {
+      f.apply(obj);
+      list.add((T)obj);
+    }
+    return list;
   }
 
   public int size() {
-    throw new RuntimeException("Not implemented");
+    return index;
   }
-
 }
