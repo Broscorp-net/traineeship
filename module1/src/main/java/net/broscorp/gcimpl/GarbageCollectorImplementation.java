2d9906fc83f329b4aec52045b90097814a9e470c
antono0106 <antono0106@gmail.com>
2022-04-11 20:37:24 +0300

    finished gcimpl task

--- module1/src/main/java/net/broscorp/gcimpl/GarbageCollectorImplementation.java
+++ module1/src/main/java/net/broscorp/gcimpl/GarbageCollectorImplementation.java
@@ -1,17 +1,36 @@
 package net.broscorp.gcimpl;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class GarbageCollectorImplementation implements GarbageCollector {
-
   @Override
   public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
     Collection<ApplicationBean> beans = heap.getBeans().values();
     Deque<StackInfo.Frame> frames = stack.getStack();
-    return Collections.emptyList();
+
+    Set<ApplicationBean> garbage = new HashSet<>(beans);//сет для фильтрации бинов
+
+    //проходимся по всем фреймам и бинам
+    for (StackInfo.Frame frame : frames) {
+      for (ApplicationBean frameBean : frame.getParameters()) {
+        //рекурсивно убираем живые бины и их наследников
+        removeReachable(frameBean, garbage);
+      }
+    }
+
+    //возвращаем мусор
+    return garbage.stream().collect(Collectors.toList());
+  }
+
+  private void removeReachable(ApplicationBean bean, Set<ApplicationBean> beanSet) {
+    beanSet.remove(bean);
+    for (ApplicationBean subBean : bean.getFieldValues().values()) {
+      if (beanSet.isEmpty()) {
+        break;
+      } else {
+        removeReachable(subBean, beanSet);
+      }
+    }
   }
 }
