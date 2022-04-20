a60f1c55042498fa294d2bff57ac9f04162ed558
Aziz <30.azizbek@gmail.com>
2022-04-20 21:33:28 +0500

    Add javadoc

--- module1/src/main/java/net/broscorp/bytecode/Plain.java
+++ module1/src/main/java/net/broscorp/bytecode/Plain.java
@@ -5,6 +5,11 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Plain {
+
+  /**
+   * Plain class with plain methods.
+   * @author Aziz Abdukarimov
+   */
   public static void main(String[] args) {
     int intNumber = 45;
     for (int variable = 0; variable < 55; variable++) {
@@ -12,12 +17,8 @@ public class Plain {
     }
     System.out.println("Hello trainee guys!");
     numberPrint(intNumber);
-
-    List<Integer> integerList = new ArrayList<>(
-        Arrays.asList(1, 2, 56, 1234, 56, 134, 321)
-    );
+    List<Integer> integerList = new ArrayList<>(Arrays.asList(1, 2, 56, 1234, 56, 134, 321));
     System.out.println("integerList = " + integerList);
-
   }
 
   public static void numberPrint(int number) {
